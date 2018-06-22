b59bcb18e933a09eb3e6fb23995d2db2483f45fa
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-22 10:54:02 -0500

    completed header styled components refactor

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import {Head, HeaderSpanTemp,HeaderSpanDate ,HeaderH1} from './HeaderStyles';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <Head>
+      <HeaderSpanDate>SMARCH 32, 2018</HeaderSpanDate>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <HeaderSpanTemp>98°</HeaderSpanTemp>
+    </Head>
   )
 }
 
-export default Header
+export default Header;
