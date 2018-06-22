a5d4dcfdb19825982283083618029d288641e085
Joe Stanfield <jlstan06@hotmail.com>
2018-06-22 13:03:15 -0400

    got most of components built

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -11,19 +11,21 @@ export default class Content extends Component {
     super(props);
     this.state = {
       // Set this to an initial value
-      selected: '',
+      selected: 'all',
       tabs: [],
       cards: []
     }
   }
 
   componentDidMount(){
+    this.setState({tabs: tabData, cards: cardData});
     // Once the component has mounted, get the data and reflect that data on the state
 
   }
 
   changeSelected = (tab) => {
     return () => {
+      this.setState({selected: tab});
       // Finish this function, reflecting the new selected tab in the state
     }
   }
@@ -32,7 +34,13 @@ export default class Content extends Component {
      of the items from cardData. If it is something else, it shoudl only return 
      those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
-    return this.state.cards;
+    if (this.state.selected === 'all'){
+      return this.state.cards;
+    } else {
+      let newCards = this.state.cards.slice();
+      newCards = newCards.filter(card => card.tab === this.state.selected);
+      return newCards;
+    }
   }
 
   render(){
@@ -43,7 +51,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()}/>
       </div>
     )
