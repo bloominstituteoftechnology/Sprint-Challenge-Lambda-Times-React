9226c4a8c97e9bb79b85e03920cea501979c4903
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-22 09:21:04 -0700

    converted all css styles in Header to styled components

--- lambdatimes/src/components/ReusableStyles.js
+++ lambdatimes/src/components/ReusableStyles.js
@@ -1,5 +1,64 @@
 import styled from "styled-components";
 
+
+export const HeaderContainer =styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  ${HeaderH1}
+  ${HeaderSpan}
+  ${SpanTime}
+  ${SpanDate}
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+export const SpanDate =styled.span`
+  margin-left: 25px;
+  flex: 1;
+  ${HeaderSpan}
+`;
+
+export const SpanTime =styled.span`
+	text-align: right;
+   	margin-right: 25px;
+         flex: 1;
+	 ${HeaderSpan}
+`;
+
+export const HeaderSpan =styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+
+
+export const HeaderH1 =styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+
+
+
+
+
+
 export const TopBarContainer= styled.div`
   width: 100%;
   display: flex;
