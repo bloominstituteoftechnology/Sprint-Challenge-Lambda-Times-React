bd35339853bb8552d3b583459809d02657e718b2
Tyson Gushiken <tysonism@gmail.com>
2018-06-22 10:21:53 -0700

    Convert Cards css using styled-components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardDiv = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
+
+const CardHeadlineDiv = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+
+const CardAuthorDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+
+  & > span {
+    padding-left: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+`;
+
+const CardImgDiv = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+
+  & > img {
+    width: 40px;
+  }
+`;
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <CardDiv>
+      <CardHeadlineDiv>{props.card.headline}</CardHeadlineDiv>
+      <CardAuthorDiv>
+        <CardImgDiv>
           <img src={props.card.img} alt="Article" />
-        </div>
+        </CardImgDiv>
         <span>By {props.card.author}</span>
-      </div>
-    </div>
+      </CardAuthorDiv>
+    </CardDiv>
   )
 }
 
