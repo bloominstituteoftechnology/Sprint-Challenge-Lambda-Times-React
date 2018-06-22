fef6e084cef3144a71b5782941ce967d1d692108
JacobLayton <jlayjlay3@gmail.com>
2018-06-22 10:38:37 -0700

    finally got data displayed, working on functionality

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,8 +4,7 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {props.cards.map(c => <Card key={c.img} card={c} />)}
     </div>
   )
 }
@@ -13,3 +12,6 @@ const Cards = props => {
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
+
+/* Using the cards prop, map over the list creating a 
+          new Card component for each passing the card as the only prop*/
