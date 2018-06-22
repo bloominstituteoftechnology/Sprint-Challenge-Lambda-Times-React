661d6e8e383e56c194c8acd19283de7dbbcf97ba
Munsu <dark_specineff2002@yahoo.com>
2018-06-22 10:53:27 -0700

    Khoa Nguyen Sprint-Challenge-LT-React

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -21,4 +21,70 @@ const TopBar = () => {
   )
 }
 
+const topBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`
+
+const container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`
+
+const containerLeft = styled.span`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+const containerCenter = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  cursor: pointer;
+  margin-right: 5%;
+`;
+
+const containerRight = styled.span`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+render(
+  <div>
+    <topBar
+      <container
+        
+      />
+    />
+  </div>
+)
+
 export default TopBar;
