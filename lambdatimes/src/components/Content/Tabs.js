ddb7beb7c80631498ef64ec132b6c5fb5ac3e966
Simon <Simon_Freelance@hotmail.com>
2018-06-22 12:03:18 -0400

    Made each individual tab without functionality

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab'
+import uuidv1 from 'uuid/v1';
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,6 +8,12 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
+
+            {
+              props.tabs.map(tab => {
+                return <Tab key = {uuidv1()} tab ={tab}/>;
+              })
+            }
       </div>
     </div>
   )
