bce54dad2b2ffb86bf76ca4d96944e4d9baa9bd9
Cesar Mejia Leiva <cmejia@gmail.com>
2018-06-22 11:04:24 -0700

    project complete + 2 stretch goals

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import TopBar from './components/TopBar';
-import Header from './components/Header';
-import Content from './components/Content/Content';
+import React from 'react'
+import TopBar from './components/TopBar'
+import Header from './components/Header'
+import Content from './components/Content/Content'
+import Login from './components/Login'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <TopBar />
       <Header />
-      <Content />
+      {localStorage.getItem('user') ? <Content /> : <Login />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- lambdatimes/src/index.js
+++ lambdatimes/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './CSS/index.css';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import './CSS/index.css'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'))
