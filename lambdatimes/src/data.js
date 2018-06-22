c21c8ed52bac7ff427f07528550941d681197198
Nicky Chen <nicky.chen0@gmail.com>
2018-06-22 10:22:21 -0700

    added more cards

--- lambdatimes/src/data.js
+++ lambdatimes/src/data.js
@@ -1,7 +1,7 @@
 /* Do not change anything in this file until you are completed with the assignment 
    and have moved onto the stretch goals. */
 
-export const tabData = ['all', 'javascript', 'technology', 'node', 'React.js', 'bootstrap'];
+export const tabData = ['all', 'javascript', 'technology', 'node', 'React.js', 'bootstrap', 'html'];
 
 export const cardData = [
   {
@@ -92,6 +92,18 @@ export const cardData = [
     headline: "The Hottest New Bootstrap 4 Components Every Developer Needs to Know About",
     tab: "bootstrap",
     img: "./assets/max.jpg",
+    author: "MAX GOODBOYE"
+  },
+  {
+    headline: "The Hottest HTML Components Every Developer Needs to Know About",
+    tab: "html",
+    img: "./assets/max.jpg",
+    author: "MAX GOODBOYE"
+  },
+  {
+    headline: "The Hottest HTML Components Every Developer Needs to Know About",
+    tab: "html",
+    img: "./assets/sir.jpg",
     author: "MAX GOODBOYE"
   }
 ]
