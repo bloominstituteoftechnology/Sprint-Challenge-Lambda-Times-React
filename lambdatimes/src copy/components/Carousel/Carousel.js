e254ce5ec73768615329869b5b2ee2ac58427f73
Tai <bowlofryce@gmail.com>
2018-06-22 12:17:43 -0500

    Initial Push

---
+++ lambdatimes/src copy/components/Carousel/Carousel.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { carouselData } from '../../data'
+// Complete this Carousel 
+export default class Carousel extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount(){
+
+  }
+
+  leftClick = () => {
+
+  }
+
+  rightClick = () => {
+
+  }
+
+  selectedImage = () => {
+    return <img src={} style={{display: 'block'}} />
+  }
+  
+  render(){
+    return (
+      <div className="carousel">
+        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+      </div>
+    )
+  }
+}
---
+++ lambdatimes/src copy/components/Content/Tab.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Tab = props => {
+  /* Using your props, determine if this tab is selected, if it is, the className 
+    should be: 'tab active-tab', if it is not it should just be 'tab' */
+  const selectedClass = 'tab'
+  return (
+    <div
+      className={""}
+      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
+    >
+      {props.tab.toUpperCase()}
+    </div>
+  )
+}
+
+// Make sure you include PropTypes on your props.
+
+export default Tab;
