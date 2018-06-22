855821cd174da5187e4dc39d224a1b0beb1824f8
Kevin Tena <kevten09@gmail.com>
2018-06-22 11:30:25 -0500

    Tab and Card data is rendered on component

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -11,7 +11,7 @@ export default class Content extends Component {
     super(props);
     this.state = {
       // Set this to an initial value
-      selected: '',
+      selected: 'all',
       tabs: [],
       cards: []
     }
@@ -19,6 +19,7 @@ export default class Content extends Component {
 
   componentDidMount(){
     // Once the component has mounted, get the data and reflect that data on the state
+    this.setState({tabs: tabData , cards: cardData});
 
   }
 
@@ -32,7 +33,8 @@ export default class Content extends Component {
      of the items from cardData. If it is something else, it shoudl only return 
      those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
-    return this.state.cards;
+    if(this.state.selected === "all")
+      return this.state.cards;
   }
 
   render(){
@@ -43,7 +45,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.state.changeSelected}/>
         <Cards cards={this.filterCards()}/>
       </div>
     )
