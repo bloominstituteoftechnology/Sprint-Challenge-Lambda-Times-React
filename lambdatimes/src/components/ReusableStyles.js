43c778d10dbbf44b3fbd6b160574fbf33b032682
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-22 08:59:10 -0700

    converted all css styles in TopBar to styled components

---
+++ lambdatimes/src/components/ReusableStyles.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const TopBarContainer= styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  ${ContainerLeft}
+  ${ContainerCenter}
+  ${ContainerRight}
+  ${SpanStyle}
+`;
+
+export const TopContainer= styled.div`
+width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+ ${ContainerLeft}
+ ${ContainerCenter}
+ ${ContainerRight}
+ ${SpanStyle}
+`;
+
+export const SpanStyle =styled.span`
+	 cursor: pointer;
+ 	 margin-right: 5%;
+	
+	 &:hover {
+   	  text-decoration: underline;
+  	}
+
+	&:last-child {
+ 	 margin-right: 0;
+	}
+`;
+
+export const SpanRight =styled.span`
+  	cursor: pointer;
+`;
+
+export const ContainerRight =styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  ${SpanRight}
+`;
+
+
+export const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  ${SpanStyle}
+`;
+
+
+export const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
