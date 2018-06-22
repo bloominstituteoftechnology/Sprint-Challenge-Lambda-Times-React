d3fb8ff1afa474128d3649e8542e4c141bc5c767
Blake Fletcher <blakefletcher@Blakes-MacBook-Air.local>
2018-06-22 12:54:27 -0400

    abs rendering and removed header CSS > > Mapped over tabs, gave three props to tab component on tabs.js > Gave all tabs basic className (ternary needed with active-tab? > Replaced dummyHandler with selectTabHandler

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Tab from './Tab'
 const Tabs = props => {
+  
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
+            {props.tabs.map(tab => <Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} /> )}
       </div>
     </div>
   )
