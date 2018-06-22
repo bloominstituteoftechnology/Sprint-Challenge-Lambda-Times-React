ae9ed200be6c7da88ea8cd56fe1a5f41b61b9aff
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-06-22 16:44:32 -0500

    Ahmed Belhadj - LT React Sprint Challenge
    Completed Content, Tabs and Cards functionality.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map(card => {
+        return <Card card={card} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       // Set this to an initial value
-      selected: '',
+      selected: "all",
       tabs: [],
       cards: []
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state
-
+    this.setState({ tabs: tabData, cards: cardData });
   }
 
-  changeSelected = (tab) => {
-    return () => {
-      // Finish this function, reflecting the new selected tab in the state
-    }
-  }
+  changeSelected = tab => {
+    // Finish this function, reflecting the new selected tab in the state
+    this.setState({ selected: tab });
+  };
 
   /* Complete this function, if the selected tab is 'all' it should return all 
      of the items from cardData. If it is something else, it shoudl only return 
      those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
-    return this.state.cards;
-  }
+    if (this.state.selected == "all") {
+      return cardData;
+    } else {
+      return cardData.filter(card => card.tab === this.state.selected);
+    }
+  };
 
-  render(){
+  render() {
     return (
       <div className="content-container">
         {/* 
@@ -43,9 +46,13 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
-        <Cards cards={this.filterCards()}/>
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
+        <Cards cards={this.filterCards()} />
       </div>
-    )
+    );
   }
 }
