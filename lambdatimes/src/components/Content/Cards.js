6f4a6cff56488b57458a8f6be5ca11bb2dc066c6
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-06-22 09:31:17 -0700

    displaying cards info only

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
+  console.log('cards data: ', props)
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map((item, i) =>  <Card key={i} card={item} /> )}
     </div>
   )
 }
