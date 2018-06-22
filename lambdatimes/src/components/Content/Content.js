ee277a81bc765a13f697e93c60930e4ad56d5100
Allen Hai <allenhai03@gmail.com>
2018-06-22 10:50:57 -0500

    set state on mounting

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -19,7 +19,7 @@ export default class Content extends Component {
 
   componentDidMount(){
     // Once the component has mounted, get the data and reflect that data on the state
-
+    this.setState({tabs: tabData, card: cardData})
   }
 
   changeSelected = (tab) => {
