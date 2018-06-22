7402586a62d9ecda385aebb7a5cbea4aa421ce3b
Vu Cao <cdhvu@yahoo.com>
2018-06-22 10:28:17 -0700

    Completed Carousel with infinite scrolling functionality.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,37 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      carousel: [],
+      selected: 0,
     }
   }
   componentDidMount(){
-
+    this.setState({
+      carousel: carouselData,
+    })
   }
 
   leftClick = () => {
-
+    this.setState({
+      selected: this.state.selected===0 ? 3 : this.state.selected-1
+    })
   }
 
   rightClick = () => {
-
+    this.setState({
+      selected: this.state.selected===3 ? 0 : this.state.selected+1
+    })
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.carousel[this.state.selected]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
