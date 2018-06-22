fa5862bc41345f2e9fe5706402768c8bb21f90f9
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-22 11:41:51 -0400

    Initial commit, answered questions

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import TopBar from './components/TopBar';
-import Header from './components/Header';
-import Content from './components/Content/Content';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
