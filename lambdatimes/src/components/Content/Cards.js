321647de8f264dac8c8e30894102240e303e9393
Nicky Chen <nicky.chen0@gmail.com>
2018-06-22 10:07:28 -0700

    mvp complete

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
+
 
 const Cards = props => {
   return (
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  
   return (
     <div
-      className={""}
+      className={props.selectedTab === props.tab ? "tab active-tab" : "tab"}
       onClick={() => {props.selectTabHandler(props.tab)}}
       /* Replace this dummy click handler function with your selectTabHandler function from props */
     >
@@ -15,6 +16,11 @@ const Tab = props => {
   )
 }
 
+
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string
+}
+
 
 export default Tab;
