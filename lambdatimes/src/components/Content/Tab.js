8983b3a5d4e0431299c82d1ca3b6ef669756a91d
Kevin <brackkevin@gmail.com>
2018-06-22 12:10:54 -0500

    COMPLETED INSTRUCTIONS IN TAB.JS
    - Updated class to indicate active-tab if it is selected
    - Checked incoming props with prop-types

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  let selectedClass = "tab";
+  if (props.tab === props.selectedTab) {
+    selectedClass = "tab active-tab";
+  }
   return (
-    <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
-    >
+    <div className={selectedClass} onClick={props.selectTabHandler}>
       {props.tab.toUpperCase()}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include PropTypes on your props.
+Tab.PropTypes = {
+  key: PropTypes.string,
+  tab: PropTypes.string,
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func
+};
 
 export default Tab;
