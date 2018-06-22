79b90099c6bb1b64cc305c33bea675bc82363c76
chainchompa <chainchompa@gmail.com>
2018-06-22 11:37:00 -0400

    convert TopBar styling to Styled Component

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import {
+  TopBarWrapper,
+  Container,
+  ContainerLeft,
+  ContainerLeftSpan,
+  ContainerCenter,
+  ContainerCenterSpan,
+  ContainerRight,
+  ContainerRightSpan
+} from "../components/ReusableStyles/Reusables";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopBarWrapper>
+      <Container>
+        <ContainerLeft>
+          <ContainerLeftSpan>TOPICS</ContainerLeftSpan><ContainerLeftSpan>SEARCH</ContainerLeftSpan>
+        </ContainerLeft>
+        <ContainerCenter>
+          <ContainerCenterSpan>GENERAL</ContainerCenterSpan><ContainerCenterSpan>BROWNBAG</ContainerCenterSpan><ContainerCenterSpan>RANDOM</ContainerCenterSpan><ContainerCenterSpan>MUSIC</ContainerCenterSpan><ContainerCenterSpan>ANNOUNCEMENTS</ContainerCenterSpan>
+        </ContainerCenter>
+        <ContainerRight>
+          <ContainerRightSpan>LOG IN</ContainerRightSpan>
+        </ContainerRight>
+      </Container>
+    </TopBarWrapper>
   )
 }
 
