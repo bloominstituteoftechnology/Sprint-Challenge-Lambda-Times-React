b43e1aedfafc8d8dc006819445cb1d624e5f4d00
Tyson Gushiken <tysonism@gmail.com>
2018-06-22 10:38:16 -0700

    Convert Tabs css using styled-components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const TabDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  ${props => props.active && css`
+    background-color: #fff;
+    color: #333;
+    border: 2px solid #333;
+  `}
+`;
 
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  let selectedClass = (props.selectedTab === props.tab) ? 'tab active-tab' : 'tab';
   return (
-    <div
-      className={selectedClass}
+    <TabDiv
+      active={(props.selectedTab === props.tab) ? true : false}
       onClick={() => {props.selectTabHandler(props.tab)}}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabDiv>
   )
 }
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import Tab from './Tab'
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TabsDiv = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }  
+`;
+
+const TabsTopicsDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const TabsTitleSpan = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsDiv>
+      <TabsTopicsDiv>
+        <TabsTitleSpan>TRENDING TOPICS:</TabsTitleSpan>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
           {props.tabs.map((tabItem, index) => <Tab key={`${Math.random()}${index}`}  selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={tabItem} />)}
-      </div>
-    </div>
+      </TabsTopicsDiv>
+    </TabsDiv>
   )
 }
 
