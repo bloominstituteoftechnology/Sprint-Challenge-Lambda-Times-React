187ee99d0826a9aa917b99118faf66c3f39c0e98
Katie Gorbell <katiegorbell@gmail.com>
2018-06-29 11:30:25 -0500

    converted TopBar styles

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -16,9 +16,7 @@ const HeaderStyles = styled.div`
   margin-top: 44px;
   padding-bottom: 15px;
   @media (min-width: 1280px) {
-    .header {
-      width: 1280px;
-    }
+    width: 1280px;
   }
 `;
 
