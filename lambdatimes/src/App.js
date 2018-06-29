83acb063e13de1bf628c6acca974bbe99bcfa57e
Adrian Adames <adrianadames@gmail.com>
2018-06-29 11:50:37 -0400

    first commit

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
-import Content from './components/Content/Content';
+import Content from './components/Content/Content'; 
 
 const App = () => {
   return (
