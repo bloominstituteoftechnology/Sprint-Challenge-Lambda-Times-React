2a1b0ee3253f045cf10f409ba30b32c365faad6f
Eric B <timberwolf.eric@gmail.com>
2018-06-29 12:29:20 -0500

    Attempting to factor Cards & Tabs

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
+  console.log(props);
   return (
     <div className="cards-container">
+    	{props.cards.map( potato => (
+	    <div key={Math.random(Date.now())}>
+	    <Card headline={potato.headline} img={potato.img} author={potato.author}/>
+    </div>)
+    )
+	}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -3,12 +3,18 @@ import React from 'react';
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  const selectedClass = 'tab active-tab';
+  const deselectedClass = 'tab';
+  let newClass = '';
+  if (props.tab === props.selectedTab) {
+    newClass = selectedClass;
+  }
+  else {
+    newClass = deselectedClass;
+  }
   return (
     <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
-    >
+      className={newClass} onClick={() => {props.selectTabHandler(props.tab)}}> {/* Replace this dummy click handler function with your selectTabHandler function from props */}
       {props.tab.toUpperCase()}
     </div>
   )
