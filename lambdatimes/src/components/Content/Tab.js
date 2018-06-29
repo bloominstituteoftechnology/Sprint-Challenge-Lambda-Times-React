43fc88454d35c75f23ad761ef9509c4e3e7eab69
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-06-29 11:09:33 -0700

    content.js and tabs.js completed 😊

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const Tab = props => {
+const Tab = (props) => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  const selectedClass = 'tab';
   return (
     <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
+      className={''}
+      onClick={
+        () => {
+          props.selectedTabHandler();
+        } /* Replace this dummy click handler function with your selectTabHandler function from props */
+      }
     >
       {props.tab.toUpperCase()}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include PropTypes on your props.
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import Tab from './Tab'
-const Tabs = props => {
+import Tab from './Tab';
+const Tabs = (props) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((tab, i) => (
+          <Tab
+            key={i}
+            tab={tab}
+            selected={props.selected}
+            selectTabHandler={props.selectTabHandler}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 // Make sure to use PropTypes to validate your types!
-export default Tabs
+export default Tabs;
