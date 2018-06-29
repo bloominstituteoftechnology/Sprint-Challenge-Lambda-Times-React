73192bb0924c61084a96b42fa23db05226e132e2
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-06-29 16:22:14 -0600

    changed Cards to class component

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-    {props.cards.map(card => {
-      return<Card card={card} />
-    })}
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
+export default class Cards extends Component {
+  render() {
+    const { cards } = this.props;
+    return (
+      <div className="cards-container">
+        {cards.map((article, i) => {
+          return <Card article={article} key={i} />;
+        })}
+      </div>
+    );
+  }
 }
+
 
 // Make sure you include prop types for all of your incoming props
 
