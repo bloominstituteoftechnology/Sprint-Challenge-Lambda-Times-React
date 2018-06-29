b95b48cf7ee400839650d0c13fac08b6cde0c76f
Adrian Adames <adrianadames@gmail.com>
2018-06-29 14:13:43 -0400

    Lambda Time content rendering. Filter not working yet.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -12,27 +12,47 @@ export default class Content extends Component {
     this.state = {
       // Set this to an initial value
       selected: '',
-      tabs: [],
-      cards: []
+      tabs: ['emptyTabs'],
+      cards: ['emptyCards']
     }
   }
 
   componentDidMount(){
     // Once the component has mounted, get the data and reflect that data on the state
-
+    console.log("Content DidMount called");
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
   changeSelected = (tab) => {
     return () => {
       // Finish this function, reflecting the new selected tab in the state
+      this.setState({selected: tab})
     }
+  }
+
+  selectTabHandler = e => {
+    // console.log(event.target.value);
+    this.setState({selected:e.target.value})
   }
 
   /* Complete this function, if the selected tab is 'all' it should return all 
      of the items from cardData. If it is something else, it shoudl only return 
      those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
-    return this.state.cards;
+    const selectedTab = this.state.selected.slice();
+    const cardData = this.state.cards.slice();
+    const filteredCardData = cardData.filter(card => card.tab === selectedTab);
+
+    if (selectedTab === "all") {
+       return this.setState({cards: cardData});
+    }
+    if (selectedTab === cardData.tab) {
+      return this.setState({cards:filteredCardData});
+    }
+
   }
 
   render(){
@@ -43,8 +63,12 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
-        <Cards cards={this.filterCards()}/>
+        <Tabs 
+          tabs={this.state.tabs}
+          selectedTab = {this.state.selectedTab}
+          selectTabHandler = {this.state.selectedTabHandler}
+        />
+        <Cards cards={this.state.cards}/>
       </div>
     )
   }
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -3,16 +3,41 @@ import React from 'react';
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
-  return (
-    <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
-    >
-      {props.tab.toUpperCase()}
-    </div>
-  )
+  const selectedClass = 'tab';
+  if (props.selectedTab === props.tab) {
+    return (
+      <div
+        className={"tab active-tab"}
+        // onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
+        onClick={props.selectTabHandler}
+      >
+        {props.tab.toUpperCase()}
+      </div>
+    )
+  }
+  else {
+    return (
+      <div
+        className={"tab"}
+        // onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
+        onClick={props.selectTabHandler}
+      >
+        {props.tab.toUpperCase()}
+      </div>
+    )
+  }
 }
+
+//   return (
+//     <div
+//       className={""}
+//       // onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
+//       onClick={props.selectTabHandlers}
+//     >
+//       {props.tab.toUpperCase()}
+//     </div>
+//   )
+// }
 
 // Make sure you include PropTypes on your props.
 
