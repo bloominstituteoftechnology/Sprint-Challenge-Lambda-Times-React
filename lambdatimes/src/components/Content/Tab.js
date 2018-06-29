5e1ed7f81a6b431e4e8f5763cabef3b9bc4730c7
Libby Hart <libbjo@gmail.com>
2018-06-29 12:00:37 -0600

    tabs super close to working, cards okay

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,25 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+
 
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  const selectedClass = 'tab';
   return (
     <div
-      className={`tab ${props.selected ? 'active-tab' : null }`}
-      onClick={() => props.selectTabHandler()} >
+      className={`tab ${props.selectedTab ? "tab active-tab" : selectedClass}`} 
+      onClick={() => props.selectTabHandler(props.tab)} 
+      >
       {/* {props.tab.toUpperCase()} */}
     </div>
-  )
+  );
 }
 
 // Make sure you include PropTypes on your props.
+// Tab.propTypes = {
+//   tab: PropTypes.array
+// };
+
 
 export default Tab;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tab from './Tab'
+import Tab from './Tab';
 import PropTypes from 'prop-types';
 
 const Tabs = props => {
@@ -9,8 +9,9 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
-            {props.tabs.map(tab => {
+            {props.tabs.map((tab, index) => {
               return <Tab 
+              key={index}
               tab={props.tab} 
               selectTabHandler={props.selectTabHandler} 
               selectedTab={props.selectedTab}
@@ -23,12 +24,9 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 
-// Tabs.propTypes = {
-//   tab: PropTypes.shape({
-//     text: PropTypes.string,
-//     username: PropTypes.string
-//   })
-// };
+Tabs.propTypes = {
+  tabs: PropTypes.array
+};
 
 
 export default Tabs
