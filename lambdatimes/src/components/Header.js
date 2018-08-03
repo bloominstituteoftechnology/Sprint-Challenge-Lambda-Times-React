89fb89e0ccd1dd0241d06cace52484c81ba90b9c
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-08-03 12:03:37 -0400

    got the css to show up just have a couple things to fix and did answers, will go back to after finished tasks

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const Header = () => {
-  return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+const HeaderContent = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
 
-export default Header
+const Headerdiv = styled.h1`
+font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+
+const Header = () => {
+  return(
+<HeaderContent>
+<Headerdiv>
+<span class="date">MARCH 32, 2018</span>
+  <h1>Lambda Times</h1>
+  <span class="temp">98°</span>
+  </Headerdiv>
+  </HeaderContent>
+  );
+};
+
+export default Header;
+
+
+// const Header = () => {
+  //   return (
+  //     <div className="header">
+  //       <span className="date">SMARCH 32, 2018</span>
+  //       <h1>Lambda Times</h1>
+  //       <span className="temp">98°</span>
+  //     </div>
+  //   )
+  // }
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,126 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarWrapper = styled.span`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const TopDiv = styled.div`
+width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+
+const TopLeftDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+
+// const TopDivLeft = styled.span`
+// cursor: pointer;
+//   margin-right: 25%;
+//   font-weight: bold;
+// `;
+
+
+const TopCenterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+
+// const TopDivCenter = styled.span`
+// cursor: pointer;
+// margin-right: 5%;
+// `;
+
+
+
+
+
+
+
+const TopRightDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+// const TopDivRight = styled.span`
+// cursor: pointer;
+// `;
+
+
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <TopBarWrapper>
+      <TopDiv>
+      <TopLeftDiv>
+        <span>TOPICS</span><span>SEARCH</span>
+      </TopLeftDiv>
+      <TopCenterDiv>
+        <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+      </TopCenterDiv>
+      <TopRightDiv>
+        <span>LOG IN</span>
+        </TopRightDiv>
+     </TopDiv>
+      </TopBarWrapper>
+   );
+};
+
+
 
 export default TopBar;
+
+
+
+
+// const TopBar = () => {
+//   return (
+//     <div className="top-bar">
+//       <div className="container">
+//         <div className="container-left">
+//           <span>TOPICS</span><span>SEARCH</span>
+//         </div>
+//         <div className="container-center">
+//           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+//         </div>
+//         <div className="container-right">
+//           <span>LOG IN</span>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
