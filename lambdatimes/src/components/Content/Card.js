975935b3aedb03c5b778437403805fa59ccd5cbd
johnoro <jjcummins102@gmail.com>
2018-08-03 13:01:30 -0400

    Refactored cards styling.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+
+  > div {
+    &:first-child {
+      font-size: 25px;
+      font-family: Didot, serif;
+    }
+
+    &:last-child {
+      display: flex;
+      justify-content: none;
+      align-items: center;
+      flex-direction: row;
+      margin-top: 15px;
+
+      > div {
+        padding-right: 10px;
+        border-right: 1px solid lightgrey;
+        height: 40px;
+
+        > img {
+          width: 40px;
+        }
+      }
+
+      > span {
+        padding-left: 10px;
+        font-size: 12px;
+        letter-spacing: 1px;
+        font-weight: bold;
+      }
+    }
+  }
+`;
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <StyledCard>
+      <div>{props.card.headline}</div>
+      <div>
+        <div>
           <img src={props.card.img} alt="The author of this article." />
         </div>
         <span>By {props.card.author}</span>
       </div>
-    </div>
+    </StyledCard>
   )
 }
 
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledCards = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <StyledCards>
       {props.cards.map(card => <Card key={card.headline} card={card} />)}
-    </div>
+    </StyledCards>
   )
 }
 
