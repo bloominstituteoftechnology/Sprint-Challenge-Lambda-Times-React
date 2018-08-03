6c48cb5a63db67f288842c4ca0f253e0cebbc73f
Ashwin <aswinjeganath@gmail.com>
2018-08-03 22:22:55 +0530

    Add carousel and implement the functionality

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -56,6 +57,7 @@ export default class Content extends Component {
           onSelectTab={this.changeSelected}
           selectedTab={this.state.selected}
         />
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
