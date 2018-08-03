d7d167253cbdbbb2fad9525b1289af7fc7fdd14f
Trevor <trevorfehrman@gmail.com>
2018-08-03 09:53:40 -0700

    Cards appearring on page

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,8 +5,13 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
+        {props.tabs.map(tab => (
+          <Tab 
+          selectTabHandler={ props.selectTabHandler }
+          selectedTab={ props.selectedTab }
+          tab={ tab }
+          />
+        ))}
       </div>
     </div>
   )
@@ -14,3 +19,6 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs
+
+/* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ 
