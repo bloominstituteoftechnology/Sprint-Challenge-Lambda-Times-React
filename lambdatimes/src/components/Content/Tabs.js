e4bb2d7ae14fe9d32f0bf391f47f60c20118313a
Ashwin <aswinjeganath@gmail.com>
2018-08-03 22:04:32 +0530

    Connect props from content > tabs > tab

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import Tab from './Tab'
-const Tabs = props => {
+import PropTypes from 'prop-types';
+import Tab from './Tab';
+
+const Tabs = ({ tabs, onSelectTab, selectedTab }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {tabs.map(tab => (
+          <Tab
+            key={tab}
+            tab={tab}
+            onSelectTab={onSelectTab(tab)}
+            selectedTab={selectedTab}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 // Make sure to use PropTypes to validate your types!
-export default Tabs
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onSelectTab: PropTypes.func.isRequired,
+  selectedTab: PropTypes.string.isRequired
+};
+
+export default Tabs;
