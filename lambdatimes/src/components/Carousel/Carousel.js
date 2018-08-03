cc86ea6573bc66da6749cc88632cf4d385f63cf0
johnoro <jjcummins102@gmail.com>
2018-08-03 13:11:25 -0400

    Refactored the rest of the styling that involved classnames.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,6 +1,61 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import styled from 'styled-components';
+import { carouselData } from '../../data';
+
+const StyledCarousel = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  > img {
+    width: 100%;
+    display: none;
+  }
+
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: none;
+    flex-direction: row;
+    color: #fff;
+    background-color: #333;
+    font-size: 40px;
+    border-radius: 50%;
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+
+    &:hover {
+      color: #333;
+      background-color: #fff;
+      border: 2px solid #333;
+    }
+
+    &:first-child {
+      top: 50%;
+      left: 25px;
+      transform: translate(0, -50%);
+    }
+
+    &:last-child {
+      top: 50%;
+      right: 25px;
+      transform: translate(0, -50%);
+    }
+  }
+`;
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
