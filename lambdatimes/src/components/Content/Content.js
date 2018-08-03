999eff6e1a6a413498d2eea9f36f67f09af861e8
AI2EJAY <JHammett1314@gmail.com>
2018-08-03 11:04:04 -0500

    Part I done.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -43,7 +43,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
+        <Tabs selectedTab={this.state.selected} selectTabHandler={this.changeSelected} tabs={this.state.tabs}/>
         <Cards cards={this.filterCards()}/>
       </div>
     )
