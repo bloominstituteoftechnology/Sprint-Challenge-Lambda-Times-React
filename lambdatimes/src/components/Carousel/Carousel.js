7f423ba8e80c5851505426fea3cee7a30d4cf3a8
codejoncode <jonathanjholloway@gmail.com>
2018-08-03 12:35:03 -0500

    attempting to add the carousel however the image isn't on the screen currently

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
+
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
+      activeImg: 0,
+      images: []
 
     }
   }
   componentDidMount(){
-
+    this.setState({images:carouselData});
   }
 
   leftClick = () => {
-
+    let newActive; 
+    this.state.activeImg === 0 ? newActive = this.state.images.length-1 : newActive = this.state.activeImg -1;
+    this.setState({activeImg: newActive});
   }
 
   rightClick = () => {
-
+    let newActive; 
+    this.state.activeImg === this.state.images.length-1 ? newActive = 0 : newActive = this.state.activeImg + 1; 
+    this.setState({activeImg: newActive});
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.images[this.state.activeImg]} style={{display: 'block'}} />
   }
   
   render(){
@@ -33,3 +41,4 @@ export default class Carousel extends Component {
     )
   }
 }
+
