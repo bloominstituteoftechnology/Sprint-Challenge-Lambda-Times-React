eee73e2d574b19cfab344e403ed138a7bb2f031b
LaikaFusion <newrad4@gmail.com>
2018-08-03 11:28:27 -0400

    Converted Topbar to all styled components

---
+++ lambdatimes/src/components/TopBar/ContainerLeft.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+const ContainerLeft = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+
+span {
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+}
+`
+  
+
+
+export default ContainerLeft;
---
+++ lambdatimes/src/components/TopBar/ContainerRight.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+const ContainerRight = styled.div`
+display: flex;
+justify-content: flex-end;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+font-weight: bold;
+span {
+  cursor: pointer;
+}
+`
+  
+
+
+export default ContainerRight;
