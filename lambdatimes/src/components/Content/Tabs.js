1c6f5a23b5fa4e29f697e7e79e66d436a5300686
jordan-massingill <jordan_massingill@hotmail.com>
2018-08-03 12:25:29 -0500

    Content on Page
    Content on page
    Missing functionality
    Working on functions/methods

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import Tab from './Tab'
+import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/ }
+        {props.tabs.map(item => <Tab tab={item} key={item} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} />)}
       </div>
     </div>
   )
 }
 
 // Make sure to use PropTypes to validate your types!
-export default Tabs
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
+
+export default Tabs;
