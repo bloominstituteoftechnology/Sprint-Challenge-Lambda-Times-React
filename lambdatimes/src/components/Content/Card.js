499cb3fe9929628d7c4a606cd5cca0bdaccd0eea
Erik Kimsey <erik@erikkimsey.com>
2018-08-03 19:10:04 -0400

    Converted styles in Card.js to sstyled-components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,24 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import style from 'styled-components';
+import styled from 'styled-components';
 
-const CardContainer = style.div`
-  
+const CardContainer = styled.div`
+   border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
+
+const Headline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+
+const Author = styled.div`
+    display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+`;
+
+const ImgContainer = styled.div`
+    padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+`;
+
+const Image = styled.img`
+  width: 40px;
+`;
+
+const AuthorSpan = styled.span`
+    padding-left: 10px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
 `;
 
 const Card = props => {
 
   let card = props.card
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={`${props.card.img}`} />
-        </div>
-        <span>By {props.card.author}</span>
-      </div>
-    </div>
+    <CardContainer>
+      <Headline>{props.card.headline}</Headline>
+      <Author>
+        <ImgContainer className="img-container">
+          <Image src={`${props.card.img}`} />
+        </ImgContainer>
+        <AuthorSpan>By {props.card.author}</AuthorSpan>
+      </Author>
+    </CardContainer>
   )
 }
 
@@ -27,7 +65,5 @@ Card.propTypes = {
   headline: PropTypes.string,
   img: PropTypes.string
 };
-
-// Make sure to include PropTypes.
 
 export default Card;
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   
   return (
-    <div className="cards-container">
+    <CardsContainer className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
           {
@@ -13,7 +27,7 @@ const Cards = props => {
               return <Card card={el}/>
             })
           }
-    </div>
+    </CardsContainer>
   )
 }
 
