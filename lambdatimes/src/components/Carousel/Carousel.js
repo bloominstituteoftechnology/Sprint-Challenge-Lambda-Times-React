f7702e484b551404ed620fb592a441f2e657f370
Charles Cortinas <crcortinas@gmail.com>
2018-08-03 15:26:26 -0500

    Carousel
    Built Carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,33 +1,56 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
-// Complete this Carousel 
+// Complete this Carousel
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      links: [],
+      index: 0,
+      length: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      links: carouselData,
+      length: carouselData.length
+    });
   }
 
   leftClick = () => {
-
+    let i = this.state.index;
+    let length = this.state.length;
+    i--;
+    if (i===-1) {
+      i=length-1;
+    }
+    this.setState({
+      index: i
+    });
   }
 
   rightClick = () => {
-
+    let i = this.state.index;
+    let length = this.state.length;
+    i++;
+    if (i===length) {
+      i=0;
+    }
+    this.setState({
+      index: i
+    });
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    console.log(this.state.index);
+    return <img src={this.state.links[this.state.index]} style={{display: 'block'}} alt='carousel' />
   }
-  
+
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -57,6 +58,7 @@ export default class Content extends Component {
           and `selectTabHandler` that includes the function to change the selected tab
         */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
+        <Carousel />
         <Cards cards={this.filterCards()}/>
       </ContentContainer>
     )
