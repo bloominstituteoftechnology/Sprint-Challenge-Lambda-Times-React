f0e59a5079df983ba47f162d2726837ab3a1dca0
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-03 11:19:50 -0400

    Answers/md and styled components completed

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,22 @@
 import React from 'react';
-
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+import {StyledTopBar, StyledContainter, StyledContainterLeft, StyledContainterCenter, StyledContainterRight} from './StyledComponents';
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+
+    <StyledTopBar>
+      <StyledContainter>
+        <StyledContainterLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </StyledContainterLeft>
+        <StyledContainterCenter>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </StyledContainterCenter>
+        <StyledContainterRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </StyledContainterRight>
+      </StyledContainter>
+    </StyledTopBar>
   )
 }
 
