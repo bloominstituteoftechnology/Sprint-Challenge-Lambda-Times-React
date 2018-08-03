19c7dd34b8a2cb1a0e34326efc4eba4f40bd772c
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-08-03 11:52:26 -0500

    Answers to questions and part 1 complete

---
+++ ANSWERS.md
@@ -0,0 +1,9 @@
+1. What are PropTypes used for?
+ - Proptypes are used like a quality control test for the data in the react app. It works by checking to see if the data still passes when there are any updates or changes to the code. 
+2. What is a lifecycle event in React?
+  - Lifecycle event is the life cycle of a component. Mounting phase, Updating phase, and Unmounting phase. 
+3. What is a Higher Order Component?
+  - A higher order component works like a parent component or a container. It wraps around another component, and passes down information to it
+4. What are three different ways to style components in React?
+  - CSS, in-line, and Javascript. 
+  
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,110 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarStyled = styled.div `
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: none;
+flex-direction: row;
+position: fixed;
+height: 44px;
+background-color: #333;
+`;
+
+
+const TBContainer = styled.div `
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+
+const TBLeft = styled.div `
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+    span {
+      cursor: pointer;
+      margin-right: 25%;
+      font-weight: bold;
+    }
+`;
+
+
+const TBCenter = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex: 3;
+    font-size: 9px;
+      span {
+        cursor: pointer;
+        margin-right: 5%;
+      }
+      span:last-child {
+        margin-right: 0;
+      }
+      span:hover {
+        text-decoration: underline;
+      }
+`;
+
+const TBRight = styled.div `
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+    span {
+     cursor: pointer;
+    }
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TopBarStyled>
+      <TBContainer>
+        <TBLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </TBLeft>
+        <TBCenter>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </TBCenter>
+        <TBRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </TBRight>
+      </TBContainer>
+    </TopBarStyled>
   )
 }
 
 export default TopBar;
+
