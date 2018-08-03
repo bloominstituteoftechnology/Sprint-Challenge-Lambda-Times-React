3cd94974d6c66c853c4e4b34d48fab1567ac5357
kamalnrf <kamalnrf@hotmail.com>
2018-08-03 22:35:59 +0530

    Completed Carousel

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -5,6 +5,7 @@ import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+import Carousel from '../Carousel/Carousel';
 
 export default class Content extends Component {
   constructor(props){
@@ -59,6 +60,7 @@ export default class Content extends Component {
           and `selectTabHandler` that includes the function to change the selected tab
         */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
+        <Carousel />
         <Cards cards={this.filterCards()}/>
       </div>
     )
