f958bf6d76ae8f42daae60c1ce6fd36c61f3b35f
codejoncode <jonathanjholloway@gmail.com>
2018-08-03 11:30:35 -0500

    the tabs are on the screen looking nice functionality not there have to complete card render

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -19,6 +19,7 @@ export default class Content extends Component {
 
   componentDidMount(){
     // Once the component has mounted, get the data and reflect that data on the state
+    this.setState({tabs:tabData, cards: cardData});
 
   }
 
@@ -36,6 +37,7 @@ export default class Content extends Component {
   }
 
   render(){
+    
     return (
       <div className="content-container">
         {/* 
@@ -43,7 +45,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.tabs[0]} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()}/>
       </div>
     )
