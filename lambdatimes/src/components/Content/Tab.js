db25300d12bb7a19650ce600774a2a8a4f547a02
francistse23 <wtse.ucsd@gmail.com>
2018-08-03 09:35:01 -0700

    Data displayed on page. filter function not working as expected. currently debugging.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
-  /* Using your props, determine if this tab is selected, if it is, the className 
-    should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  const selectedClass = `tab ${props.selected === props.tab ? 'active-tab':''}`; 
   return (
     <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
+      className={selectedClass}
+      onClick={() => props.selectTabHandler(props.tab)}
     >
       {props.tab.toUpperCase()}
     </div>
   )
 }
 
-// Make sure you include PropTypes on your props.
-
+Tab.proptypes = {
+  selectTabHandler: PropTypes.func,
+  selected: PropTypes.string,
+  tab: PropTypes.string
+}
 export default Tab;
