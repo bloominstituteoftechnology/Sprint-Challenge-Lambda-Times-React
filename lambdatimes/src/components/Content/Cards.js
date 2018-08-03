b4aed7acb339e772f929fe9708a8b7da856c0aa1
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-08-03 11:23:56 -0500

    working on Cards
    Got the cards to show up but the css is not working right

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React, { Component } from "react";
+import Card from "./Card";
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map((cardInfo, index) => {
+        return (
+          <div key={index}>
+            <div className="card">
+              <div className="headline">{cardInfo.headline}</div>
+              <div className="author">
+                <div className="img-container">
+                  <img src={cardInfo.img} alt="dogs"/* image source goes here */ />
+                </div>
+                <span>By {cardInfo.author}</span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
