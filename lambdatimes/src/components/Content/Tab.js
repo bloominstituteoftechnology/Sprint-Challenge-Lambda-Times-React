42f026b0479ba992c8a6c0533918ba728af370d5
Charlie Winslow <cwinslow22@yahoo.com>
2018-08-03 13:21:55 -0500

    trying to get the tab onClick working

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  const selectedClass = props.tab === props.selectedTab ? 'active-tab' : 'null';
   return (
     <div
-      className={""}
-      onClick={() => {  /* Replace this dummy click handler function with your selectTabHandler function from props */}}
+      className={selectedClass}
+      onClick={() => { props.selectTabHandler(props.tab) /* Replace this dummy click handler function with your selectTabHandler function from props */}}
     >
       {props.tab.toUpperCase()}
     </div>
