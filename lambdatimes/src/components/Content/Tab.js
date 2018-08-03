64d44c68269505c885aecbf01fcb22028379c616
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-03 13:18:04 -0400

    tab components working

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if this tab is selected, if it is, the className 
-    should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+    const selectedClass = `tab${props.selectedTab === props.tab ? 'active-tab' : ''}`;
   return (
-    <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
-    >
-      {props.tab.toUpperCase()}
+    <div className={selectedClass} onClick={props.selectTabHandler}>
+    {props.tab.toUpperCase()}
     </div>
   )
 }
