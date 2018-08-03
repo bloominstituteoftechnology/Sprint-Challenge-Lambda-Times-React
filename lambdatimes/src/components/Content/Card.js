d813ba3779e79828312a0c498e53c9d963b0f968
Ashwin <aswinjeganath@gmail.com>
2018-08-03 22:11:32 +0530

    Connect props from content > cards > card - Basic objectives complete

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Card = props => {
+const Card = ({ card: { headline, img, author } }) => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={""/* image source goes here */} />
+          <img src={img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {author}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.shape({
+    headline: PropTypes.string.isRequired,
+    tab: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default Card;
