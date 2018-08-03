8b2556bc91f1de04aafbcc3a8592059913cbb3cf
szincone <sawyerzincone@gmail.com>
2018-08-03 10:14:10 -0600

    working click function on tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       // Set this to an initial value
-      selected: '',
+      selected: "",
       tabs: [],
       cards: []
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state
-
+    this.setState({ tabs: tabData, cards: cardData });
   }
 
-  changeSelected = (tab) => {
+  changeSelected = tab => {
     return () => {
       // Finish this function, reflecting the new selected tab in the state
-    }
-  }
+      console.log("working click, tab value equals", tab);
+    };
+  };
 
   /* Complete this function, if the selected tab is 'all' it should return all 
-     of the items from cardData. If it is something else, it shoudl only return 
-     those cards whose 'tab' mached that which is selected. */
+  of the items from cardData. If it is something else, it shoudl only return 
+  those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
     return this.state.cards;
-  }
+  };
 
-  render(){
+  render() {
     return (
       <div className="content-container">
         {/* 
@@ -43,9 +44,9 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
-        <Cards cards={this.filterCards()}/>
+        <Tabs tabs={this.state.tabs} selectedTab={this.changeSelected()} />
+        <Cards cards={this.filterCards()} />
       </div>
-    )
+    );
   }
 }
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  console.log("Tab", props);
+
+  const selectedClass = "tab";
   return (
-    <div
-      className={""}
-      onClick={() => {/* Replace this dummy click handler function with your selectTabHandler function from props */}}
-    >
+    <div className={selectedClass} onClick={props.selectedTab}>
       {props.tab.toUpperCase()}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include PropTypes on your props.
 
