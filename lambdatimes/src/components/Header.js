8a029afb31fd082b543807db30cbe796df486b59
Arthur Bates <aabatesj@gmail.com>
2018-08-03 13:25:06 -0400

    Part 1
    'Title' is already used. webkit wont let me name lambda times 'Lambda", Name", "logo", "cover" I spent half an hour just trying to find a name for my styled component. ridiculous to say the least.

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
 
-const Header = () => {
+const Title = styled.title`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const Span = styled.span`
+align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+const Date = Span.extend`
+ margin-left: 25px;
+  flex: 1;
+`;
+
+const Temp = Span.extend`
+text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+const HeaderContainer = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
+    <Header>
+      <Date>SMARCH 32, 2018</Date>
+      <Title>Lambda Times</Title>
+      <Temp>98°</Temp>
+    </Header>
+  );
 }
 
-export default Header
+export default HeaderContainer;
