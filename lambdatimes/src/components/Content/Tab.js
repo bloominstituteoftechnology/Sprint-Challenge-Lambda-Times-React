ef500c9278e1649feb001085971d4399cc0bcfc3
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-03 14:12:26 -0700

    Major Changes - Tabs and Cards functionality
    Gave the proper functionality according to the solution (when clicked on tab, active class is added to the className of the tag.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab';
+  // const selectedClass = 'tab';
   return (
     <div
-      className={`tab ${props.selected ? true : false}`}
+      className={`tab ${props.selectedTab ? 'active-tab' : null}`}
       onClick={() => {
         props.selectTabHandler(props.tab)}}
     >
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Tab from './Tab'
-import PropTypes from 'prop-types';
 
 const Tabs = props => {
   return (
@@ -13,20 +12,13 @@ const Tabs = props => {
               tab={tab} 
               key={index}  
               selectTabHandler={props.selectTabHandler}
-              selectedTab={props.selectedTab}
+              selectedTab={props.selectedTab === tab}
             />)})}
       </div>
     </div>
   )
 }
 
-Tabs.propTypes = {
-  tabs: PropTypes.arrayOf(
-    PropTypes.shape({
-      tabdData: PropTypes.string
-    })
-  )
-}
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs
