4d3ebb95482e9077402d5eee2f7d865a009fd787
LaikaFusion <newrad4@gmail.com>
2018-08-03 12:24:14 -0400

    Working tab select and card filter

--- lambdatimes/src/data.js
+++ lambdatimes/src/data.js
@@ -1,7 +1,9 @@
 /* Do not change anything in this file until you are completed with the assignment 
    and have moved onto the stretch goals. */
 
-export const tabData = ['all', 'javascript', 'technology', 'node', 'React.js', 'bootstrap'];
+/*Yes I saw that ^ but I wanted to avoid using toLowerCase() when I can just change five values in here so their capitalization actually matches the rest of the data*/
+
+export const tabData = ['all', 'javascript', 'technology', 'node', 'react.js', 'bootstrap'];
 
 export const cardData = [
   {
@@ -30,7 +32,7 @@ export const cardData = [
   },
   {
     headline: "History of Javascript Libraries: from jQuery to React",
-    tab: "React.js",
+    tab: "react.js",
     img: "./assets/puppers.jpg",
     author: "PUPPER S. DOGGO"
   },
@@ -54,7 +56,7 @@ export const cardData = [
   },
   {
     headline: "React.js v16 What's new, and what you should know about.",
-    tab: "React.js",
+    tab: "react.js",
     img: "./assets/fido.jpg",
     author: "FIDO WALKSALOT"
   },
@@ -84,7 +86,7 @@ export const cardData = [
   },
   {
     headline: "React vs Angular vs Vue vs ??? How the frameworks stack up.",
-    tab: "React.js",
+    tab: "react.js",
     img: "./assets/fido.jpg",
     author: "FIDO WALKSALOT"
   },
