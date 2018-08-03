90837ffe30f82eb2d8609111314e52216fb403a6
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-03 11:48:47 -0400

    Content, tabs, tab filled in with proptypes

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -13,37 +13,41 @@ export default class Content extends Component {
       // Set this to an initial value
       selected: '',
       tabs: [],
-      cards: []
+      cards: [],
     }
   }
 
   componentDidMount(){
     // Once the component has mounted, get the data and reflect that data on the state
+    this.setState({ selected: 'all', tabs: tabData, cards: cardData });
 
   }
 
   changeSelected = (tab) => {
     return () => {
       // Finish this function, reflecting the new selected tab in the state
+      this.setState({selected: tab});
     }
   }
 
-  /* Complete this function, if the selected tab is 'all' it should return all 
-     of the items from cardData. If it is something else, it shoudl only return 
+  /* Complete this function, if the selected tab is 'all' it should return all
+     of the items from cardData. If it is something else, it shoudl only return
      those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
-    return this.state.cards;
+    return this.state.cards.filter(card => card.tab === this.state.selected);
   }
 
   render(){
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
+        {/*
+          Add 2 props to the Tabs component,
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs}/>
+        <Tabs selectedTab={this.state.selected}
+              selectTabHandler={this.changeSelected}
+              tabs={this.state.tabs}/>
         <Cards cards={this.filterCards()}/>
       </div>
     )
