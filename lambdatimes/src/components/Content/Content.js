17129eb6c180150d9db5aaca3b35b7498f9cd524
Erik Kimsey <erik@erikkimsey.com>
2018-08-03 16:30:11 -0400

    Cleanedup

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -19,18 +19,13 @@ export default class Content extends Component {
   constructor(props){
     super(props);
     this.state = {
-      // Set this to an initial value
       selected: 'all',
       tabs: [],
       cards: [],
-      selectedCards:[]
     }
-    // console.log("this selected",this.state.selected);
-    
   }
 
   componentDidMount(){
-    // Once the component has mounted, get the data and reflect that data on the state
     this.setState({
       tabs: tabData, cards: cardData,
     });
@@ -42,9 +37,6 @@ export default class Content extends Component {
       })
   }
 
-  /* Complete this function, if the selected tab is 'all' it should return all 
-     of the items from cardData. If it is something else, it shoudl only return 
-     those cards whose 'tab' mached that which is selected. */
   filterCards = () => {
     if(this.state.selected === 'all') return this.state.cards;
     let nuC = this.state.cards.filter((card,i)=>{ 
@@ -56,11 +48,6 @@ export default class Content extends Component {
   render(){
     return (
       <ContentContainer>
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()}/>
       </ContentContainer>
