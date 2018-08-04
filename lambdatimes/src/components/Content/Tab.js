4449bb71e0d0f5a3221b89393b7b421bc103f037
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-04 09:44:01 -0700

    add PropTypes check

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
@@ -15,5 +16,8 @@ const Tab = props => {
 }
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string
+}
 
 export default Tab;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab'
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -11,6 +13,10 @@ const Tabs = props => {
       </div>
     </div>
   )
+}
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
 }
 
 // Make sure to use PropTypes to validate your types!
