0cfb0858ad5d80a8c74203586c76db9a8a4254d2
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-04 10:31:16 -0700

    add functional carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,28 +5,38 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      data: [],
+      current: 0
     }
   }
   componentDidMount(){
-
+    this.setState({data: carouselData});
   }
 
   leftClick = () => {
-
+    this.setState(prevState => {
+      return ({
+        current: prevState.current - 1 < 0 ? this.state.data.length - 1 : prevState.current - 1
+      })
+    });
   }
 
   rightClick = () => {
-
+    this.setState(prevState => {
+      return ({
+        current: prevState.current + 1 > this.state.data.length - 1 ? 0 : prevState.current + 1
+      })
+    });
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.data[this.state.current]} style={{display: 'block'}} alt="cute doggy" />
   }
   
   render(){
     return (
       <div className="carousel">
+        {this.selectedImage()}
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
