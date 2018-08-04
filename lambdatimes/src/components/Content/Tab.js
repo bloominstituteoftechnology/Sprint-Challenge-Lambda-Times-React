0cae74f0580400a4ec1418f7e883260c115fd666
Bryce Evans <bryceevans@205-185-99-110.directcom.com>
2018-08-03 18:06:45 -0600

    another change

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 const Tab = props => {
   /* Using your props, determine if this tab is selected, if it is, the className 
     should be: 'tab active-tab', if it is not it should just be 'tab' */
-  const selectedClass = 'tab'
+  
+    // const selectedClass = 'tab'
 
   return (
     <div
