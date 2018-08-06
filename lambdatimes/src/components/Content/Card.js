7deb4cfd978285fe67f57667094908312de5060f
Ryan Hamblin <ryanhca@gmail.com>
2018-08-06 11:02:54 -0600

    card file

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -6,13 +6,13 @@ const Card = props => {
       <div className="headline">{/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
-          <img src={""/* image source goes here */} />
+          <img src={'' /* image source goes here */} />
         </div>
         <span>By {/* author goes here */}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // Make sure to include PropTypes.
 
