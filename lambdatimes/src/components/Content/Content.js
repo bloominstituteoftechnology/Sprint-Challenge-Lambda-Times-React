d0d08d4ce23976dc7a7b3b3459af3dd2f308ca8e
JaredRCooper <jaredrcoopet@gmail.com>
2018-08-09 17:52:01 -0400

    tabs are displaying

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,13 +18,24 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
   changeSelected = tab => {
+    this.setState({
+      selected: tab
+    })
     // this function should take in the tab and update the state with the new tab.
   };
 
   filterCards = () => {
+    return this.state.selected === 'all' ?  this.state.cards : 
+    this.state.cards.filter((card) => {
+      return card.selected;
+    })
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
@@ -37,7 +48,6 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
   };
 
   render() {
@@ -48,7 +58,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
+        {props.tabs.map((tab, i) => {
+          return <Tab selectTabhandler={props.selectTabhandler} selectedTab={props.selectedTab} tab={tab}/>
+        })/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
     </div>
@@ -13,4 +16,10 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.array,
+  selectedTab: PropTypes.string,
+  selectTabhanlder: PropTypes.function
+
+}
 export default Tabs;
