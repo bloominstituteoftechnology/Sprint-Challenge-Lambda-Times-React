c9ad4a020cf09eb881c0cb998419c9d02bcc1d6a
Fred <sohnf90@yahoo.com>
2018-08-10 10:56:34 -0700

    Added some proptypes and Completed Part 3: Styled Components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
   return (
@@ -15,5 +16,10 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  headline: PropTypes.string,
+  img: PropTypes.string,
+  author: PropTypes.string
+}
 
 export default Card;
