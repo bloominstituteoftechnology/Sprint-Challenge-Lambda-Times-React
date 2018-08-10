a23a3bf2c46e87614f947d046cb58956976db85c
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-08-10 11:17:26 -0500

    tabs are showing on screen, and able to toggle through. Working on the cards showing on screen

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,15 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState ({tabs: tabData, cards: cardData});
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState ({
+      selected: tab
+    });
+
   };
 
   filterCards = () => {
@@ -35,8 +40,9 @@ export default class Content extends Component {
       Your algorithim for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
           of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
+        - else, it should only return those cards whose 'tab' matched this.state.selected.'
     */
+
     return this.state.cards;
   };
 
@@ -48,7 +54,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Tab from './Tab';
+
+
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+            props.tabs.map(tab => {
+              return <Tab tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab}/>
+            })
+            
+          }
       </div>
     </div>
   );
