72cc5add770135cf797fcc51858a9136b7f0f0db
Erica Thompson <EricaThompson@iMac.lan>
2018-08-10 12:10:49 -0700

    Push

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,12 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    
   };
 
   filterCards = () => {
@@ -31,6 +33,7 @@ export default class Content extends Component {
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
+    
 
       Your algorithim for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
