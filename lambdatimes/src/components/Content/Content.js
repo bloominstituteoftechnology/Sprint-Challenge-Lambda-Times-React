d75b57d7e42b969a40750c61a6115ca851dc4614
Erica Thompson <EricaThompson@iMac.lan>
2018-08-12 20:47:45 -0700

    more complete

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,12 +18,17 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-    
+   this.setState({
+     tabs: tabData,
+     cards: cardData
+   })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    
+    this.setState({
+      selected: tab
+    })
   };
 
   filterCards = () => {
@@ -39,9 +44,26 @@ export default class Content extends Component {
         - if the selected tab is 'all' it should return all 
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
+
+      
+
+
     */
-    return this.state.cards;
+   if (this.state.selected === 'all')
+    {return this.state.cards;
+    } else {
+      const filtered = this.state.cards.filter(each => {
+        return each.tab === this.state.selected}
+      )} 
+      
   };
+
+
+
+
+
+
+
 
   render() {
     return (
@@ -51,7 +73,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs selectedTab={this.state.selected} tabs={this.state.tabs} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -10,6 +10,7 @@ const Tab = props => {
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         props.selectTabHandler(props.tab)
       }}
     >
       {props.tab.toUpperCase()}
