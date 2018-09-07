9a71607df1bbfe6b4a2c461fbf374e1ed3632ed3
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-09-07 13:30:13 -0400

    Tweaks

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -7,7 +7,7 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/
-          props.cards?props.cards.map((e,i)=><Card key={i} card={e}/>):null}
+          props.cards.map((jo,ja)=> <Card key={ja} card={jo}/>)}
     </div>
   )
 }
