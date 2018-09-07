274e1d90b5d9f08ff912a0075a692bead2d84c4a
Evan Carlstrom <evan.carlstrom@gmail.com>
2018-09-07 12:39:43 -0400

    fixed an issue with a comment appearing, Cards.js complete

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -9,7 +9,7 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-        {props.tabs.map(tab => <Tab selectTabHandler = {props.selectTabHandler} selectedTab = {props.selectedTab} tab = {tab} />)} // assigning props to mapped component
+        {props.tabs.map(tab => <Tab selectTabHandler = {props.selectTabHandler} selectedTab = {props.selectedTab} tab = {tab} />)} {/*assigning props to mapped component*/}
       </div>
     </div>
   );
