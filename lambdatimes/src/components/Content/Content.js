16ea9b5625ec787534ce9f93301b3021f9c6bdac
Alex <alexandraswartz@Alexandras-MacBook-Air-2.local>
2018-09-07 13:38:47 -0400

    answered questions, checked data with proptypes, styled components, tabs and cards are appearing, selected tab works, filtercards does not work

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,11 +17,12 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({tabs: tabData, cards: cardData});
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({selected: tab});
   };
 
   filterCards = () => {
@@ -37,7 +38,12 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    
+    const card = this.state.cards.filter(card=> {
+     if (card['tab'] === this.state.selected) {
+       return card;
+     }
+    })
   };
 
   render() {
@@ -48,8 +54,8 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} filterCards={this.filterCards}/>
+        <Cards cards={this.filterCards()} card={this.state.cards} />
       </div>
     );
   }
