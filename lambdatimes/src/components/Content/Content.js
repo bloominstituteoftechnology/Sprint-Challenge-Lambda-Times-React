6d7164d9a45e9f33ff9df6cd049ac923c1e8c57b
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-09-07 12:20:16 -0500

    Completed Sprint Challenge

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 import Tabs from './Tabs';
 import Cards from './Cards';
-
-// Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -17,40 +21,33 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: tabData });
+    this.setState({ cards: cardData });
   }
 
   changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
+    return () => {
+      this.setState({ selected: tab });
+    };
   };
 
   filterCards = () => {
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
-
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
-    return this.state.cards;
+    if (this.state.selected === 'all') {
+      return cardData;
+    }
+    return cardData.filter(card => card.tab === this.state.selected);
   };
 
   render() {
     return (
-      <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
-        <Tabs tabs={this.state.tabs} />
+      <ContentContainer>
+        <Tabs
+          tabs={this.state.tabs}
+          selectTabHandler={this.changeSelected}
+          selectedTab={this.state.selected}
+        />
         <Cards cards={this.filterCards()} />
-      </div>
+      </ContentContainer>
     );
   }
 }
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// const TabsStyle = styled.div`
+//   border-bottom: 1px solid lightgrey;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: row;
+//   width: 100%;
+//   height: 47px;
+//   background-color: #fff;
+
+//   @media (min-width: 1280px) {
+//     .tabs {
+//       width: 1280px;
+//     }
+//   }
+// `;
+
+// const Topics = styled.div`
+//   display: flex;
+//   justify-content: none;
+//   align-items: center;
+//   flex-direction: row;
+
+// `;
+
+const Title = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        <Title>TRENDING TOPICS:</Title>
+        {props.tabs.map(item => (
+          <Tab
+            tab={item}
+            selectTabHandler={props.selectTabHandler(item)}
+            selectedTab={props.selectedTab}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.PropTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
+};
+
 export default Tabs;
