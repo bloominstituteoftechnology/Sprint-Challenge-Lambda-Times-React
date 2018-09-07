3385b20f63c41923e9e0028ce47e67472a4d1c1c
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-07 10:16:17 -0500

    Initial PR

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,14 +3,30 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <TopBar />
+//       <Header />
+//       <Content />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <TopBar />
+        <Header />
+        <Content />
+      </div>
+    );
+  }
 }
 
 export default App;
