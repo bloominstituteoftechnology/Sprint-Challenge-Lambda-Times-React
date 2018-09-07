83cce43f71bfe0cd175b688d0696a0b04ce62103
MikeRGarrity <mikergarrity@gmail.com>
2018-09-07 10:52:47 -0500

    adding changeSelected and filterCards functionality

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,7 +8,7 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-            {props.tabs.map(tab => <Tab tab={tab} key={tab} handleTab={() => props.selectTabHandler(tab)} selectedTab={props.selected} />)}
+            {props.tabs.map(tab => <Tab tab={tab} key={tab} handleTab={() => props.selectTabHandler(tab)} selectedTab={props.selected}  />)}
       </div>
     </div>
   );
