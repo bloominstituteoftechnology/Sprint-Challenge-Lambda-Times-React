b8969114148ef1d79beb32d872243459cd2fb731
Eva Li <evalee904@gmail.com>
2018-09-07 12:45:30 -0500

    Header and TopBar sc done.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -24,9 +24,9 @@ export default class Content extends Component {
     });
   }
 
-  changeSelected = e => {
+  changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    this.setState({selected: selectedTab});
+    this.setState({selected: tab});
   };
 
   filterCards = () => {
@@ -42,7 +42,12 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if(this.state.selected === 'all') {
+      return this.state.cards;
+    } else {
+      return this.state.cards.filter(card => card.tab === this.state.selected)
+    }
+    
   };
 
   render() {
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,61 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  }
+  @media (min-width: 1280px) {
+  .header {
+    width: 1280px;
+  }
+`;
+
+const Date =styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const Temp =styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
+const H1 = styled.h1`
+font-size: 60px;
+font-family: Didot, serif;
+font-weight: bold;
+flex: 8;
+text-align: center;
+color: #000;
+`;
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderDiv>
+      <Date>SMARCH 32, 2018</Date>
+      <H1>Lambda Times</H1>
+      <Temp>98°</Temp>
+    </HeaderDiv>
   )
 }
 
