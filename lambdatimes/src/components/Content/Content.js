b3143873d59a5efd53bd0a48442b2242289b5ed9
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-09-07 09:21:30 -0700

    adding filterCards

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -40,7 +40,15 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    let selectedTab = this.state.selected;
+    if (selectedTab === 'all') {
+      return this.state.cards
+    }
+    else {
+      return selectedTab.filter(tab => tab === this.state.cards)
+    }
+
+    // return this.state.cards;
   };
 
   render() {
@@ -51,7 +59,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab = {this.changeSelected()} selectedTabHandler ={this.filterCards()} />
         <Cards cards={this.filterCards()} />
       </div>
     );
