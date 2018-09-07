66757e3b221b8ad271056b68367d7ea4c768ac07
MikeRGarrity <mikergarrity@gmail.com>
2018-09-07 12:01:19 -0500

    refactoring TopBar and Header with styled components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -6,11 +6,11 @@ const Tab = props => {
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
       const isSelected = props.selectedTab === props.tab;
-      const selectedClass = 'tab'
+      const tabClass = 'tab'
       const activeClass = 'tab active-tab'
   return (
     <div
-      className={isSelected ? activeClass : selectedClass}
+      className={isSelected ? activeClass : tabClass}
       onClick= {props.handleTab}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
