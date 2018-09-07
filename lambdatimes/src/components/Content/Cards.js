4d341e7013a9221e229d4c9eca18b10f57e668f0
unknown <cornielleandres@gmail.com>
2018-09-07 12:10:57 -0400

    Complete MVP for Content section.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
+import PropTypes from 'prop-types';
+
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <div className = "cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      { props.cards.map((card, i) => <Card key = { i } card = { card } />) }
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string,
+      tab: PropTypes.string,
+      image: PropTypes.string,
+      author: PropTypes.string,
+    })
+  ),
+}
 
 export default Cards;
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,37 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      let conditionalClassName = '';
+
+      if (props.tab === props.selectedTab) {
+        conditionalClassName = 'tab active-tab';
+      } else {
+        conditionalClassName = 'tab';
+      }
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className = { conditionalClassName } 
+      // onClick = {() => {
+      //   /* Replace this dummy click handler function with your selectTabHandler function from props 
+      //    you'll need to pass the `tab` in as an argument to this handler. */
+      // }}
+      onClick = { props.selectTabHandler(props.tab) }
     >
-      {props.tab.toUpperCase()}
+      { props.tab.toUpperCase() }
     </div>
   );
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string,
+  tab: PropTypes.string,
+}
 
 export default Tab;
