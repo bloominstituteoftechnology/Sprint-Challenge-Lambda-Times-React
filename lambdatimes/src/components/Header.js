ee45c708cc24466f0923750f59027a03cd57e0cd
decagondev <decagondevelopment@gmail.com>
2018-09-07 18:34:54 +0100

    (MVP - STYLES) : refactored the header component main div

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+import styled from 'styled-components';
+
+export const StyledHeader = styled.div`
+  width: 100%;
+  height: 120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
 
 const Header = () => {
   return (
-    <div className="header">
+    <StyledHeader>
       <span className="date">SMARCH 32, 2018</span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
-    </div>
+    </StyledHeader>
   )
 }
 
