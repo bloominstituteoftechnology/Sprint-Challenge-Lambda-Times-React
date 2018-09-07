1943fa906f559420b884fd3c378a88c76bdf600c
decagondev <decagondevelopment@gmail.com>
2018-09-07 17:15:35 +0100

    (MVP) :  maped over the tabs provided on the props, created a new Tab component for each one. gave the tab component a selectTabHandler, the selectedTab, and the 	ab itself as props

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,7 +6,10 @@ const Tabs = props => {
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+          props.tabs.map(tab, index => <Tab tab={tab} key={index} selectedTab={props.selectedTab} selectedTabHandler={props.selectedTabHandler} />)
+          // TODO: define Tab
+        }
       </div>
     </div>
   );
