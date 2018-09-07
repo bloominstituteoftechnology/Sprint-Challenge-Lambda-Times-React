00970e7b52718713084ba26043fc1f5697e8835b
Michael Agard <mdagard@gmail.com>
2018-09-07 13:20:24 -0400

    tab selector fixed

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,16 +4,12 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  // if (props.tab == props.selectedTab) {
-  //   return (tabClass = 'tab active-tab');
-  // } else {
-  //   return (tabClass = 'tab');
-  // }
+
   return (
     <div
-      className={'tab active-tab'}
-      onClick={event => {
-        props.selectTabHandler(event.target.value);
+      className={props.selectedTab === props.tab ? 'tab active-tab' : 'tab'}
+      onClick={() => {
+        props.selectTabHandler(props.tab);
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
