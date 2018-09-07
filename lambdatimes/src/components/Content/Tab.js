9b0b214759c0188e19be91606d9c387c3d3b7b2a
lyang9 <lyang9@buffalo.edu>
2018-09-07 14:01:42 -0400

    Deleted the single quotes on className line 16

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -13,7 +13,7 @@ const Tab = props => {
       }
   return (
     <div
-      className={'selectedClass'}
+      className={selectedClass}
       onClick={() => {props.selectTabHandler(props.tab)}}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
