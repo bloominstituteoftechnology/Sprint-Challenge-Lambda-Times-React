5e90d07b93e4dc70651e266d14c05c3ac75f79fd
Colin Preston <prestonresult@gmail.com>
2018-09-07 11:27:48 -0500

    tabs Working

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,12 +17,12 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
- this.setState({cards:cardData})
+ this.setState({cards:cardData, tabs : tabData},()=>{});
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-
+this.setState({selected:tab.target.innerText.toLowerCase()})
   };
 
   filterCards = () => {
@@ -38,7 +38,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-   if(this.state.selected = 'all'){
+   if(this.state.selected === 'all'){
     return this.state.cards;
   }
   return this.state.cards.filter(e => e.tab ===this.state.selected);
@@ -51,7 +51,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs selectedTab={this.state.selected} selectedTabHandler={this.changeSelected} tabs={this.state.tabs}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,17 +1,14 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      let selectedClass = 'tab'
+      if(props.selected === true)
+      selectedClass = 'tab active-tab';
   return (
-    <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
+    <div className={selectedClass} onClick={(props.selectTabHandler)}>
       {props.tab.toUpperCase()}
     </div>
   );
@@ -20,3 +17,8 @@ const Tab = props => {
 // Make sure you include PropTypes on your props.
 
 export default Tab;
+Tab.propTypes = {
+  selected : PropTypes.bool,
+  tab : PropTypes.string,
+  selectTabHandler : PropTypes.func
+}
