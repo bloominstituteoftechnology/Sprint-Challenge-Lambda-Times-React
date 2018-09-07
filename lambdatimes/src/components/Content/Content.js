3ac4427d9c2e4440bb0d684805406f68336b4e13
Andrew <ghobrial.andrew@gmail.com>
2018-09-07 08:40:04 -0700

    componentDidMount in Content.js complete

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,6 +18,8 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: tabData });
+    this.setState({ cards: cardData });
   }
 
   changeSelected = tab => {
