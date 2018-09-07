6aa337e6e264c8171f291f8e4c611b6ef2d9977e
Lndubose <lndubose13@gmail.com>
2018-09-07 11:26:27 -0500

    Tabs and cards are working

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Card from './Card';
 
 const Cards = props => {
   return (
     <div className="cards-container">
+      {props.cards.map(card => {
+        return <Card card={card}  key={card.headline} />
+      })}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
@@ -11,5 +16,15 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape({
+    card: PropTypes.shape({
+      headline: PropTypes.string,
+      tab: PropTypes.string,
+      img: PropTypes.string,
+      author: PropTypes.string,
+    })
+  }))
+}
 
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Tab from './Tab';
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+        {props.tabs.map((tab, index) => {
+          return <Tab 
+            tab={tab} 
+            key={index}
+            selectedTab={props.selectedTab}
+            selectedTabHandler={props.selectedTabHandler}
+          />
+        })}
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
@@ -13,4 +23,8 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
+
 export default Tabs;
