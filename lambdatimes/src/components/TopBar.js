762aa29a8af91470edead625d753e7ae865ffcaf
decagondev <decagondevelopment@gmail.com>
2018-09-07 18:08:22 +0100

    (MVP - STYLES) : refactored the topbars container in to a styled component

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+export const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
+    <StyledTopBar>
       <div className="container">
         <div className="container-left">
           <span>TOPICS</span><span>SEARCH</span>
@@ -17,7 +29,7 @@ const TopBar = () => {
           <span>LOG IN</span>
         </div>
       </div>
-    </div>
+    </StyledTopBar>
   )
 }
 
