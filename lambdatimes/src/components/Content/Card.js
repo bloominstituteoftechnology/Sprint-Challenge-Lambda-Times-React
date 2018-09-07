ac1905fa771872317b49bde648951f6e9c6162ca
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-07 13:48:35 -0500

    Answered comprehension questions in ANSWERS.md

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -9,7 +9,7 @@ const Card = props => {
         <div className="img-container">
           <img src={props.card.img} alt={'Card'} />
         </div>
-        <span>By {props.card.author}</span>
+        <span>By Changegit {props.card.author}</span>
       </div>
     </div>
   );
