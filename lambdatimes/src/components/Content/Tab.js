76a24b231e28f10bb5ed1817bbf4eac1e2d96367
Lucas Beemer <you@example.com>
2018-09-07 09:49:23 -0700

    I need to commit more

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
+  const isSelected = props.selectedTab === props.tab;
+  const selectedClass = 'tab'
+  const activeClass = 'tab active-tab'
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={isSelected ? activeClass : selectedClass}
+      onClick={props.handleTab}
     >
       {props.tab.toUpperCase()}
     </div>
-  );
-};
+  )
+}
 
-// Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  selectedTab: PropTypes.string,
+  tab: PropTypes.string,
+  handleTab: PropTypes.func
+}
 
 export default Tab;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,21 @@
 import React from 'react';
-
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+import {TopbarWrapper, TopbarContainer, TopbarInner, TopbarInnerContent} from './styles';
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopbarWrapper>
+      <TopbarContainer>
+        <TopbarInner left>
+          <TopbarInnerContent left>TOPICS</TopbarInnerContent><TopbarInnerContent left>SEARCH</TopbarInnerContent>
+        </TopbarInner>
+        <TopbarInner center>
+          <TopbarInnerContent center>GENERAL</TopbarInnerContent><TopbarInnerContent center>BROWNBAG</TopbarInnerContent><TopbarInnerContent center>RANDOM</TopbarInnerContent><TopbarInnerContent center>MUSIC</TopbarInnerContent><TopbarInnerContent center>ANNOUNCEMENTS</TopbarInnerContent>
+        </TopbarInner>
+        <TopbarInner right>
+          <TopbarInnerContent right>LOG IN</TopbarInnerContent>
+        </TopbarInner>
+      </TopbarContainer>
+    </TopbarWrapper>
   )
 }
 
