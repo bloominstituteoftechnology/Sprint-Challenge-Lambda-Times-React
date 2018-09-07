f390eb55413c7c827bef374c1c1da0c09cf0b95b
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2018-09-07 12:01:43 -0400

    Added props to Card, Mapped over cards to produce instances of Card

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,6 +6,12 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map((card, i)=> (
+            <Card
+              card={card}
+              key={i}
+            />
+          ))}
     </div>
   )
 }
