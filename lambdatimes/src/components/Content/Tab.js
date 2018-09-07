3e74bde57ef38c26ee6a644790fc01b7b3b4cfc2
ezra davis davis <ezra.rr.davis@gmail.com>
2018-09-07 11:15:51 -0700

    finished

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,21 +1,50 @@
 import React from 'react';
+import styled from "styled-components"
 
 const Tab = props => {
+  
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
-    <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
+    <TabContainer active ={props.selectedTab === props.tab}  onClick={(e) => props.selectTabHandler(e, props.tab)}>
       {props.tab.toUpperCase()}
-    </div>
+      
+    </TabContainer>
+    // <div
+    //   name={props.tab}
+    //   className ={props.selectedTab === props.tab && 'tab-active-tab'}       
+    //   onClick={(e) => props.selectTabHandler(e, props.tab)}
+    //     /* Replace this dummy click handler function with your selectTabHandler function from props 
+    //      you'll need to pass the `tab` in as an argument to this handler. */     
+    // >
+    //   {props.tab.toUpperCase()}
+    // </div>
   );
 };
+{ console.log(Tab)}
+
+const TabContainer = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;  
+  :hover{
+    text-decoration: underline;
+  }
+  ${props => props.active && 
+    `background-color: #fff;
+     color: #333;
+     border: 2px solid #333;`}
+`
 
 // Make sure you include PropTypes on your props.
 
