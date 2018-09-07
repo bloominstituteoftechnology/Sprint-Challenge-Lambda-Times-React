af3b9bf7515da7a1786183736c365e83a1120a3b
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-09-07 10:10:26 -0700

    adding tablogic into tab component

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,17 +4,33 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  if (props.tab === props.selectedTab) {
+    return (
+      <div
+        className={'tab active-tab'}
+        onClick={props.tab.selectTabHandler
+          /* Replace this dummy click handler function with your selectTabHandler function from props 
+           you'll need to pass the `tab` in as an argument to this handler. */
+        }
+      >
+        {props.tab.toUpperCase()}
+      </div>
+    )}
+
+    else {
+
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={'tab'}
+      onClick={props.tab.selectTabHandler
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      }
     >
       {props.tab.toUpperCase()}
     </div>
   );
+  }
 };
 
 // Make sure you include PropTypes on your props.
