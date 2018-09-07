b45768c5bcc33d335dd39b59a11c460e64e3141d
MikeRGarrity <mikergarrity@gmail.com>
2018-09-07 10:40:51 -0500

    initial commit, getting tabs and cards to display

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,6 +18,11 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ 
+      tabs: tabData.slice(),
+      cards: cardData.slice(),
+      selected: tabData[0]
+    })
   }
 
   changeSelected = tab => {
@@ -48,7 +53,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,18 +4,21 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      const isSelected = props.selectedTab === props.tab;
+      const selectedClass = 'tab'
+      const activeClass = 'tab active-tab'
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={isSelected ? activeClass : selectedClass}
+      onClick= {props.handleTab}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
+      >
       {props.tab.toUpperCase()}
     </div>
   );
 };
+
 
 // Make sure you include PropTypes on your props.
 
