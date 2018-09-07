4bdb879f47e48fbdbe078a8d49d8d77674eca6d0
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-07 12:57:28 -0500

    Completed Part III - Refactored TopBar and Header components' css into js  using styled components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const HeaderStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+      width: 1280px;
+    }
+`;
+
+const Date = styled.div`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const Temp = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
+const HeaderH1 = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderStyle>
+      <Date>SMARCH 32, 2018</Date>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <Temp>98°</Temp>
+    </HeaderStyle>
   )
 }
 
-export default Header
+export default Header;
