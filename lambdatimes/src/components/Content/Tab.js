65b61e7647ebc5ada79995941873ec6861d10724
lyang9 <lyang9@buffalo.edu>
2018-09-07 12:27:52 -0400

    Changed const to let on line 8

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,7 +5,7 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-      const selectedClass = 'tab';
+      let selectedClass = 'tab';
       if (props.tab === props.selectedTab) {
         selectedClass = 'tab active-tab';
       } else {
