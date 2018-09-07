1f8df08cc7dda8cdac49c631850f4a1e0f92312e
BrianARuff <thelonewolf198927@gmail.com>
2018-09-07 12:19:45 -0400

    Cards.js

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -44,14 +44,16 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+
+    tab = this.state.selected;
+    
     if(this.state.selected === 'all'){
       return this.state.cards;
-    } else {
-      return this.state.cards.filter(card => card.tab === tab); 
     }
   };
 
   render() {
+    console.log(this.state.cards);
     return (
       <div className="content-container">
         {/* 
