7977fdbc6f6fcbf07f19ee16b21fdfce83b2a648
decagondev <decagondevelopment@gmail.com>
2018-09-07 17:47:51 +0100

    (MVP) : Using the cards prop, maped over the list creating a new Card component for each passing the card as the only prop

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
@@ -21,5 +21,8 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string
+};
 
 export default Tab;
