0ea734a4ebe5e0f56798474c1f59585278a48628
Andrew <ghobrial.andrew@gmail.com>
2018-09-07 09:49:19 -0700

    Cards.js map function complete, may need some improvement in formatting

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -71,11 +71,6 @@ export default class Content extends Component {
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs 
         tabs={this.state.tabs} 
         selectedTab={this.state.selected} 
