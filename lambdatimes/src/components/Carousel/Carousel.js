a4ea48b0b7eca4d39871f24c9e3e4fb37308047e
Austinlp4 <apendergrast44@gmail.com>
2018-09-07 12:37:36 -0500

    Finished Project, working on stretch

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,11 +5,11 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      carousel: []
     }
   }
   componentDidMount(){
-
+     this.setState({carousel: carouselData});
   }
 
   leftClick = () => {
@@ -21,13 +21,14 @@ export default class Carousel extends Component {
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={carouselData[0]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        <this.selectedImage />
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
