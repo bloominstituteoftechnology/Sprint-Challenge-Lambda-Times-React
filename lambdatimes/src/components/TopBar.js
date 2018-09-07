82212c15e60c71312438b651ccf7065008894ee7
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-07 13:55:08 -0400

    Finished styling TopBar.js with styled components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,103 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <TopBarWrapper>
+      <Container>
+        <ContainerLeft>
+          <LeftSpan>TOPICS</LeftSpan>
+          <LeftSpan>SEARCH</LeftSpan>
+        </ContainerLeft>
+        <ContainerCenter>
+          <CenterSpan>GENERAL</CenterSpan>
+          <CenterSpan>BROWNBAG</CenterSpan>
+          <CenterSpan>RANDOM</CenterSpan>
+          <CenterSpan>MUSIC</CenterSpan>
+          <CenterSpan last>ANNOUNCEMENTS</CenterSpan>
+        </ContainerCenter>
+        <ContainerRight>
+          <RightSpan>LOG IN</RightSpan>
+        </ContainerRight>
+      </Container>
+    </TopBarWrapper>
+  );
+};
 
 export default TopBar;
+
+const TopBarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const Span = styled.span`
+  cursor: pointer;
+`;
+
+const LeftSpan = styled(Span)`
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+const CenterSpan = styled(Span)`
+  margin-right: ${(props) => (props.last ? '0' : '5%')};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const RightSpan = styled(Span)``;
