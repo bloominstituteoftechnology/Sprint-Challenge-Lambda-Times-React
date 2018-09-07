900ef5ee85271331901d95134c1b8df0648b09b3
Timothy Hoang <timh1203@gmail.com>
2018-09-07 13:55:41 -0400

    Add carousel sc stylings

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import { carouselData } from '../../data'
 // Complete this Carousel 
 export default class Carousel extends Component {
@@ -21,15 +22,78 @@ export default class Carousel extends Component {
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <Img src={} style={{display: 'block'}} />
   }
   
   render(){
     return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
+      <Div1>
+        <Div2 onClick={this.leftClick}>{"<"}</Div2>
+        <Div3 onClick={this.rightClick}>{">"}</Div3>
+      </Div1>
     )
   }
 }
+
+const Div1 = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`
+const Div2 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  :hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+  top: 50%;
+  left: 25px;
+  transform: translate(0, -50%);
+`
+const Div3 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  :hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+  top: 50%;
+  left: 25px;
+  transform: translate(0, -50%);
+`
+const Img = styled.img`
+  width: 100%;
+  display: none;
+`
