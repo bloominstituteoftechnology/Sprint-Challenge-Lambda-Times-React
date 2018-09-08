9ab62cf70512227dfbb1e4b2c26114fb530a3314
lyang9 <lyang9@buffalo.edu>
2018-09-08 00:32:15 -0400

    Fixed the if/else statement

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,15 +5,12 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-      let selectedClass = 'tab';
-      if (props.tab === props.selectedTab) {
-        selectedClass = 'tab active-tab';
-      } else {
-        selectedClass = 'tab';
-      }
+      const isActive = props.tab === props.selectedTab;
   return (
     <div
-      className={'tab'}
+      className={`tab ${isActive ? 'active-tab': ''}`}
+      // or this way
+      // className={`tab ${isActive && 'active-tab'}`}
       onClick={() => props.selectTabHandler(props.tab)}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
