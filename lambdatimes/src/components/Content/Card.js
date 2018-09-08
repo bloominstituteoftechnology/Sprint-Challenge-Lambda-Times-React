33ae56cd20dad0472144a5f4657ad144e75ca74c
lyang9 <lyang9@buffalo.edu>
2018-09-08 02:26:15 -0400

    Destructor props.card for DRY code

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = props => {
+  const {
+    headline, // save as const headline = props.card.headline
+    img,
+    author,
+  } = props.card;
+
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */ props.card.headline}</div>
+      <div className="headline">{/* headline goes here */ headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.card.img /* image source goes here */} className='card-img' alt='card image'/>
+          <img src={img /* image source goes here */} className='card-img' alt='card image'/>
         </div>
-        <span>By {/* author goes here */ props.card.author}</span>
+        <span>By {/* author goes here */ author}</span>
       </div>
     </div>
   );
