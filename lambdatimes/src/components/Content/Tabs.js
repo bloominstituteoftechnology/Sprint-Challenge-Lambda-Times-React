e54f8453765533ebad57ef8670c205983efdfeea
Timothy Hoang <timh1203@gmail.com>
2018-09-08 10:01:13 -0400

    Add conditional render with sc

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import Tab from './Tab'
 
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <Div1>
+      <Div2>
+        <Span>TRENDING TOPICS:</Span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
           {
@@ -19,10 +20,34 @@ const Tabs = props => {
               />
             })
           }
-      </div>
-    </div>
+      </Div2>
+    </Div1>
   )
 }
+const Div1 = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`
+const Div2 = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`
+const Span = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`
 
 // Make sure to use PropTypes to validate your types!
 Tabs.propTypes = {
