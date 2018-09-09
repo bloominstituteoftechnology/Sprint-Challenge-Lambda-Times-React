fbe17b28477681e15615ed78f7830b6622325310
lyang9 <lyang9@buffalo.edu>
2018-09-09 00:15:52 -0400

    Fixed className Date and Temp css

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -19,22 +19,30 @@ const HeaderStyled = styled.div `
     @media (min-width: 1280px) {
       width: 1280px;
     }
-    h1 {
-      font-size: 60px;
-      font-family: Didot, serif;
-      font-weight: bold;
-      flex: 8;
-      text-align: center;
-      color: #000;
-    }
 `;
 
-const Date = styled.span `
+const HeaderH1 = styled.h1 `
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const HeaderSpan = styled.span `
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+const DateSpan = styled(HeaderSpan) `
   margin-left: 25px;
   flex: 1;
 `;
 
-const Temp = styled.span `
+const TempSpan = styled(HeaderSpan) `
   text-align: right;
   margin-right: 25px;
   flex: 1;
@@ -43,9 +51,9 @@ const Temp = styled.span `
 const Header = () => {
   return (
     <HeaderStyled>
-      <Date>MARCH 32, 2018</Date>
-      <h1>Lambda Times</h1>
-      <Temp>98°</Temp>
+      <DateSpan >MARCH 32, 2018</DateSpan>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <TempSpan>98°</TempSpan>
     </HeaderStyled>
   )
 }
