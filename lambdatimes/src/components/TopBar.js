46125e9352244ac0da6e74ecf639cef0a62b21ec
Anthony Greb <agreb255@gmail.com>
2018-09-10 22:48:51 -0400

    OMG I DID IT! sorry if I went a bit over

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,12 +1,35 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  `;
+  const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
 
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+  `;
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
+    <Header>
+      <Container>
         <div className="container-left">
           <span>TOPICS</span><span>SEARCH</span>
         </div>
@@ -16,8 +39,8 @@ const TopBar = () => {
         <div className="container-right">
           <span>LOG IN</span>
         </div>
-      </div>
-    </div>
+        </Container>
+    </Header>
   )
 }
 
