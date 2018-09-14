0f9967dc9de41117dfaeb2170320e6af3a1ba66c
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-09-14 11:27:56 -0500

    Task 1 Complete
    Everything rendered. Nothing functional.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,8 +4,10 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {/* Using the cards prop, map over the list creating a
+          new Card component for each passing the card as the only prop*/
+          props.cards.map(item => <Card card={item} />)
+      }
     </div>
   )
 }
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,7 +6,11 @@ const Tabs = props => {
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+            props.tabs.map(item => <Tab tab={item}
+                                        selectedTabHandler={props.selectedTabHandler}
+                                        selectedTab={props.selectedTab} />)
+        }
       </div>
     </div>
   );
