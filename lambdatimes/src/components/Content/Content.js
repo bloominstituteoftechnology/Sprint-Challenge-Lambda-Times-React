be79a090c7deb4fc31ce57f9f007f0f7f570bccc
Brandon White <bwhite06@stac.edu>
2018-09-14 14:23:05 -0400

    last commit before lunch

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -21,6 +21,7 @@ this.setState(
   {
     cards: cardData,
     tabs: tabData,
+
 });
   }
 
@@ -37,7 +38,40 @@ this.setState({
     // this function should take in the tab and update the state with the new tab.
   };
 
-  filterCards = () => {
+  filterCards = (tab) => {
+    let s = this.state.selected
+    console.log(this.state.cards.filter(function (n,i){
+        return n.tab==="React.js";
+    }));
+
+    if(this.state.selected==="all"){
+      return this.state.cards;
+    }
+    if(this.state.selected==="javascript"){
+      return this.state.cards.filter(function (n,i){
+          return n.tab==="javascript";
+      });
+    }
+    if(this.state.selected==="technology"){
+      return this.state.cards.filter(function (n,i){
+          return n.tab==="technology";
+      });
+    }
+    if(this.state.selected==="bootstrap"){
+      return this.state.cards.filter(function (n,i){
+          return n.tab==="bootstrap";
+      });
+    }
+    if(this.state.selected==="React.js"){
+      return this.state.cards.filter(function (n,i){
+          return n.tab==="React.js";
+      });
+    }
+    if(this.state.selected==="node"){
+      return this.state.cards.filter(function (n,i){
+          return n.tab==="node";
+      });
+    }
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
@@ -50,7 +84,7 @@ this.setState({
           of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+
   };
 
   render() {
