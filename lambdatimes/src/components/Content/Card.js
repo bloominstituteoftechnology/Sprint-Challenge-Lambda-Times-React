512047f85c2bc98cdcd9d0e0047e4142feda6445
Matthew Wright <matthewbwright@gmail.com>
2018-09-14 13:57:17 -0700

    completes the card and cards js

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -4,18 +4,19 @@ import PropTypes from 'prop-types';
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{props.cardData.headline}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.cardData.img} />
+          <img src={props.card.img} />
         </div>
-        <span>By {props.cardData.author}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
 };
 
-// Make sure to include PropTypes.
+/*Make sure to include PropTypes.*/
+
 Card.propTypes = {
   headline: PropTypes.string,
   img: PropTypes.string,
