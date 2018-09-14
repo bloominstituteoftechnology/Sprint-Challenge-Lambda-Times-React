0867c4611daac54dc77c84be955934e02be04741
Brandon White <bwhite06@stac.edu>
2018-09-14 12:39:05 -0400

    got cards and tabs to appear

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
+
+let classname = '';
+  if(props.tab===props.selectedTab){
+  classname = 'tab active-tab';
+  } else{
+    classname = 'tab';
+  }
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
+      if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
+      className={`${classname}`}
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+        /* Replace this dummy click handler function with your selectTabHandler function from props
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
     >
