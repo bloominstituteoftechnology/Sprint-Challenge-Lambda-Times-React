42f4047bf21c06e3c3d3a7d50695ea444bee4575
Matthew Wright <matthewbwright@gmail.com>
2018-09-14 09:08:16 -0700

    sets up card js

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -55,7 +55,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selectedTab}
+          selecTabHandler={this.state.selecTabHandler}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
