21386effd5ae07c9e018f7d073797e102bd31e8d
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-09-14 11:33:48 -0500

    cleaned up instructions

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-
 import Tabs from './Tabs';
 import Cards from './Cards';
-
-// Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
 
 export default class Content extends Component {
@@ -23,7 +20,6 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     this.setState({selected: tab})
-    // this function should take in the tab and update the state with the new tab.
   };
 
   filterCards = () => {
@@ -45,11 +41,6 @@ export default class Content extends Component {
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
