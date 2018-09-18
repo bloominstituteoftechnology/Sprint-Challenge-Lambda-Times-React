1c6f7175e270ee40a4ed74b09c41fc48dbfb3fc1
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-09-17 22:19:26 -0500

    fixed css issue of tab styling

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../CSS/index.css';
+
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      
   return (
     <div
-      className={props.tab === props.selectedTab ? 'active-tab': ''}
+      className={props.tab === props.selectedTab ? 'tab active-tab': 'tab'}
       onClick={() => props.selectTabHandler(props.tab)}
     >
       {props.tab.toUpperCase()}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Tab from './Tab';
 import PropTypes from 'prop-types';
+import '../../CSS/index.css';
 
 const Tabs = props => {
   return (
     <div className="tabs">
-      <div className="topics">
+      <div className="topics">  
         <span className="title">TRENDING TOPICS:</span>
         {props.tabs.map(tab => <Tab key={tab} tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} />)}
       </div>
     </div>
   );
-};
+}; 
 
 // Make sure to use PropTypes to validate your types!
 Tabs.propTypes = {
