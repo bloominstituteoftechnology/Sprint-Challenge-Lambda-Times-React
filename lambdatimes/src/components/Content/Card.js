bd68c33fbd0bce866331b9258cff59475834ce99
Lajawanti <lajawanti.dhake@gmail.com>
2018-10-12 14:41:39 -0400

    cards with images not filtered

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,5 +1,22 @@
 import React from 'react';
 //import {bones, fido, max, puppers , sir} from '.../assets/carousel'
+import styled from 'styled-components';
+
+const Carddiv = styled.div`
+    padding: 7px 12px;
+    margin: 8px;
+    border: none;
+    background : black;
+    color: white;
+    font-weight : bold;
+`;
+
+const Headlinediv = styled.div``;
+
+const Authordiv = styled.div``;
+
+const ImgContainerdiv = styled.div ``;
+
 
 const Card = props => {
   console.log("In Card-component : ",props);
@@ -8,7 +25,7 @@ const Card = props => {
       <div className="headline">{/* headline goes here */props.cardToDisplay.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'{props.cardToDisplay.img}' /* image source goes here */} />
+          <img alt = "image" src={props.cardToDisplay.img /* image source goes here */} />
         </div>
         <span>By {/* author goes here */props.cardToDisplay.author}</span>
       </div>
