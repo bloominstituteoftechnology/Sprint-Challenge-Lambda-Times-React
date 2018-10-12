7f4ab2f1b7942c779140f11c380bdb8a20512482
Unknown <zachary.arney@gmail.com>
2018-10-12 13:00:43 -0500

    Converted CSS fro TopBar to styled-components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import { StyledTopBar, TopBarContainer, TopBarContainerLeft, TopBarContainerLeftSpan, TopBarContainerCenter, TopBarContainerCenterSpan, TopBarContainerRight, TopBarContainerRightSpan,  } from './TopBarStyles';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <StyledTopBar>
+      <TopBarContainer>
+        <TopBarContainerLeft>
+          <TopBarContainerLeftSpan>TOPICS</TopBarContainerLeftSpan>
+          <TopBarContainerLeftSpan>SEARCH</TopBarContainerLeftSpan>
+        </TopBarContainerLeft>
+        <TopBarContainerCenter>
+          <TopBarContainerCenterSpan>GENERAL</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>BROWNBAG</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>RANDOM</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>MUSIC</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>ANNOUNCEMENTS</TopBarContainerCenterSpan>
+        </TopBarContainerCenter>
+        <TopBarContainerRight>
+          <TopBarContainerRightSpan>LOG IN</TopBarContainerRightSpan>
+        </TopBarContainerRight>
+      </TopBarContainer>
+    </StyledTopBar>
+  );
+};
 
 export default TopBar;
