0ecb1b93ae2410d6af55dc2692114b4ca50c90d5
TRIF3X <thewizardofcrypto@gmail.com>
2018-10-12 10:57:49 -0500

    included proptypes in cards

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Card = props => {
   return (
@@ -15,5 +16,14 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
+
+  Card.proptypes = {
+    card: PropTypes.shape({
+      author: PropTypes.string,
+      headline: PropTypes.string,
+      img: PropTypes.string,
+      tab: PropTypes.string
+    })
+  }
 
 export default Card;
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types'
 
 const Cards = props => {
   return (
@@ -8,7 +9,7 @@ const Cards = props => {
           new Card component for each passing the card as the only prop*/}
           {props.cards.map(card => <Card 
           card={card} 
-          key={card.tab} 
+          // key={card.tab} 
           />
           )}
     </div>
@@ -16,5 +17,14 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+
+  Cards.proptypes = {
+    cards: PropTypes.shape({
+      author: PropTypes.string,
+      headline: PropTypes.string,
+      img: PropTypes.string,
+      tab: PropTypes.string
+    })
+  }
 
 export default Cards;
