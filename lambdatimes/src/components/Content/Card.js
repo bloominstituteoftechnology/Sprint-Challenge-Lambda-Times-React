83c47afaf2c3f22ddc5624b686fcbabfa7d404b0
bummings <eabummings@gmail.com>
2018-10-12 12:47:55 -0400

    just about done with Card.js, needs proptypes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
+    <div className='card'>
+      <div className='headline'>{props.card.headline}</div>
+      <div className='author'>
+        <div className='img-container'>
+          <img src={props.card.img} alt='Featured article' />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
