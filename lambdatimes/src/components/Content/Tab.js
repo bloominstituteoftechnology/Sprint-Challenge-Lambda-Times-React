837b6f2d690ab13c0057aa730804b694620f3ee2
BryanGf <briangf@uw.edu>
2018-10-12 09:27:20 -0700

    functionality

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,15 +4,23 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+  let active = '';
+  if(props.tabs === props.selectedTab) {
+    active = 'tab active-tab';
+  } else {
+    active = 'tab';
+  }
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={active}
+      onClick={event => {
+        props.selectTabHandler(event, props.tabs);
+      }}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
     >
-      {props.tab.toUpperCase()}
+      {props.tabs.toUpperCase()}
     </div>
   );
 };
