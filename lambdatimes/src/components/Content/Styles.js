c3788106f9a97c1f2340bc579558b1eee0622c8b
Adam <ajmboosh@gmail.com>
2018-10-12 11:29:22 -0500

    finished converting Header component to styled-component. commented out relevant css to prove it works

---
+++ lambdatimes/src/components/Content/Styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+//Header
+export const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+  /* date span */
+
+  /* h1 */
+
+  /* temp span*/
+`;
+
+export const StyledSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  flex: 1;
+
+  ${props => (props.date ? `margin-left:25px` : `margin-right:25px`)};
+  ${props => (props.temp ? `text-align:right` : null)};
+`;
