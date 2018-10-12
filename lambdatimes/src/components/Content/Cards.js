31a9e315076a8a2f0b26cdbfb4670ee6ee0b2b04
Sungyun Won <seanwon8@gmail.com>
2018-10-12 11:08:51 -0700

    Sean

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map((card)=> (
+            <Card card={card}/>
+          ))}
     </div>
   )
 }
 
-// Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  card: PropTypes.object
+}
 
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +9,19 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((tab, i) => (
+          <Tab key={i} tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} />
+        ))}
       </div>
     </div>
   );
 };
+
+Tabs.propTypes = {
+  tab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
+}
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
