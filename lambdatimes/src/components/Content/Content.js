f01b7aca06ae8f05a65b39a44852ebd7e8575cdd
Ryan Boris <neodynia@icloud.com>
2018-10-12 12:31:58 -0400

    Adds filter functionality for cards based on selected tab

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -26,9 +26,9 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    this.setState( tab => ({
+    this.setState({
       selected: tab
-    }))
+    })
   };
 
   filterCards = () => {
@@ -44,7 +44,12 @@ export default class Content extends Component {
           of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === 'all') {
+      return this.state.cards;
+    } else {
+      return this.state.cards.filter( card => card.tab === this.state.selected);
+    }
+
   };
 
   render() {
