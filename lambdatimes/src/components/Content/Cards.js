8f20ec5cb1d66b3e11e311f41319f6f9034df262
Justin <jdlow2@gmail.com>
2018-10-12 11:48:16 -0400

    passed props to Cards and Card. coming back to do propTypes later.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
+      // {/* Using the cards prop, map over the list creating a 
+      //     new Card component for each passing the card as the only prop*/}
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+          {props.cards.map((c, i)=> (
+            <Card
+            key={i}
+            card={c}
+            />
+          ))}
     </div>
   )
 }
