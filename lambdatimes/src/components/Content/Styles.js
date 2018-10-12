d1ed0973e5f2570f3c708e552ac2987e461b675a
Adam <ajmboosh@gmail.com>
2018-10-12 11:44:59 -0500

    MVP complete, TopBar styles converted to styled-components. commented out CSS to prove it works. going to work on stretch

--- lambdatimes/src/components/Content/Styles.js
+++ lambdatimes/src/components/Content/Styles.js
@@ -24,11 +24,6 @@ export const StyledHeader = styled.div`
     text-align: center;
     color: #000;
   }
-  /* date span */
-
-  /* h1 */
-
-  /* temp span*/
 `;
 
 export const StyledSpan = styled.span`
@@ -40,4 +35,80 @@ export const StyledSpan = styled.span`
 
   ${props => (props.date ? `margin-left:25px` : `margin-right:25px`)};
   ${props => (props.temp ? `text-align:right` : null)};
+`;
+
+//TopBar
+export const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+export const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+export const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+
+  span {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+`;
+
+export const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+
+  span {
+    cursor: pointer;
+    margin-right: 5%;
+  }
+  span:last-child {
+    margin-right: 0;
+  }
+  span:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+
+  span {
+    cursor: pointer;
+  }
 `;
