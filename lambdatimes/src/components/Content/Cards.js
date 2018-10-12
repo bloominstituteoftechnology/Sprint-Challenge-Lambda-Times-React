0c447258f0ba69dfae420446a96b745dd20d82ff
bummings <eabummings@gmail.com>
2018-10-12 12:35:21 -0400

    added proptypes to Cards.js

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
+import PropTypes from 'prop-types';
+
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
 
-      {props.cards.map(card => <Card key = {card} card = {card} />)}
+      {props.cards.map(card => <Card key={card} card={card} />)}
     </div>
   )
 }
@@ -15,5 +17,14 @@ const Cards = props => {
 console.log({Cards});
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  card: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    headline: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    tab: PropTypes.string.isRequired
+  })
+}
 
 export default Cards;
