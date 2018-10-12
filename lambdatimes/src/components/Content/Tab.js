0222302b98b230795db01aee1744b2667a58a9fa
Justin <jdlow2@gmail.com>
2018-10-12 12:04:44 -0400

    added PropTypes to Tab and Tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
       
-      // {/* className={''} */}
-      console.log(props.tab);
   return (
     <div
       className={`tab ${props.tab === props.selectedTab ? 'active-tab' : null}`}
@@ -23,3 +22,8 @@ const Tab = props => {
 // Make sure you include PropTypes on your props.
 
 export default Tab;
+Tab.propTypes = {
+  tab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string
+}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 const Tabs = props => {
-  console.log(props.tabs)
         // {/* map over the tabs provided on your props, create a new Tab component for each one.
         //     give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
   return (
@@ -23,3 +23,12 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.string
+  ),
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func
+}
+
