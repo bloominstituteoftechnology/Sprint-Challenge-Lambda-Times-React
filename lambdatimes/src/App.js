813e4d931e68584bd6329b7cd4ec3a877fc23acc
Blake <koenneckerb@gmail.com>
2018-10-12 14:55:52 -0700

    Changed where the carousel was placed in the page to be in line with the directions in README.md

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import TopBar from './components/TopBar';
-import Header from './components/Header';
-import Content from './components/Content/Content';
-import Carousel from "./components/Carousel/Carousel"
+import React from "react";
+import TopBar from "./components/TopBar";
+import Header from "./components/Header";
+import Content from "./components/Content/Content";
 
 const App = () => {
   return (
     <div className="App">
       <TopBar />
       <Header />
-      <Carousel />
       <Content />
     </div>
   );
-}
+};
 
 export default App;
