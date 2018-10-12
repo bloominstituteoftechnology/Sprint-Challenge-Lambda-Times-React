e062665ba03baefc766b77529561215339b83658
Adam <ajmboosh@gmail.com>
2018-10-12 13:01:21 -0500

    added onClick function for TopBar's  span that removes username from localstorage and forces a reload, taking user to login page

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -10,27 +10,36 @@ import {
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
-const TopBar = () => {
-  return (
-    <StyledTopBar>
-      <TopBarContainer>
-        <ContainerLeft>
-          <span>TOPICS</span>
-          <span>SEARCH</span>
-        </ContainerLeft>
-        <ContainerCenter>
-          <span>GENERAL</span>
-          <span>BROWNBAG</span>
-          <span>RANDOM</span>
-          <span>MUSIC</span>
-          <span>ANNOUNCEMENTS</span>
-        </ContainerCenter>
-        <ContainerRight>
-          <span>LOG IN</span>
-        </ContainerRight>
-      </TopBarContainer>
-    </StyledTopBar>
-  );
-};
+class TopBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  logout = () => {
+    localStorage.removeItem("username");
+    window.location.reload();
+  };
+  render() {
+    return (
+      <StyledTopBar>
+        <TopBarContainer>
+          <ContainerLeft>
+            <span>TOPICS</span>
+            <span>SEARCH</span>
+          </ContainerLeft>
+          <ContainerCenter>
+            <span>GENERAL</span>
+            <span>BROWNBAG</span>
+            <span>RANDOM</span>
+            <span>MUSIC</span>
+            <span>ANNOUNCEMENTS</span>
+          </ContainerCenter>
+          <ContainerRight>
+            <span onClick={this.logout}>LOG IN</span>
+          </ContainerRight>
+        </TopBarContainer>
+      </StyledTopBar>
+    );
+  }
+}
 export default TopBar;
