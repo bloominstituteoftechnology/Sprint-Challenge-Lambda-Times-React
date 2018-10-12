7ae2d8fd60c0b1c4226ca7146648ddb828889d84
MEBonham <programmer@mckaybonham.com>
2018-10-12 10:45:19 -0600

    Cleaned up commented lines that were for debugging

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -43,15 +43,10 @@ export default class Content extends Component {
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
     if (this.state.selected === "all") {
-      // this.setState({ cards: cardData });
-      // console.log("all", this.state.cards);
       return this.state.cards;
     } else {
-      // this.setState({ cards: cardData.filter(card => card.tab === this.state.selected) });
-      // console.log(this.selected, this.state.cards);
       return this.state.cards.filter(card => card.tab === this.state.selected);
     }
-    // return this.state.cards;
   };
 
   render() {
