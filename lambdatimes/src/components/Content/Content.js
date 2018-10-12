ff68e4be8a8455fe133c1dc88bdd7a21ee92e0d4
bkwillia052 <bkwillia052@gmail.com>
2018-10-12 11:27:44 -0500

    Cards switch functional

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -41,7 +41,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+   let cardsCopy = [...this.state.cards];
+   if(this.state.selected === 'all'){
+     return this.state.cards
+   }
+   else{
+    return cardsCopy.filter(card => card.tab === this.state.selected);
+   }
+    
   };
 
   render() {
