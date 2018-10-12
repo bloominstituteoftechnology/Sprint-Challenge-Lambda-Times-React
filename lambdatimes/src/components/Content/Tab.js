efa2ab4c2a390081c3a7f67d5864a2e9e88da92a
sarahtennis <sarahtennis@Sarahs-Air.fios-router.home>
2018-10-12 11:59:05 -0400

    implements tab filter functionality

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
   return (
     <div
       className={(props.tab !== props.selected) ? "tab active-tab" : "tab" }
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-         props.selectTabHandler(props.tab)
+         props.changeSelected(props.tab)
       }}
     >
       {props.tab.toUpperCase()}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,9 +5,7 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-        {props.tabs.map((tab) => <Tab tab={tab} selectTabHandler={props.selectTabHandler} selected={props.selected} />)}
+        {props.tabs.map((tab, index) => <Tab tab={tab} changeSelected={props.changeSelected} selected={props.selected} key={index}/>)}
       </div>
     </div>
   );
