5327ede5636803f7f17411cad447d6c25ef8c24d
TRIF3X <thewizardofcrypto@gmail.com>
2018-10-12 10:18:51 -0500

    Finished answers

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,6 +18,7 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: tabData, cards: cardData })
   }
 
   changeSelected = tab => {
@@ -48,7 +49,9 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+        tabs={this.state.tabs} 
+        selectedTab={this.state.selected} selectTabHandler={this.tabHandler} />
         <Cards cards={this.filterCards()} />
       </div>
     );
