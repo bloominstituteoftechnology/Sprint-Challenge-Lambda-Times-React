048a9b8c42c3f6190b1f5c466fba0ec91e0195a4
Edward J <edjeong99@gmail.com>
2018-10-12 11:41:43 -0400

    implement card

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
-// test
+
 
 const App = () => {
   return (
