f28f934ee25a1a2459c17012c8c976e60dd73cfd
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-12 15:38:10 -0400

    tabs formatting

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Tab from './Tab';
+
+const TabComponent = props => {
+  return(<button>{props.name}</button>)
+}
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+        <div>
+  {props.tabs.map(tab => <Tab selectTabHandler = {this.selectTabHandler} selectedTab = {this.selectedTab} tab= {tab}></Tab>)}
+        </div>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
