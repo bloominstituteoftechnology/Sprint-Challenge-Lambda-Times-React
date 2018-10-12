6168501576e1a3db19e1e21e3a3aa3d21e95f631
Riley-Brown <riley@riley.gg>
2018-10-12 12:53:00 -0400

    Added proptype checking for all data

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -47,7 +47,7 @@ export default class Content extends Component {
     if (this.state.selected === "all") {
       return this.state.cards;
     } else {
-      return this.state.cards.filter(card => card.tab == this.state.selected);
+      return this.state.cards.filter(card => card.tab === this.state.selected);
     }
 
     // return this.state.cards;
