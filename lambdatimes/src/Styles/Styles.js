ff753bcb41d6e692a26515241a7f75a4cf4b678d
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-12 12:19:19 -0500

    header converted

---
+++ lambdatimes/src/Styles/Styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import React from 'react';
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px
+`;
+
+export const HeaderTitle = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+export const HeaderSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: ${props => (props.date ? '25px' : null)};
+  flex: ${props => (props.flex ? '1' : null)};
+  text-align: ${props => (props.temp ? 'right' : null)};
+  margin-right: ${props => (props.temp ? '25px' : null)};
+`;
+// .header span {
+//   align - self: flex - end;
+//   font - size: 11px;
+//   font - weight: bold;
+//   letter - spacing: 1px;
+// }
+// .header.date {
+//   margin - left: 25px;
+//   flex: 1;
+// }
+// .header.temp {
+//   text - align: right;
+//   margin - right: 25px;
+//   flex: 1;
+// }
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { StyledHeader, HeaderTitle, HeaderSpan } from '../Styles/Styles';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+    <StyledHeader>
+      <HeaderSpan date flex>
+        SMARCH 32, 2018
+      </HeaderSpan>
+      <HeaderTitle>Lambda Times</HeaderTitle>
+      <HeaderSpan temp flex>
+        98°
+      </HeaderSpan>
+    </StyledHeader>
+  );
+};
 
-export default Header
+export default Header;
+
+// <div className="header">
+//   <span className="date">SMARCH 32, 2018</span>
+//   <h1>Lambda Times</h1>
+//   <span className="temp">98°</span>
+// </div>
