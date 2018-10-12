67d1adf6b86303d3ec22fc453d35b81ccf80d6bd
Justin <jdlow2@gmail.com>
2018-10-12 13:11:37 -0400

    switched filterCards to ternary operator

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -39,12 +39,9 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    if (this.state.selected === 'all') {
-      return this.state.cards;
-    }
-    else {
-      return this.state.cards.filter(c => c.tab === this.state.selected)
-    }
+    return this.state.selected === "all"
+      ? this.state.cards
+      : this.state.cards.filter(c => c.tab === this.state.selected);
   };
 
   render() {
@@ -55,10 +52,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} 
-              selectedTab={this.state.selected} 
-              selectTabHandler={this.changeSelected}
-              />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
