e12e26687953a961c1c9a6532cc6007745ce0ce4
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-12 11:44:10 -0600

    Finished MVP

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -46,7 +46,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    // return this.state.cards;
+    
   };
 
   render() {
