8cd9bb5aa6523049aea39b0c8be21fd7a6e02516
Rahul Desai <rd825@georgetown.edu>
2018-10-12 13:27:58 -0400

    Deployed to netlify and converted App.js CSS to styled componenents

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -5,6 +5,13 @@ import Cards from './Cards';
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
 import Carousel from '../Carousel/Carousel';
+import styled from 'styled-components';
+
+let ContentContainer = styled.div`
+  display: flex; 
+  flex-direction: column; 
+  align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -57,7 +64,7 @@ export default class Content extends Component {
 
   render() {
     return (
-      <div className="content-container">
+      <ContentContainer>
         {/* DONE: 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
@@ -66,7 +73,7 @@ export default class Content extends Component {
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Carousel/>
         <Cards cards={this.filterCards()} />
-      </div>
+      </ContentContainer>
     );
   }
 }
