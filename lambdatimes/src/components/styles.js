a5f0ceaf76157a964615c8b4e79ddaac11ff5702
cembreyfarquhar <cembreyfarquhar@gmail.com>
2018-10-12 11:52:30 -0500

    TopBar refactored to styled components

---
+++ lambdatimes/src/components/styles.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+export const StyledContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  width: 1280px;
+`;
+
+export const StyledContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+export const StyledContainerLeftSpan = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+export const StyledContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+export const StyledContainerCenterSpan = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+`;
+export const StyledContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+export const StyledContainerRightSpan = styled.span`
+    cursor: pointer;
+`
