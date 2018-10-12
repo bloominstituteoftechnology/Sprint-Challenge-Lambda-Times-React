4528ed6b3a2343d96aa2584d5bd46a63c9212612
Rahul Desai <rd825@georgetown.edu>
2018-10-12 11:42:24 -0400

    Completed Tabs and Tab file functionality and prop types

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
+        {/* DONE: map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((tab, i) => <Tab key={i} tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler}/> )}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+// DONE: Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired,
+};
+
 export default Tabs;
