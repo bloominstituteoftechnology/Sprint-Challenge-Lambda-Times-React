275b6ed0f09ff20ec3bb21cf70befcc4de268907
frankbenevento <fb.mentoslabs@gmail.com>
2018-10-12 10:33:04 -0700

    more styled components. MVP achieved, moving on

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,94 @@
 import React from 'react';
+import Styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+const LeftContainer = Styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`
+const LeftSpan = Styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`
+const RightContainer = Styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`
+const RightSpan = Styled.span`
+  cursor: pointer;  
+`
+const CenterContainer = Styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`
+const CenterSpan = Styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+
+  :last-child {
+    margin-right: 0;
+  }
+
+  :hover {
+    text-decoration: underline; 
+  }
+`
+const Container = Styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    .top-bar .container {
+      width: 1280px;
+    }
+  }
+`
+const TopBarSection = Styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`
+
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopBarSection>
+      <Container>
+        <LeftContainer>
+          <LeftSpan>TOPICS</LeftSpan><LeftSpan>SEARCH</LeftSpan>
+        </LeftContainer>
+        <CenterContainer>
+          <CenterSpan>GENERAL</CenterSpan><CenterSpan>BROWNBAG</CenterSpan><CenterSpan>RANDOM</CenterSpan><CenterSpan>MUSIC</CenterSpan><CenterSpan>ANNOUNCEMENTS</CenterSpan>
+        </CenterContainer>
+        <RightContainer>
+          <RightSpan>LOG IN</RightSpan>
+        </RightContainer>
+      </Container>
+    </TopBarSection>
   )
 }
 
