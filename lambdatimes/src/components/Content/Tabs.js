8efcee9f7e735b17c245487a2f4f1bba08d0259c
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-12 09:41:53 -0600

    tabs.js done

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,12 +5,19 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+         {props.tabs.map( (tabs, index) =>
+          <Tab tab = {tab} selectedTab = {props.selectedTab} selectedTabHandler = {props.selectedTabHandler} key = {index} />
+          
+          )}
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
     </div>
   );
 };
-
+Tabs.propTypes = {
+  selectedTab : propTypes.string.isRequired,
+  selectedTabHandler : propTypes.func.isRequired
+}
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
