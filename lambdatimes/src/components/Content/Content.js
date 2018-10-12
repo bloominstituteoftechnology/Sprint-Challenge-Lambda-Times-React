fbb7a7b061bbb5c995ee18a5e30e3dfd3a289d0f
Riley-Brown <riley@riley.gg>
2018-10-12 12:24:46 -0400

    Added filter functionality

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -27,7 +27,7 @@ export default class Content extends Component {
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
     this.setState({
-      selected: this.state.tabs
+      selected: tab
     });
   };
 
@@ -44,7 +44,13 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === "all") {
+      return this.state.cards;
+    } else {
+      return this.state.cards.filter(card => card.tab == this.state.selected);
+    }
+
+    // return this.state.cards;
   };
 
   render() {
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -9,10 +9,10 @@ const Tab = props => {
   return (
     <div
       className={props.tab === props.selectedTab ? "tab active-tab" : "tab"}
-      onClick={tab => {
+      onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-        props.changeSelected;
+        props.changeSelected(tab);
       }}
     >
       {props.tab.toUpperCase()}
