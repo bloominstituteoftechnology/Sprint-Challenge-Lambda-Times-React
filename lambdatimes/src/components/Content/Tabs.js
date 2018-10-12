c4be7fe5a23a76e1886a55cdf8678e937d47e13f
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-12 11:39:14 -0500

    got tabs to show up

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import Tab from './Tab';
+import React from "react";
+import Tab from "./Tab";
+import PropTypes from "prop-types";
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab => (
+          <Tab
+            selectedTab={props.selectedTab}
+            selectTabHandler={props.selectTabHandler}
+            tab={tab}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.PropTypes = {
+  selectedTab: PropTypes.string,
+  tabs: PropTypes.arrayOf(PropTypes.string)
+};
+
 export default Tabs;
