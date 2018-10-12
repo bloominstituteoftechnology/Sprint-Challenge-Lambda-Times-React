3d5f6a790e8b21c152a9f27325fe05951810ab71
unknown <quan.nguyen199712@gmail.com>
2018-10-12 12:16:31 -0400

    added functionality

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
-  return (
-    <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
-      {props.tab.toUpperCase()}
-    </div>
-  );
+ 
+  if (props.tab === props.selectedTab) {
+    return (
+      <div
+        className={"tab active-tab"}
+      >
+        {props.tab.toUpperCase()}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={"tab"}
+        onClick={() => {
+          props.selectTabHandler(props.tab)
+        }}
+      >
+        {props.tab.toUpperCase()}
+      </div>
+    );
+  }
 };
 
 // Make sure you include PropTypes on your props.
