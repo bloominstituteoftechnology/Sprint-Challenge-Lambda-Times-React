8c4409a95192bd8255f4fe533fb7e7af249869df
Axolotl <samuelmachat@gmail.com>
2018-10-12 13:14:41 -0400

    converts styling for header and topbar to styled-components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,13 @@
-import React from 'react';
-
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+import React from 'react'
+import { Container, H1, TempSpan, DateSpan } from '../styles/Header'
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <Container>
+      <DateSpan>SMARCH 32, 2018</DateSpan>
+      <H1>Lambda Times</H1>
+      <TempSpan>98°</TempSpan>
+    </Container>
   )
 }
 
