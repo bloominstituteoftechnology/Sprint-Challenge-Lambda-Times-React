fbc2e3b3690460317f247d3a845f0ec023cab470
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-12 13:07:00 -0400

    followed instructions, filtered out the cards for when tab is selected

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -23,7 +23,7 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    
+    this.setState({selected:tab})
   };
 
   filterCards = () => {
@@ -39,7 +39,11 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+   if(this.state.selected === "all"){
+     return cardData;
+   }else{
+     return cardData.filter(card => card.tab === this.state.selected);
+   }
   };
 
   render() {
@@ -50,7 +54,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs} 
+          selectedTab={this.state.tabs} 
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
