5e58c1568d283acfafc623e751830ede735ef046
Rahul Desai <rd825@georgetown.edu>
2018-10-12 13:44:51 -0400

    Converted all styles to styled components, deleted CSS file

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
+
+const Headline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+
+const Author = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+`;
+
+const ImgContainer = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+`;
+
+const Headshot = styled.img`
+  width: 40px;
+`;
+
+const Byline = styled.span`
+  padding-left: 10px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.card.headline}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={props.card.img} alt={props.card.author}/>
-        </div>
-        <span>By {props.card.author}</span>
-      </div>
-    </div>
+    <StyledCard>
+      <Headline>{props.card.headline}</Headline>
+      <Author>
+        <ImgContainer>
+          <Headshot src={props.card.img} alt={props.card.author}/>
+        </ImgContainer>
+        <Byline>By {props.card.author}</Byline>
+      </Author>
+    </StyledCard>
   );
 };
 
