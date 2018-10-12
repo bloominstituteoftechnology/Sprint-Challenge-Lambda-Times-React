6ae6d701300ea9bf5857599ed186474328ae2832
Andre Myrick <amyrick.820@gmail.com>
2018-10-12 12:22:48 -0400

    creates tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -5,6 +5,7 @@ import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+let copyCardData = cardData;
 
 export default class Content extends Component {
   constructor(props) {
@@ -18,10 +19,17 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-  }
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
+  };
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: tab
+    })
   };
 
   filterCards = () => {
@@ -37,7 +45,19 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    let selected = this.state.selected;
+    if (selected === "all") {
+      this.setState({
+        cards: cardData
+      })
+    } else {
+      let filteredCards = copyCardData.filter(card => {
+        return card.tab.includes(selected);
+      })
+      this.setState({
+        cards: filteredCards
+      })
+    }
   };
 
   render() {
@@ -48,8 +68,12 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
+        <Tabs 
+        tabs={this.state.tabs}
+        selectedTab={this.state.selected}
+        changeSelected={this.changeSelected}
+        />
+        <Cards cards={this.filterCards} />
       </div>
     );
   }
