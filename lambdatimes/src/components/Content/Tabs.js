8d32b400e2cd5360ad6de1c217afdfec39c8cc20
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-10-12 08:49:55 -0700

    added data to state and finished tabs component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +8,26 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.Tabs.map(tab, index => {
+              return (
+                <Tab 
+                key={index}
+                selectTabHandler={props.selectTabHandler}
+                selectedTab={props.selectedTab}
+                tab={tab}
+                />
+              );
+            })}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+
+Tabs.propTypes = {
+  selectedTab : propTypes.string,
+  selectedTabHandler : propTypes.func
+}
+
 export default Tabs;
