d8d9c8871fbe5acd520a756d6827b923cafecc81
David247365 <ds051128@gmail.com>
2018-10-12 13:11:23 -0500

    completed header styling

--- lambdatimes/src/components/Style.js
+++ lambdatimes/src/components/Style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+// TopBar Styling
+
 export const Topbar = styled.div`
   width: 100%;
   display: flex;
@@ -69,4 +71,48 @@ export const Right = styled.div`
   span {
     cursor: pointer;
   }
+`;
+
+// Header Styling
+
+export const Head = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 67.1%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+`;
+
+export const Date = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 15px;
+`;
+
+export const Temp = styled.div`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
 `;
