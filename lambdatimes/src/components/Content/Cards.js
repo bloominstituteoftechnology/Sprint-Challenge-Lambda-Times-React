8dd7df7ca464050ce594d2379df522235284664b
Unknown <zachary.arney@gmail.com>
2018-10-12 11:57:53 -0500

    Mapped through cards to display all cards

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
 
-const Cards = props => {
+const Cards = (props) => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {props.cards.map((card) => {
+        return (
+          <Card
+            card={card}
+            key={card.headline}
+            headline={card.headline}
+            tab={card.tab}
+            img={card.img}
+            author={card.author}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
+
+/* Using the cards prop, map over the list creating a 
+          new Card component for each passing the card as the only prop*/
