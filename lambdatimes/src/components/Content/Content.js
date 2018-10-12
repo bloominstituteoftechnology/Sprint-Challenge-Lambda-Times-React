f1491be4f4230a5fe6986fa8527c33fc17ee0dbc
Brad Mortensen <b.mort86@gmail.com>
2018-10-12 10:33:08 -0600

    everything displaying working on logic

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,15 +17,15 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
     this.setState({tabs: tabData, cards: cardData})
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({selected: tab})
   };
 
-  filterCards = () => {
+  filterCards = tab => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
@@ -38,7 +38,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    
   };
 
   render() {
@@ -49,8 +49,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
+        <Tabs 
+          select={this.state.selected}
+          changeSelected={this.changeSelected} 
+          tabs={this.state.tabs} />
+        <Cards cards={this.state.cards} />
       </div>
     );
   }
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -6,11 +6,8 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={'tab'}
+      onClick={props.change}
     >
       {props.tab.toUpperCase()}
     </div>
