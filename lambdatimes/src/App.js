0bded39a1b2e7786e4d60f8cda8a9ff7bbaaf023
Axolotl <samuelmachat@gmail.com>
2018-10-12 13:32:43 -0400

    adds infinite carousel

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TopBar from './components/TopBar'
 import Header from './components/Header'
+import Carousel from './components/Carousel/Carousel'
 import Content from './components/Content/Content'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <TopBar />
       <Header />
+      <Carousel />
       <Content />
     </div>
   )
--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { carouselData } from '../../data'
-// Complete this Carousel 
-export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
 
+export default class Carousel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: [],
+      selected: 0
     }
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({ images: carouselData })
   }
 
   leftClick = () => {
-
+    const { images, selected } = this.state
+    this.setState({
+      selected: selected === 0 ? images.length - 1 : selected - 1
+    })
   }
 
   rightClick = () => {
-
+    const { images, selected } = this.state
+    this.setState({
+      selected: selected === images.length - 1 ? 0 : selected + 1
+    })
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    const { images, selected } = this.state
+    return (
+      <img src={images[selected]} style={{ display: 'block' }} alt="carousel" />
+    )
   }
-  
-  render(){
+
+  render() {
+    const { leftClick, rightClick, selectedImage } = this
+
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <div className="left-button" onClick={leftClick}>
+          {'<'}
+        </div>
+        {selectedImage()}
+        <div className="right-button" onClick={rightClick}>
+          {'>'}
+        </div>
       </div>
     )
   }
