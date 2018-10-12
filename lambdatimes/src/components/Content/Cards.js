100b434f62643c82ba229f88f5a4702037362261
Rahul Desai <rd825@georgetown.edu>
2018-10-12 11:35:33 -0400

    Added prop-types for Cards and Card

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
@@ -11,6 +12,9 @@ const Cards = props => {
   )
 }
 
-// Make sure you include prop types for all of your incoming props
+// DONE: Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Cards;
