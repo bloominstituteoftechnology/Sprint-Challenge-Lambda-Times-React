7d12d2150900edac21076c7b5baac22171bf2b37
unknown <brandongrych@gmail.com>
2018-10-12 10:33:45 -0500

    basic functionality built, but nothing displaying yet

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -26,6 +26,10 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+
+    this.setState({
+      selected: {tab}
+     })
   };
 
   filterCards = () => {
@@ -39,7 +43,15 @@ export default class Content extends Component {
           of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+    if (this.state.selected === 'all') {
     return this.state.cards;
+  } else {
+    let newCards = [...this.state.cards];
+    newCards = newCards.filter(card => {
+      return card.tab === this.state.selected;
+    })
+    this.setState({ cards: newCards })
+  }
   };
 
   render() {
