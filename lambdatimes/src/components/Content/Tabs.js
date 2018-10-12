73531227e04cce7db14305783463400b9aff9acd
Drewsup123c <drewsup123c@gmail.com>
2018-10-12 09:54:46 -0600

    finished tabs.js except the PropTypes those will be last

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,6 +7,9 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.tabs.map(tab => {
+              return (<Tab tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab}/>)
+            })}
       </div>
     </div>
   );
