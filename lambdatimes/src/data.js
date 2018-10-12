cac79625dc985fdfc66fd88281b07603980a42f5
Lee Len Formento <leebatef@gmail.com>
2018-10-12 13:08:11 -0400

    MVP Done

--- lambdatimes/src/data.js
+++ lambdatimes/src/data.js
@@ -99,6 +99,16 @@ export const cardData = [
 export const carouselData = [
   "./assets/carousel/mountains.jpeg",
   "./assets/carousel/computer.jpeg",
-  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/trees.jpeg"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ,
   "./assets/carousel/turntable.jpeg"
 ]
