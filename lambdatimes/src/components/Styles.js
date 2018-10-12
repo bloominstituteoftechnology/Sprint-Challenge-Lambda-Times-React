32ce7cc80432604865a11af8b9422e8ca1593d5f
Justin <jdlow2@gmail.com>
2018-10-12 12:27:29 -0400

    styled components for TopBar

---
+++ lambdatimes/src/components/Styles.js
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+export const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+export const TopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+
+  span {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+`;
+
+export const TopBarContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+
+  span {
+    cursor: pointer;
+    margin-right: 5%;
+
+    &:last-child {
+      margin-right 0;
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  `;
+
+export const TopBarContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+
+  span {
+    cursor: pointer;
+  }
+`;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import { StyledTopBar, TopBarContainer, TopBarContainerLeft, TopBarContainerCenter, TopBarContainerRight } from "./Styles";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const TopBar = () => {
-  return (
-    <div className="top-bar">
+    {/* <div className="top-bar">
       <div className="container">
         <div className="container-left">
           <span>TOPICS</span><span>SEARCH</span>
@@ -17,7 +16,22 @@ const TopBar = () => {
           <span>LOG IN</span>
         </div>
       </div>
-    </div>
+    </div> */}
+const TopBar = () => {
+  return (
+    <StyledTopBar>
+      <TopBarContainer>
+        <TopBarContainerLeft>
+          <span>TOPICS</span><span>SEARCH</span>
+        </TopBarContainerLeft>
+        <TopBarContainerCenter>
+          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+        </TopBarContainerCenter>
+        <TopBarContainerRight>
+          <span>LOG IN</span>
+        </TopBarContainerRight>
+      </TopBarContainer>
+    </StyledTopBar>
   )
 }
 
