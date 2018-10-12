a4e75e1f56b63902686502202b87e05f72fde01c
MEBonham <programmer@mckaybonham.com>
2018-10-12 11:24:32 -0600

    Converted TopBar and Header to styled components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { StyledTopBar } from "../Styles";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
+    <StyledTopBar>
       <div className="container">
         <div className="container-left">
           <span>TOPICS</span><span>SEARCH</span>
@@ -17,7 +18,7 @@ const TopBar = () => {
           <span>LOG IN</span>
         </div>
       </div>
-    </div>
+    </StyledTopBar>
   )
 }
 
