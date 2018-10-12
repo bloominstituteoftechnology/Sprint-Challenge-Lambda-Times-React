6b10774115d8c3bca17fd623c9db8e934154adff
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-10-12 11:48:38 -0500

    refactored my topbar to styled components

--- lambdatimes/src/CSS/styles.js
+++ lambdatimes/src/CSS/styles.js
@@ -43,3 +43,75 @@ export const HeaderSpanTemp = styled.span`
   margin-right: 25px;
   flex: 1;
 `;
+
+export const TopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+export const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+export const TopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+export const TopBarContainerLeftSpan = styled.div`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+export const TopBarContainerCenter = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+export const TopBarContainerCenterSpan = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+  :last-child {
+    margin-right: 0;
+  }
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export const TopBarContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+export const TopBarContainerRightSpan = styled.span`
+  cursor: pointer;
+`;
