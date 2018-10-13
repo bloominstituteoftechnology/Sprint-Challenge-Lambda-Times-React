305640a70070d9f2b8d673917cb633f1d279b6d1
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-13 12:51:42 -0400

    working on tab and tabs files

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs"
+import Cards from "./Cards"
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data"
 
 export default class Content extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      selected: 'all',
+      selected: "all",
       tabs: [],
       cards: []
-    };
+    }
   }
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-  };
+    this.setState({})
+  }
 
   filterCards = () => {
     /* Right now this function only returns the cards on state.
@@ -37,8 +42,8 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
-  };
+    return this.state.cards
+  }
 
   render() {
     return (
@@ -48,9 +53,13 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
-    );
+    )
   }
 }
