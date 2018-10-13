fc0c8685fa520ac96081c9a1023b29dd419e573c
ShardeDeLane <rhcprocker@yahoo.com>
2018-10-13 13:42:29 -0400

    cards updated

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -45,17 +45,22 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards
+    let newCards = this.state.cards
+    if (this.state.selected === "all") {
+      return cardData
+    } else {
+      let newInfo = newCards.filter(newCard => {
+        if (newCard.tab === this.state.selected) {
+          return newCard
+        }
+        newInfo
+      })
+      return newInfo
+    }
   }
-
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs
           tabs={this.state.tabs}
           selectedTab={this.state.selected}
