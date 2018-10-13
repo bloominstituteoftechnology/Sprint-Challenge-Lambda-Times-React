e8aab57368888a62c63eaba52425e175c90e52a7
Robin U Familara <robin.familara@cgius.com>
2018-10-13 14:25:14 -0500

    Modified the card file

--- lambdatimes/lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/lambdatimes/src/components/Content/Cards.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Cards = props => {
+const Card = props => {
   return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/
-      props.cards.map(card => (
-        <Card key={card.headline} card={card} />
-      ))}
+    <div className="card">
+      <div className="headline">{props.card.headline}</div>
+      <div className="author">
+        <div className="img-container">
+          <img src={props.card.img} alt="Author Photo" />
+        </div>
+        <span>By {props.card.author}</span>
+      </div>
     </div>
   );
 };
 
-// Make sure you include prop types for all of your incoming props
-Cards.propTypes = {
-  cards: PropTypes.arrayOf(
-    PropTypes.shape({
-      headline: PropTypes.string,
-      tab: PropTypes.string,
-      img: PropTypes.string,
-      author: PropTypes.string
-    })
-  )
+// Make sure to include PropTypes.
+Card.proptypes = {
+  headline: PropTypes.string,
+  img: PropTypes.string,
+  author: PropTypes.string
 };
 
-export default Cards;
+export default Card;
