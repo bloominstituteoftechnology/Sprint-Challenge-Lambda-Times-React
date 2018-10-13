dc508e35819966b74fabe8a7bbbf964b040154cf
Clem <stacy.clem@e-hps.com>
2018-10-13 08:17:00 +0100

    Updated  all files > Added Cards, Tabs, and carousel.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,28 +5,45 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      imgs: [],
+      active:0
     }
   }
   componentDidMount(){
-
+    this.setState({ imgs: carouselData, active:0})
   }
 
   leftClick = () => {
+    if(this.state.active >0){
+    this.setState(prevState => ({
+      active: prevState.active - 1
+    }))}
+    else{
+this.setState({active:this.state.imgs.length-1})
+    }
 
   }
 
   rightClick = () => {
+    if (this.state.active < this.state.imgs.length-1) {
+      this.setState(prevState => ({
+        active: prevState.active + 1
+      }))
+    }
+    else {
+      this.setState({ active: 0 })
+    }
 
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.imgs[this.state.active]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
+      {this.selectedImage(this.state.active)}
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,14 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: tabData, cardData: cardData });
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+   this.setState({
+     selected: tab
+   })
   };
 
   filterCards = () => {
@@ -37,7 +41,10 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    const filteredCards = cardData.filter(card => this.state.selected === 'all'
+     || this.state.selected === card.tab);
+     
+    return filteredCards;
   };
 
   render() {
@@ -48,7 +55,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTabHandler={this.changeSelected} selectedTab={this.state.selected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
