bc5186310ca95afc6a1c07ad5fc8aa61ecae86e0
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 15:53:50 -0400

    added console.log to log props

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
+  console.log('props', props);
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
+      if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
   return (
     <div
       className={''}
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+        /* Replace this dummy click handler function with your selectTabHandler function from props
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
     >
-      {props.tab.toUpperCase()}
+      {/*props.tab.toUpperCase()*/}
     </div>
   );
 };
