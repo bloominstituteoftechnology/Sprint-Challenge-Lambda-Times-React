ee7b59bda69ad8fa1723374de287c9c509b66c04
Tommy Collison <tommy@collison.ie>
2018-10-13 11:24:23 -0700

    top bar is styled. span, span, wherever you may be

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -16,17 +16,29 @@ width: 100%;
 color: white;
 `;
 
+// there must be a better way of doing it than this but this is what I'll go with since there's only 45 mins left:
+export const TopBarContainerLeftSpan = styled.span`
+margin-right: 25%;
+`;
+
+
 export const TopBarContainerCenter = styled.div`
 display: flex;
 width: 100%;
 color: white;
 `;
 
+export const TopBarContainerCenterSpan = styled.span`
+margin-right: 5%;
+`;
+
 export const TopBarContainerRight = styled.div`
 display: flex;
 width: 100%;
 color: white;
+margin-right: -20%;
 `;
+
 
 export const Container = styled.div`
 display: flex;
@@ -35,19 +47,21 @@ color: white;
 padding: 0 20px;
 `;
 
+
+
 const TopBar = () => {
   return (
     <StyledTopBar>
       <Container>
         <TopBarContainerLeft>
-          <span>TOPICS</span><span>SEARCH</span>
+          <TopBarContainerLeftSpan>TOPICS</TopBarContainerLeftSpan><TopBarContainerLeftSpan>SEARCH</TopBarContainerLeftSpan>
         </TopBarContainerLeft>
         <TopBarContainerCenter>
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+          <TopBarContainerCenterSpan>GENERAL</TopBarContainerCenterSpan><TopBarContainerCenterSpan>BROWNBAG</TopBarContainerCenterSpan><TopBarContainerCenterSpan>RANDOM</TopBarContainerCenterSpan><TopBarContainerCenterSpan>MUSIC</TopBarContainerCenterSpan><TopBarContainerCenterSpan>ANNOUNCEMENTS</TopBarContainerCenterSpan>
           </TopBarContainerCenter>
-          <TopBarContainerCenter>
+          <TopBarContainerRight>
           <span>LOG IN</span>
-          </TopBarContainerCenter>
+          </TopBarContainerRight>
         </Container>
     </StyledTopBar>
   )
