7ff683f8635f695143c8c8e623dfab984c138bce
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 17:17:21 -0400

    added console.log for props corrected map method to set Card key to index corrected props passed to Card component

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
+  console.log(props);
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map((card, i) => {
+        return (
+          <Card
+            key={i}
+            tab={props.tab}
+            author={props.author}
+            img={props.img}
+            headline={props.headline}
+          />
+        )
+      })}
     </div>
   )
 }
