483834d0643dddb73b30163d3e97d72bae04cbf5
Rob Salzberg <robsalzberg@gmail.com>
2018-10-13 14:01:31 -0400

    PropTypes validation added

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+	/* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-      const selectedTabClass = 'tab active-tab';
-      const notSelectedTabClass = 'tab';
-  return (
-    <div
-      className={props.tab === props.selectedTab ? selectedTabClass : notSelectedTabClass}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+	const selectedTabClass = 'tab active-tab';
+	const notSelectedTabClass = 'tab';
+	return (
+		<div
+			className={
+				props.tab === props.selectedTab ? selectedTabClass : notSelectedTabClass
+			}
+			onClick={() => {
+				/* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-         props.selectTabHandler(props.tab)
-      }}
-    >
-      {props.tab.toUpperCase()}
-    </div>
-  );
+				props.selectTabHandler(props.tab);
+			}}
+		>
+			{props.tab.toUpperCase()}
+		</div>
+	);
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+	tab: PropTypes.string
+};
 
 export default Tab;
