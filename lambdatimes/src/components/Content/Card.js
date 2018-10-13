0055d46a51d839be36cdf486c7c77e6d3ce5cd98
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-10-13 12:56:06 -0500

    Card component done with proptypes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import Proptypes from "prop-types";
 
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={props.img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
-
+Card.prototype = {
+  card: Proptypes.shape({
+    headline: Proptypes.string,
+    img: Proptypes.string,
+    author: Proptypes.string,
+  })
+}
 export default Card;
