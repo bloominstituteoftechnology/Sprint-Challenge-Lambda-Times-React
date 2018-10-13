ff693f65d72c07f98bba7865c1ae42f5ba5f4568
Marc de la Torre <mdelatorre1314@gmail.com>
2018-10-13 11:34:02 -0700

    commit content (content, Tabs, Tab, Cards, Card) js' as well as header and topbar refactor

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{/* headline goes here */props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={ /* image source goes here */props.card.img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {/* author goes here */props.card.author}</span>
       </div>
     </div>
   );
@@ -16,4 +17,11 @@ const Card = props => {
 
 // Make sure to include PropTypes.
 
+Card.propTypes = {
+  card: propTypes.shape({
+    headline: propTypes.string,
+    img: propTypes.string,
+    author: propTypes.string,
+  })
+}
 export default Card;
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,54 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+	}
+	> h1 {
+		font-size: 60px;
+	  font-family: Didot, serif;
+	  font-weight: bold;
+	  flex: 8;
+	  text-align: center;
+	  color: #000;
+	}
+	> span {
+		align-self: flex-end;
+	  font-size: 11px;
+	  font-weight: bold;
+	  letter-spacing: 1px;
+	}
+`;
+ const HeaderDate = styled.span`
+  margin-left: 25px;
+  flex: 1;
+`;
+ const HeaderWeather = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <HeaderTitle>
+      <HeaderDate>MARCH 32, 2018</HeaderDate>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <HeaderWeather>98°</HeaderWeather>
+    </HeaderTitle>
   )
 }
 
