a23b84b9eb9a01842beb00e8a4a718fa633b3c4c
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-13 12:42:21 -0400

    cards are showing up

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -44,13 +44,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    this.state.cards.filter(card => {
-      if(this.state.selected === 'all') {
-        return this.state.cards;
-      } else {
-        return card.tab.includes(this.state.selected);
-      }
-    });
+    // this.state.cards.filter(card => {
+    //   if(this.state.selected === 'all') {
+    //     return this.state.cards;
+    //   } else {
+    //     return card.tab.includes(this.state.selected);
+    //   }
+    // });
+    return this.state.cards;
   };
 
   render() {
