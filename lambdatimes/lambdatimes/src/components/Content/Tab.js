f396201727cc4345d4ce252ce904b1ef7722bd5d
Robin U Familara <robin.familara@cgius.com>
2018-10-13 13:45:23 -0500

    Setup click handler

---
+++ lambdatimes/lambdatimes/src/components/Content/Tab.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Tab = props => {
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+      if they match, the className should be: 'tab active-tab', 
+      if it is not it should just be 'tab'*/
+  return (
+    <div
+      className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
+      onClick={() => props.selectedTabHandler(props.tab)}
+      /* Replace this dummy click handler function with your selectTabHandler function from props 
+         you'll need to pass the `tab` in as an argument to this handler. */
+    >
+      {props.tab.toUpperCase()}
+    </div>
+  );
+};
+
+// Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  tab: PropTypes.string
+};
+
+export default Tab;
