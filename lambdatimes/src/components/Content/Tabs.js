41aa0d6015c6836673ef219340d908fe3c42556a
cferrell2 <cferrell12182@gmail.com>
2018-10-13 15:48:34 -0400

    add keys to individual tabs and cards using index of related array

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -9,8 +9,8 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-            {props.tabs.map(tab =>{
-              return <Tab tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab}/>
+            {props.tabs.map((tab, index) =>{
+              return <Tab key={index} tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab}/>
             })}
       </div>
     </div>
