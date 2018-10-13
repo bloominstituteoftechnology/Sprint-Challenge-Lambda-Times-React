0eec58cd99a92245e6ed75db5f86c9b2f6b4d25a
Jennifer King <jenbellpgh@gmail.com>
2018-10-13 13:11:22 -0400

    Tabs.js: Passed tab props to Tab.js     - propType validation to check info being passed.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Tab from './Tab';
+
+// Props passed in:
+  // - tabs: ()
+  // - selectedTab: "all"
+  // - selectTabHandler: fn()
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +14,20 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map( (tab) => {
+          return (
+            <Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} ></Tab>
+          )
+        })}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tab: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired
+};
 export default Tabs;
