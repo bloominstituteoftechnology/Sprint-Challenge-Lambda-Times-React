bbac38db67295041bdcf4d89fcc85f251c4b8ba9
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 18:53:44 -0400

    commented out console log

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = props => {
-  console.log('card props', props);
+  // console.log('card props', props);
   return (
     <div className="card">
       <div className="headline">{props.headline}{/* headline goes here */}</div>
