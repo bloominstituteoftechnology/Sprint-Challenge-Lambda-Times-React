9717a31560b761ea91b5784374aa6ca5f6d6ebf4
Jennifer King <jenbellpgh@gmail.com>
2018-10-13 15:30:19 -0400

    Updated tab & tabs to use styled components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Styled from 'styled-components';
+
+// Styled components:
+const TabWrapper = Styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  :hover {
+    text-decoration: underline;
+  }
+
+  ${props => (props.selectedTab === props.tab ? `
+    background-color: #fff;
+    color: #333;
+    border: 2px solid #333;
+  ` : null )}
+`;
 
 // Props taken in:
   // - tab: ""
@@ -17,12 +44,11 @@ const Tab = props => {
   };
 
   return (
-    <div
-      className={tabClass}
+    <TabWrapper
       onClick={tabHandler}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabWrapper>
   );
 };
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Styled from 'styled-components';
 import Tab from './Tab';
+
+// Styled components:
+const TabsWrapper = Styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+const TopicsWrapper = Styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+
+  span {
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+`;
 
 // Props passed in:
   // - tabs: ()
@@ -9,9 +38,9 @@ import Tab from './Tab';
 
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsWrapper>
+      <TopicsWrapper>
+        <span>TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
         {props.tabs.map( (tab) => {
@@ -19,8 +48,8 @@ const Tabs = props => {
             <Tab tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} ></Tab>
           )
         })}
-      </div>
-    </div>
+      </TopicsWrapper>
+    </TabsWrapper>
   );
 };
 
