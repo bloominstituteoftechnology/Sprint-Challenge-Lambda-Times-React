6ae9f101594fb988d7bc403f350f30c1944b8195
FreeKeyBoy <jjhutton81@gmail.com>
2018-10-13 10:39:01 -0700

    Got the trending topics tab to render, though it needs styling. Declared the proptypes on Cards.js, after converting from functional component to class component.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {props.cards.map((card, index) => <Card key = {index} card = {card} />)}
-    </div>
-  )
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+     this.state = {
+      cards: props.cards
+    }
+  }
+  
+  render() {  
+    return (
+      <div className="cards-container">
+        {this.props.cards.map((card, index) => <Card key = {index} card = {card} />)}
+      </div>
+    )
+  }
 }
 
+
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.string)
+}
+
 
 export default Cards;
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -27,7 +27,7 @@ export default class Content extends Component {
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
     this.setState({
-      tabs: tab.target.value
+      selected: tab.target.value
     })
   };
 
@@ -44,6 +44,13 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+    if (this.state.selected === 'all') {
+      return this.state.cards;
+    } else {
+      this.setState({ cards: this.state.cards.filter (card => 
+        card.tab === this.state.selected 
+        )})
+    }
     return this.state.cards;
   };
 
@@ -55,7 +62,9 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs} 
+          selectedTab = {this.tab} />
         <Cards cards={this.filterCards()} />
       </div>
     );
