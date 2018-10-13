cf205dc7543e79020b3edbc26493fe0463ca4c0a
Bruce Cabanayan <bcabanayan@Bruces-MBP.lan>
2018-10-13 09:39:41 -0700

    Added functionality to check if tab selected to determine tab className

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,9 +4,19 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+  let checkSelected = () => {
+    if (props.tab === props.selectedTab) {
+      return 'tab active-tab'
+    }
+    else {
+      return 'tab'
+    }
+  }
+
   return (
     <div
-      className={''}
+      className={checkSelected()}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
