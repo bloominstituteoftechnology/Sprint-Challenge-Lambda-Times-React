b99245f8d4d76483084d1024ec4efe9faed83ca5
Tommy Collison <tommy@collison.ie>
2018-10-13 10:14:07 -0700

    tabs.js proptypes

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 
 const Tabs = props => {
   return (
@@ -22,6 +23,8 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
-
+PropTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
 
 export default Tabs;
