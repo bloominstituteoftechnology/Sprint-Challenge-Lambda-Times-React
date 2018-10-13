f267cf1279dc5f3bb2fd9fb993e7f4c95279c1fa
Denise Escobar <descobar1490@gmail.com>
2018-10-13 13:08:21 -0500

    Able to filter posts with the tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -45,7 +45,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+  let filteredArr= []
+  if(this.state.selected !== 'all'){
+    filteredArr=cardData.filter(obj => obj.tab === this.state.selected)
+  } else{
+    filteredArr= cardData;
+  }
+    console.log(filteredArr)
+    return filteredArr;
   };
 
   render() {
