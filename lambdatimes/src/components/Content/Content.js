82b09b615086765db9f97d0bf7cea575011af8b4
Bruce Cabanayan <bcabanayan@Bruces-MBP.lan>
2018-10-13 09:23:59 -0700

    Added data to content state using componentDidMount and added props to tabs component

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,9 +18,13 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
-  changeSelected = tab => {
+  changeSelected = tab => { 
     // this function should take in the tab and update the state with the new tab.
   };
 
@@ -48,7 +52,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
