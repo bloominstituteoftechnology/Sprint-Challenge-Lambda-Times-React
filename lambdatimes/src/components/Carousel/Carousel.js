fbce5c19e09fc7794bf475181764b23bf3e83651
Kendra Williams <kmonique.williams@gmail.com>
2018-10-13 14:59:40 -0400

    Stretch goal: carousel functionally completed, but the images won't show up

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,30 +1,66 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
+//Carousel functions correctly, but the images won't show up
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      currentImg: "",
+      imgPosition: 0,
+      data: [],
     }
   }
   componentDidMount(){
-
+    this.setState({
+      currentImg: carouselData[this.state.imgPosition],
+      imgPosition: 0,
+      data: carouselData,
+    })
   }
 
   leftClick = () => {
-
+    if(this.state.imgPosition === 0) {
+      this.setState(prevState => {
+        return {
+          imgPosition: prevState.imgPosition + 3,
+          currentImg: carouselData[prevState.imgPosition + 3],
+        }
+      })
+    } else {
+      this.setState(prevState => {
+        return {
+          imgPosition: prevState.imgPosition - 1,
+          currentImg: carouselData[prevState.imgPosition - 1],
+        }
+      })
+    }
   }
 
   rightClick = () => {
-
+    if(this.state.imgPosition === 3) {
+      this.setState(prevState => {
+        return {
+          imgPosition: prevState.imgPosition - 3,
+          currentImg: carouselData[prevState.imgPosition - 3],
+        }
+      })
+    } else {
+      this.setState(prevState => {
+        return {
+          imgPosition: prevState.imgPosition + 1,
+          currentImg: carouselData[prevState.imgPosition + 1],
+        }
+      })
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.currentImg} style={{display: 'block'}} alt="carousel"/>
   }
   
   render(){
+    console.log(this.state)
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
