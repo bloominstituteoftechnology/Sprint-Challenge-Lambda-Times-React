ac70502e4de3e9c4b05aea0ef627a93f1837e05b
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-13 12:48:36 -0400

    Wrote logic for Content.js, moving onto components.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,13 +18,29 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      tabs: this.state.tabs.push(tab)
+    })
   };
 
   filterCards = () => {
+
+    cardData.map(card => {
+      if(card.tab === 'all'){
+        return card
+      }
+      else {
+       return this.state.cards.push(card)
+      }
+    })
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
@@ -48,7 +64,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs selectedTab={this.changeSelected} selectTabHandler={this.filterCards} tabs={this.state.tabs} />
         <Cards cards={this.filterCards()} />
       </div>
     );
