c2b315bdb653edcb0ac7298da284f6d6311d62e5
Bruce Cabanayan <bcabanayan@Bruces-MBP.lan>
2018-10-13 09:32:05 -0700

    Added PropType checking to tabs

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -20,3 +22,9 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+Tabs.propTypes = {
+  tabs: PropTypes.array,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.func
+}
