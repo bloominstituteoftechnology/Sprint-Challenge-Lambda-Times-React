bf1b04556415d4c0fbba4524e617ef03d1787aa9
marcusbass323 <marcus@bassconsultantgroup.com>
2018-10-13 10:31:24 -0700

    Fixed card tabs w/ map method

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -3,9 +3,13 @@ import Card from './Card';
 
 const Cards = props => {
   return (
+  
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      
+          
+
     </div>
   )
 }
