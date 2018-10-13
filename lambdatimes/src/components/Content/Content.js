0bd0903a6644d2544027a50beddbf2268ab79c65
Glenn-David Daniel <d2rd@outlook.com>
2018-10-13 14:25:01 -0700

    WIP.  Update to clear yarn start error.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,12 +18,12 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-    this.setState({ tabs: tabData }, { cards: cardData});
+    // this.setState({ tabs: tabData }, { cards: cardData});
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    
+
   };
 
   filterCards = () => {
