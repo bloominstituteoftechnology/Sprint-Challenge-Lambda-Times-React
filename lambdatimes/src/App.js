d6105700396c99c9519fa7366e78cf6c5e270cfb
Colin Dismuke <colin.dismuke@gmail.com>
2018-10-13 12:20:08 -0500

    Refactored App with styled components

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <AppContainer>
       <TopBar />
       <Header />
       <Content />
-    </div>
+    </AppContainer>
   );
 }
 
