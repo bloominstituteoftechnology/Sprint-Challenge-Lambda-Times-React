bb4f66acb9b19046f121031388e1eae5606b70e5
Andres Rivera <andres.rivera.toro@gmail.com>
2018-10-13 14:04:53 -0400

    refactored styling for header.js

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
-
 const StyledHeader = styled.div`
   display: flex;
   justify-content: space-between;
@@ -15,23 +12,36 @@ const StyledHeader = styled.div`
   height: 120px;
   margin-top: 44px;
   padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    .header {
+      width: 1280px;
+    }
+  }
+
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
 `;
 
-const StyledHeaderDate = styled.span`
+const HeaderDate = styled.span`
   margin-left: 25px;
   flex: 1;
 `;
 
-const StyledHeaderH1 = styled.h1`
-  font-size: 60px;
-  font-family: Didot, serif;
-  font-weight: bold;
-  flex: 8;
-  text-align: center;
-  color: #000;
-`;
-
-const StyleHeaderTemp = styled.span`
+const HeaderTemp = styled.span`
   text-align: right;
   margin-right: 25px;
   flex: 1;
@@ -40,9 +50,9 @@ const StyleHeaderTemp = styled.span`
 const Header = () => {
   return (
     <StyledHeader>
-      <StyledHeaderDate>SMARCH 32, 2018</StyledHeaderDate>
-      <StyledHeaderH1>Lambda Times</StyledHeaderH1>
-      <StyleHeaderTemp>98°</StyleHeaderTemp>
+      <HeaderDate>SMARCH 32, 2018</HeaderDate>
+      <h1>Lambda Times</h1>
+      <HeaderTemp>98°</HeaderTemp>
     </StyledHeader>
   )
 }
