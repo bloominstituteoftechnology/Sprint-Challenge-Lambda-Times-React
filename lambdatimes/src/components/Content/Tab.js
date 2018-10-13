4b6fb36101748e64b2c9bede14521d8ad1baa1a1
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-10-13 11:28:22 -0500

    Components are rendering the correct data. functionality has not yet been added to any components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -6,7 +6,7 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
+      className={'tab'}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
