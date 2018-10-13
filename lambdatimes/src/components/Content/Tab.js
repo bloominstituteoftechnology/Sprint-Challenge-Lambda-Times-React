bb7bd0ae1907907dc3523200a49c6afada812093
paulapivat <paul.apivat@gmail.com>
2018-10-14 01:30:33 +0700

    changed Tab.js to class component, with state, componentDidMount, still rendering empty cards

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,55 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-
-// class Tab extends React.Component {
-//   constructor(props){
-//     super(props);
-//   }
-
-//    /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-//       if they match, the className should be: 'tab active-tab', 
-//       if it is not it should just be 'tab'*/
   
+class Tab extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      tab: ''
+    }
+  }
 
-//   render(){
-//     if (this.props.tab === this.props.selectedTab) 
-//     return (
-//       <div
-//         className={'tab active-tab'}
-//         onClick={(tab) => {
-//           /* Replace this dummy click handler function with your selectTabHandler function from props 
-//            you'll need to pass the `tab` in as an argument to this handler. */
-//           this.props.selectTabHandler
-//         }}
-//       >
-//         {this.props.tab.toUpperCase()}
-//       </div>
-//     );
-//   }
-// }
- 
+  componentDidMount(){
+    this.setState({tab: this.props.tab})
+  }
 
-  
-const Tab = props => {
+  render(){
+    if (this.props.tab === this.props.selectedTab) {
+      return (
+        <div
+          className={'tab active-tab'}
+          onClick={(tab) => {
+            /* Replace this dummy click handler function with your selectTabHandler function from props 
+             you'll need to pass the `tab` in as an argument to this handler. */
+            this.props.selectTabHandler
+          }}
+        >
+          {/* {props.tab.toUpperCase()}  */} Placeholder
+        </div>
+      );
+    }
+  }
+}
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  if (props.tab === props.selectedTab) 
+  
 
-  return (
-    <div
-      className={'tab active-tab'}
-      onClick={(tab) => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-        props.selectTabHandler
-      }}
-    >
-      {/* {props.tab.toUpperCase()}  */} Placeholder
-    </div>
-  );
-};
+
+
+  
+// const Tab = props => {
+//   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+//       if they match, the className should be: 'tab active-tab', 
+//       if it is not it should just be 'tab'*/
+//   if (props.tab === props.selectedTab) 
+
+//   return (
+//     <div
+//       className={'tab active-tab'}
+//       onClick={(tab) => {
+//         /* Replace this dummy click handler function with your selectTabHandler function from props 
+//          you'll need to pass the `tab` in as an argument to this handler. */
+//         props.selectTabHandler
+//       }}
+//     >
+//       {/* {props.tab.toUpperCase()}  */} Placeholder
+//     </div>
+//   );
+// };
 
 
 
