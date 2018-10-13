4bc6fbc05034a5ec4ab1227e4df612dfe540d6a4
Rob Salzberg <robsalzberg@gmail.com>
2018-10-13 13:11:16 -0400

    Cards and tabs appear on screen

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
-}
+	return (
+		<div className="cards-container">
+			{/* Using the cards prop, map over the list creating a 
+          new Card component for each passing the card as the only prop*/
+			props.cards.map((card, index) => (
+				<Card key={index} card={card} />
+			))}
+		</div>
+	);
+};
 
 // Make sure you include prop types for all of your incoming props
 
