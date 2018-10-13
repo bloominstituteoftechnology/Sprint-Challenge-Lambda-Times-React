0d5f234f1ee69c905aa9118befa861aa1b92bfd0
SeanOne <basement365@gmail.com>
2018-10-13 12:21:12 -0700

    work on cards

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -15,7 +15,6 @@ export default class Content extends Component {
       selected: 'all',
       tabs: [],
       cards: [],
-      filtered: []
     };
   }
 
@@ -30,35 +29,21 @@ export default class Content extends Component {
     this.setState({
       selected: tab
     })
-    // this function should take in the tab and update the state with the new tab.
   };
 
   filterCards = () => {
-    let filtered = this.state.cards.filter(card => card.tab.includes(this.state.selected))
-    console.log(filtered)
-    return this.state.cards
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
-
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
+    
+    if (this.state.selected === 'all') {
+      return this.state.cards
+    } else {
+      let filtered = this.state.cards.filter(card => card.tab.includes(this.state.selected))
+      return filtered
+    }
   };
 
   render() {
     return (
       <ContentContainer>
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs
           tabs={this.state.tabs}
           changeSelected={this.changeSelected}
