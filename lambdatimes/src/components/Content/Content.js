c5d626c1a9ab073ca707750ffd2d8d31345a44a8
Tommy Collison <tommy@collison.ie>
2018-10-13 09:33:00 -0700

    changeSelected and filterCards work

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -21,7 +21,7 @@ export default class Content extends Component {
   }
 
   changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
+    this.setState({selected: tab})
   };
 
   filterCards = () => {
@@ -37,7 +37,11 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if(this.state.selected === "all") { 
+      return this.state.selected;
+    } else {
+      return this.state.cards.filter(card => card.tab === this.state.selected)   
+    }
   };
 
   render() {
