ac9a815e9feaf45067e64f86bfabc005dd8d3b1e
Jason Hedrick <jasonleehedrick@gmail.com>
2018-10-13 11:47:58 -0700

    Removed a stray console.log

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -3,7 +3,7 @@ import Card from './Card';
 import PropTypes from 'prop-types';
 
 const Cards = props => {
-  console.log(props.cards);
+  
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
