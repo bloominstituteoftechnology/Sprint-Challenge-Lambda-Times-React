2d73754daff903e8426c29395e396201ed14921b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 13:23:11 -0400

    corrected setState methods tabData and cardData

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -19,8 +19,8 @@ export default class Content extends Component {
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
     this.setState({
-      tabs: tabs.tabData,
-      cards: cards.cardData
+      tabs: tabData,
+      cards: cardData
     });
   };
 
