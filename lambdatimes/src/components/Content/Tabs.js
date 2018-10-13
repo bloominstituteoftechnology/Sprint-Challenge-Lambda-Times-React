a141c9ada49bad3866d803ede79937b765d0ee9b
Joseph Thompson <joe.thompson838@gmail.com>
2018-10-13 12:17:57 -0400

    Added code for data injection into content, and tabs, tab data to render to page

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,8 +5,16 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+      {props.tabs.map(tab => {
+        return (
+        <Tab key={tab} tab={tab}></Tab>
+        );
+      })
+
+
+          /* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+          }
       </div>
     </div>
   );
