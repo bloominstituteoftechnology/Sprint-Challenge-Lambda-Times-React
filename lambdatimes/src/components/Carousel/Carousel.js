0a77e80f3223c6ea52172135c7729cb48c15cf4f
Mitchell Robles <mitchellr92@live.com>
2018-10-13 11:58:14 -0700

    started on carousel but did not finish

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,39 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      data: [],
+      asset: '',
     }
   }
   componentDidMount(){
-
+    this.setState({
+      data: carouselData,
+      asset: carouselData[0],
+    })
   }
 
   leftClick = () => {
+    this.setState({
+      asset: carouselData[1-1],
+    })
 
   }
 
   rightClick = () => {
-
+    this.setState({
+      asset: carouselData[0+1],
+    })
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.asset} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
