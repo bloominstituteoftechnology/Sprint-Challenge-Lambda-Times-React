8d6ca1eb617b06dfcdd01b4a090016d208c1ed3b
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 15:48:39 -0400

    corrected Tab key property

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,13 +8,13 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-          {props.tabs.map((tab, index) => {
+          {props.tabs.map(tab => {
             return (
               <div>
                 <Tab
-                  key={tab[index]}
+                  key={tab.toString()}
                   tab={props.tabs.tab}
-                  selectedTab={props.tabs.tab}
+                  selectedTab={props.tabs.selected}
                   selectTabHandler={props.changeSelected}
                 />
               </div>
