5681f7a7e4b6d1365665e24978bc0a7dc8c6cf27
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-10-13 12:13:03 -0600

    Cards are displaying

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -5,7 +5,12 @@ const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+          new Card component for each passing the card as the only prop*/
+          
+          props.cards.map(card => (
+            <Card card={card} />
+          ))
+      }
     </div>
   )
 }
