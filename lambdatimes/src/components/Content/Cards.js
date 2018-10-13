d2e3e4aec988296466f144f89718504cf52af48b
Jason Hedrick <jasonleehedrick@gmail.com>
2018-10-13 10:03:26 -0700

    Content displays correctly. Tabs correctly select cards to display.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,6 +6,9 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map(card => {
+            return <Card card={card} />
+          })}
     </div>
   )
 }
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,17 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData,
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: tab,
+    })
   };
 
   filterCards = () => {
@@ -37,7 +44,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if(this.state.selected === 'all') {
+      return this.state.cards;
+    }
+
+    else {
+      return this.state.cards.filter(card => card.tab === this.state.selected);
+    }
+
   };
 
   render() {
@@ -48,7 +62,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
