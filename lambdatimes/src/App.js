f85d01b0b1a9658ed092f3d34385462afc9f3012
Witanday <witanday.cd@gmail.com>
2018-10-13 15:20:52 -0400

    Basic settings and passing props and states

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,12 +3,17 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 
-const App = () => {
+
+const App = props => {
   return (
     <div className="App">
       <TopBar />
       <Header />
-      <Content />
+      <Content 
+      tabData ={props.tabData}
+      cardData ={props.cardData}
+      carouselData={props.carouselData}
+      />
     </div>
   );
 }
--- lambdatimes/src/index.js
+++ lambdatimes/src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
 import App from './App';
+import {tabData,cardData, carouselData} from './data'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App 
+tabData ={tabData}
+cardData ={cardData}
+carouselData={carouselData}
+/>,
+     document.getElementById('root'));
