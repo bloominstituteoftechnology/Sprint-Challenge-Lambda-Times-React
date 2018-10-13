cd8a751921a3ace0c8bcd7dcb018eeb4de526721
stevekutz <stkutz@gmail.com>
2018-10-13 13:12:42 -0500

    building Cards, need to add a key from warningmessage but directions say card is only prop, adding key

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -3,14 +3,17 @@ import Card from './Card';
 
 const Cards = props => {
   console.log(props);   // cool!, they are showing up
-  
+
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {props.cards.map( card => <Card card = {card}/>  )}
+
+
+      {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
     </div>
   )
-}
+};
 
 // Make sure you include prop types for all of your incoming props
 
