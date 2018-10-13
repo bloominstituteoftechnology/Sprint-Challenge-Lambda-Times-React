6bdc7af3470cadbbec8f984586c9a565b22c71a4
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-13 13:50:28 -0400

    Header is styled

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {
+  HeaderStyle,
+  DateStyle,
+  Temp
+} from './Styles/Header';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <HeaderStyle>
+      <DateStyle>SMARCH 32, 2018</DateStyle>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <Temp>98°</Temp>
+    </HeaderStyle>
   )
 }
 
