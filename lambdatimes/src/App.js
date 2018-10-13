2876e9747f322ccb622abe458705ab9a2590106a
FreeKeyBoy <jjhutton81@gmail.com>
2018-10-13 10:01:29 -0700

    Got the cards to display. Lot's of errors on the console. Unused defenitions, and key prop errors, nothing that breaks the app. Moving on to next function, I'll debug later.

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,7 +3,15 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 
-const App = () => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render () {
   return (
     <div className="App">
       <TopBar />
@@ -11,6 +19,7 @@ const App = () => {
       <Content />
     </div>
   );
+}
 }
 
 export default App;
