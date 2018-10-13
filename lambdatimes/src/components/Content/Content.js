7f9570451f982e35d923ade90acde39489f4f348
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-10-13 10:51:16 -0600

    Tabs showing up

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,13 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: tabData });
+    this.setState({ cards: cardData });
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
@@ -48,8 +51,12 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
+        <Tabs tabs={this.state.tabs} 
+        selectedTab={this.state.selected}
+        selectTabHandler={this.changeSelected}
+        />
+        <Cards cards={this.filterCards()} 
+        />
       </div>
     );
   }
