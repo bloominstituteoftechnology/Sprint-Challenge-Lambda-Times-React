5e61fca2a26c4ea0818dc748fd5f87d1afca8d35
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-13 12:59:51 -0400

    cards is using propTypes

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
@@ -16,5 +17,7 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
-
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object)
+}
 export default Cards;
