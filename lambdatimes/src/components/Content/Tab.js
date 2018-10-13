8d7d8a36ff78e660d3ac14b67778e5364f92efff
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 18:51:33 -0400

    added console.log for props.selectTabHandler added ternerey operator to determine className passed seletTabHandler as props to onClick

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 const Tab = props => {
-  console.log('props', props);
+  console.log('tab props', props.selectTabHandler);
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
       if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
+      className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
       onClick={() => {
+        props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
     >
-      {/*props.tab.toUpperCase()*/}
+      {props.tab.toUpperCase()}
     </div>
   );
 };
