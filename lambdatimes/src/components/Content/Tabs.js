b6c7c23a2cde40ab59d697f6dfed11ef08e88dd4
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-13 14:23:57 -0400

    corrected map function that renders Tab components

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,6 +7,18 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          {props.tabs.map((tab, index) => {
+            return (
+              <div>
+                <Tab
+                  key={tab[index]}
+                  tab={props.tab}
+                  selectedTab={props.tab}
+                  selectTabHandler={props.changeSelected}
+                />
+              </div>
+            )
+          })}
       </div>
     </div>
   );
