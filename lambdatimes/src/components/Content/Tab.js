251e6bf3a21c31caf8cdee06e88ea3764d961136
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-10-13 12:08:44 -0600

    Added Styled Components
    I added styled components to the tabs for clarity when working further on them

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components'
 
+const StyledTab = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+color: #fff;
+background-color: #333;
+margin: 0 5px;
+padding: 2px 10px;
+font-size: 12px;
+letter-spacing: 2px;
+cursor: pointer;
+font-weight: bold;
+`
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
-    <div
+    <StyledTab
       className={''}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
@@ -13,7 +28,7 @@ const Tab = props => {
       }}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </StyledTab>
   );
 };
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import Tab from './Tab';
+import '../../CSS/index.css'
+import styled from 'styled-components'
+
+const TabsStyle = styled.div `
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+`
+
+const Topics = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+`
+
+const Title = styled.span`
+font-size: 12px;
+letter-spacing: 1px;
+font-weight: bold;
+`
+
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsStyle>
+      <Topics>
+        <Title>TRENDING TOPICS:</Title>
     {props.tabs.map(tab => <Tab key={tab} tab={tab}/>)}
-      </div>
-    </div>
+      </Topics>
+    </TabsStyle>
   );
 };
 
