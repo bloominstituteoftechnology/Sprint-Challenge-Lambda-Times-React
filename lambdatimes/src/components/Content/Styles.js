073889ded1130f9db78383cefa76c2511589667d
Troy Amlee <troyamlee19@gmail.com>
2018-10-14 16:44:46 -0500

    [chore] Added css components for Header

--- lambdatimes/src/components/Content/Styles.js
+++ lambdatimes/src/components/Content/Styles.js
@@ -86,3 +86,52 @@ export const StyledRightSpan = styled.span`
 cursor: pointer;
 `;
 
+//Header Styles
+
+export const StyledHeader = styled.div`
+display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+
+      width: 1280px;
+    
+  }
+`;
+
+export const StyledH1 = styled.h1`
+font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+export const StyledDateSpan = styled.span`
+align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`;
+
+export const StyledTempSpan = styled.span`
+align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
