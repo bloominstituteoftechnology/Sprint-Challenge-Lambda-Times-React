b94426057e4172b25a03581e922db4b02d55e3a5
Willie Yturralde <willieino@hotmail.com>
2018-10-13 19:00:51 -0700

    added styled-components

---
+++ lambdatimes/src/components/Styles/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+/* import { Nav } from 'reactstrap'; */
+
+const Nav = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: none;
+flex-direction: row;
+position: fixed;
+height: 44px;
+background-color: #333;
+`
+ 
+export default Nav;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,58 @@
 import React from 'react';
+import { Button, Input, NavItem } from 'reactstrap';
+import styled from 'styled-components';
+import Nav from './Styles/Nav'
+import Container from './Styles/Container'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
+const Navbar = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+cursor: pointer;
+margin-right: 25%;
+font-weight: bold;
+`
+const Navbar2 = styled.div`
+display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  cursor: pointer;
+`
+const NavLink = styled.a`
+margin-right: 10px;
+color: white;
+`
+const NavbarCenter = styled.div`
+cursor: pointer;
+  margin-right: 20%;
+`
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <Nav>
+      <Container>
+        <Navbar>
+          <NavLink>TOPICS</NavLink><NavLink>SEARCH</NavLink>
+        </Navbar >
+        <NavbarCenter >
+          <NavLink>GENERAL</NavLink><NavLink>BROWNBAG</NavLink><NavLink>RANDOM</NavLink><NavLink>MUSIC</NavLink><NavLink>ANNOUNCEMENTS</NavLink>
+        </NavbarCenter >
+        <Navbar2 >
+          <NavLink>LOG IN</NavLink>
+        </Navbar2 >
+      </Container>
+    </Nav>
   )
 }
 
