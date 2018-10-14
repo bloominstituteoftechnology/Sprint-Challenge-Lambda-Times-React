c228a54411f38a07a36fe1b5d132581fbc81fca1
Robin U Familara <robin.familara@cgius.com>
2018-10-14 09:44:06 -0500

    Encountered some errors on stretch goals -Just removed them -Removed Login.js -Performed minor modifications

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const HeaderCont = styled.div`
+  width: 100%;
+  height: 125px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  margin-top: 46px;
+  padding-bottom: 17px;
+  
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+  .date {
+    margin-left: 26px;
+    flex: 1;
+  }
+  .temperature {
+    text-align: right;
+    margin-right: 24px;
+    flex: 1;
+  }
+`
+
+const Title = styled.h1`
+  font-size: 67px;
+  font-family: Didot, serif;
+`
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderCont>
+      <span className="date">OCTOBER 12, 2018</span>
+      <Title>Lambda Times</Title>
+      <span className="temperature">98°</span>
+    </HeaderCont>
   )
 }
 
-export default Header
+export default Header;
