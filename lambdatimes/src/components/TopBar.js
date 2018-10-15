b8fa69175ceef48b41da618f3f5e3dd4c7f1023d
Javier Alavrez <javieralvarez82@live.com>
2018-10-14 23:28:58 -0700

    Completed Challenge MVP's

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -14,7 +14,7 @@ const Header = styled.div`
   position: fixed;
   height: 44px;
   background-color: #333;
-`
+`;
 
 const Container = styled.div`
   width: 100%;
@@ -25,7 +25,7 @@ const Container = styled.div`
   color: #fff;
   letter-spacing: 1px;
   padding: 0 10px;
-`
+`;
 
 const ContainerL = styled.div`
   display: flex;
@@ -34,7 +34,7 @@ const ContainerL = styled.div`
   flex-direction: row;
   flex: 1;
   font-size: 11px;
-`
+`;
 
 const ContainerC = styled.div`
   display: flex;
@@ -43,12 +43,14 @@ const ContainerC = styled.div`
   flex-direction: row;
   flex: 3;
   font-size: 9px;
-  text-decoration: underline;
-`
-const Span = styled.span`
-margin-right: 5%;
-`
+`;
 
+const Span = styled.span`
+  margin-right: 5%;
+  ${ContainerC}:hover & {
+  text-decoration: underline;
+}
+`;
 
 const ContainerR = styled.div`
   display: flex;
@@ -58,38 +60,26 @@ const ContainerR = styled.div`
   flex: 1;
   font-size: 11px;
   font-weight: bold;
-`
+`;
 
 const TopBar = () => {
   return (
-    // <div className="top-bar">
     <Header>
-      {/* <div className="container"> */}
-      <Container>
-        {/* <div className="container-left"> */}
-      <ContainerL>
-          <span>TOPICS</span><span>SEARCH</span>
-       </ContainerL>   
-        {/* </div> */}
-
-        {/* <div className="container-center"> */}
-        <ContainerC>
-          <Span>
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-         </Span>
-        </ContainerC>
-        {/* </div> */}
-
-        {/* <div className="container-right"> */}
-      <ContainerR>
-          <span>LOG IN</span>
-      </ContainerR>    
-        {/* </div> */}
-        </Container>
-
-      {/* </div> */}
+        <Container>
+            <ContainerL>
+                <Span> TOPICS </Span><span> SEARCH </span>
+            </ContainerL>
+          <ContainerC>
+              <Span>
+              <span> GENERAL </span><span> BROWN BAG </span><span> RANDOM </span><span> MUSIC </span>
+              <span> ANNOUNCEMENTS </span>
+            </Span>
+          </ContainerC>
+              <ContainerR>
+                  <span>LOG IN</span>
+              </ContainerR>
+          </Container>
     </Header>
-    // </div>
   )
 }
 
