b22734a6c80aa27160a70d429c19ffccc05b7f29
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-10-15 22:59:50 -0500

    changes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -4,12 +4,12 @@ import Proptypes from "prop-types";
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{props.card.headline}</div>
+      <div className="headline">{props.cards.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.img} />
+          <img src={props.cards.img} />
         </div>
-        <span>By {props.card.author}</span>
+        <span>By {props.cards.author}</span>
       </div>
     </div>
   );
