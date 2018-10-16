5dffbb5093153d9572505961fe807959e535cec8
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-16 01:12:59 -0400

    added media query for TopBarContainer

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -24,6 +24,10 @@ const TopBarContainer = styled.div`
   color: #fff;
   letter-spacing: 1px;
   padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
 `;
 
 const TBLeft = styled.div`
