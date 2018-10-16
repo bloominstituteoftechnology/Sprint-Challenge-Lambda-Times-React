18a52cdd91159e6715ece2c74dc51ac1a8ef52b1
Kyle Baker <k@gnomiq.com>
2018-10-15 21:15:06 -0700

    Cards

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
+    const {card} = props;
+
+    return (
+        <div className="card">
+            <div className="headline">{card.headline}</div>
+            <div className="author">
+                <div className="img-container">
+                    <img src={'' || card.img}/>
+                </div>
+                <span>By {card.author}</span>
+            </div>
         </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
-  );
+    );
 };
 
-// Make sure to include PropTypes.
+Card.propTypes = {
+    headline: PropTypes.string.isRequired,
+    img: PropTypes.string,
+    author: PropTypes.string.isRequired
+};
+
+Card.defaultProps = {
+    headline: 'An Article',
+    author: 'Unknown Author'
+};
 
 export default Card;
