2847c56dbaf78ca29b83e7cf3ff4b31eb23733c6
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-10-16 00:18:44 -0400

    corrected props

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,20 +1,27 @@
 import React from 'react';
+// import PropTypes form 'prop-types';
 
 const Card = props => {
   // console.log('card props', props);
   return (
     <div className="card">
-      <div className="headline">{props.headline}{/* headline goes here */}</div>
+      <div className="headline">{props.card.headline}{/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.img /* image source goes here */} />
+          <img src={props.card.img /* image source goes here */} />
         </div>
-        <span>By {props.author} {/* author goes here */}</span>
+        <span>By {props.card.author /* author goes here */}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+// Card.propTypes = {
+//   card: PropTypes.shapeOf({
+//
+//   })
+// }
+
 
 export default Card;
