5666af70b671e76c9759f60a7b3e0176d7b75d44
Alex Ostrander <alexanderostrander23@gmail.com>
2018-10-16 21:46:13 -0400

    Finished the content component

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -24,7 +24,7 @@ export default class Content extends Component {
   }
 
   changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
+    selected: tab
   };
 
   filterCards = () => {
@@ -40,6 +40,11 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+	  if (this.state.selected !== 'all') {
+		  this.setState({
+			  cards: cardData.filter(data => {return data.tab === this.state.selected})
+         });
+	  }
     return this.state.cards;
   };
 
@@ -51,7 +56,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.changeSelected} selectTabHandler={this.filterCards} />
         <Cards cards={this.filterCards()} />
       </div>
     );
