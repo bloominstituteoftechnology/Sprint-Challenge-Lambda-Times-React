fa3396cbf042fd2f726d9a61e2d6c27e99781124
jaklundun <govi.escudero@gmail.com>
2018-10-19 15:33:01 -0700

    Was able to get tabs to filter. Also installed npm package styled-components.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -20,7 +20,7 @@ export default class Content extends Component {
     // Once the component has mounted, get the data and reflect that data on the state.
     this.setState({
       tabs: tabData,
-      cards:cardData,
+      cards: cardData,
     })
   }
 
@@ -32,31 +32,23 @@ export default class Content extends Component {
   };
 
   filterCards = () => {
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
+    // this.state.selected === 'all' ? this.state.cards : cards.filter(card => card.tab === this.state.selected);
+  
+    // return this.state.cards;
 
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
-    this.state.selected === 'all' ? cardData : cardData.filter(card => card.tab === this.state.selected);
-    
+      if (this.state.selected==='all') {
+         return this.state.cards;
+           } else {
+          return this.state.cards.filter((card) => {
+           return card.tab === this.state.selected;
+      })
+    }
     return this.state.cards;
   };
 
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs
          tabs={this.state.tabs}
          selectedTab = {this.state.selected}
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,15 +4,12 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-
+      let selectedClass = (props.selectedTab === props.tab) ? 'tab active-lab' : 'tab';
   return (
     <div
-      className={`tab ${props.selected ? 'active-tab' : ''}`}
-      onClick={() => {props.selectTabHandler(props.tab)}
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }
-    >
+      className={selectedClass}
+      onClick = {() => {props.selectTabHandler(props.tab)}}
+      >
       {props.tab.toUpperCase()}
     </div>
   );
