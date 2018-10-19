6d3db59ef8aa1f2698e154fddd1df0bbd7dd4fce
Phil Hart <phil@Phils-iMac.local>
2018-10-19 11:56:15 -0600

    Got everyting to show, still need to do syled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import propTypes from 'prop-types';
 
 const Cards = props => {
   return (
     <div className="cards-container">
+      {props.cards.map(card =>
+      <Card 
+      card={card}
+      name={card.tab}
+      key={card.headline}
+      />
+    )}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
-  )
+
+
+
+
+
+
+
+)
 }
+
+
+
+
+  Cards.propTypes = {
+      card: propTypes.shape({
+      headline : propTypes.string,
+      tab : propTypes.string
+
+  })
+
+  }
+
 
 // Make sure you include prop types for all of your incoming props
 
