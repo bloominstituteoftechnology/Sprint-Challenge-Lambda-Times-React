b7db37b7c6a789167a263e91017aa55c6d99ac5a
mm2079 <vayne3252@gmail.com>
2018-10-19 13:51:30 -0400

    MVP Done

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  let className = 'tab';
+  if( props.tab === props.selectedTab )
+  {
+    className = 'tab active-tab';
+  }
   return (
     <div
-      className={''}
+      className={ className }
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        props.selectTabHandler( props.tab );
       }}
     >
       {props.tab.toUpperCase()}
@@ -18,5 +25,8 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
-
+Tab.PropTypes = 
+{
+  tab: PropTypes.string.isRequired
+}
 export default Tab;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,101 @@
 import React from 'react';
-
+import styledComponent from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
+  const TopContainer = styledComponent.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  `;
+
+  const MainContainer = styledComponent.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  @media (min-width: 1280px) {
+    .top-bar .container {
+      width: 1280px;
+    }
+  }
+  `;
+
+  const LeftContainer = styledComponent.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  > span
+  {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+  `;
+  
+  const CenterContainer = styledComponent.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  > span
+  {
+    cursor: pointer;
+    margin-right: 5%;
+    &:last-child
+    {
+      margin-right: 0;
+    }
+    &: hover
+    {
+      text-decoration: underline;
+    }
+  }
+  `;
+
+  const RightContainer = styledComponent.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  > span
+  {
+    cursor: pointer;
+  }
+  `;
+
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TopContainer>
+      <MainContainer>
+        <LeftContainer>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </LeftContainer>
+        <CenterContainer>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </CenterContainer>
+        <RightContainer>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </RightContainer>
+      </MainContainer>
+    </TopContainer>          
   )
 }
 
