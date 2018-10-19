947426b6269d039edffd63e6d62bbe027287f28b
jaklundun <govi.escudero@gmail.com>
2018-10-19 09:26:04 -0700

    Set up changeSelected handler in Content compnt.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -26,6 +26,9 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: tab,
+    })
   };
 
   filterCards = () => {
