46c52e621631b0cdc9890f4ed98f6a08bf80c9d0
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-10-19 13:30:37 -0400

    Css Complete

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,65 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const MainHeader = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: row;
+background-color: #fff;
+border-bottom: 1px solid lightgrey;
+width: 100%;
+height: 120px;
+margin-top: 44px;
+padding-bottom: 15px;
+
+MainHeader h1 {
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+}
+`;
+
+const HeaderH1 = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const Date = styled.span`
+  margin-left: 25px;
+  flex: 1;  
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+const Temp = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">MARCH 23, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <MainHeader>
+      <Date>MARCH 23, 2018</Date>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <Temp>98°</Temp>
+    </MainHeader>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,101 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopStyled = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const SpanLeft = styled.div`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const CenterSpan = styled.div`
+  cursor: pointer;
+  margin-right: 5%;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const SpanRight = styled.div`
+cursor: pointer;
+
+&:hover {
+  text-decoration: underline;
+}
+`;
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopStyled>
+      <TopContainer>
+        <ContainerLeft>
+          <SpanLeft>TOPICS</SpanLeft><SpanLeft>SEARCH</SpanLeft>
+        </ContainerLeft>
+        <ContainerCenter>
+          <CenterSpan>GENERAL</CenterSpan><CenterSpan>BROWNBAG</CenterSpan><CenterSpan>RANDOM</CenterSpan><CenterSpan>MUSIC</CenterSpan><CenterSpan>ANNOUNCEMENTS</CenterSpan>
+        </ContainerCenter>
+        <ContainerRight>
+          <SpanRight>LOG IN</SpanRight>
+        </ContainerRight>
+      </TopContainer>
+    </TopStyled>
   )
 }
 
