0247877d646a823f0a14c73b8c3f4dd7ceb5d074
jaklundun <govi.escudero@gmail.com>
2018-10-19 10:45:39 -0700

    Type-checked props for Tabs cmpnt.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+            props.tabs.map(tab => {
+              <Tab
+                selectTabHandler= {props.tabs.selectTabHandler}
+                selectedTab= {props.tabs.selectedTab}
+                tab= {tab} />
+            })}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+
+// yarn add prop-types
+
+// export const tabData = ['all', 'javascript', 'technology', 'node', 'React.js', 'bootstrap'];
+
+Tabs.propTypes = {
+ tabData : PropTypes.arrayOf(PropTypes.string.isRequired)
+}
+
 export default Tabs;
