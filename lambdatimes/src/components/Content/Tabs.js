9b7cd731d9caa816ffe5fff36155da1c01388f6b
Lambda_School_Loaner_27 <lambda_school_loaner_27@Lambda-School-Loaner-27s-MacBook-Pro.local>
2018-10-19 12:33:29 -0500

    At least the tabs are rendering. That's better than nothing.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Tab from './Tab';
 const Tabs = props => {
+
+
+
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.tabs.map( (tab, i) => {
+              return (
+                <Tab 
+                  key={i}
+                  tab={tab}
+                  onClick={props.selectTabHandler}
+                  selectedTab={props.selectedTab}/>
+              )
+            })}
       </div>
     </div>
   );
