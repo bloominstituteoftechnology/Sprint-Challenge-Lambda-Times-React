28632f8896e31a727102295a5566a60d74fa4ed2
Brandon <brandon.g.elzy@gmail.com>
2018-10-20 14:07:02 -0500

    Brandon G. Elzy - Completed part I

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
+          <img src={props.card.img} alt="card-author"/>
+       </div>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
 };
-
-// Make sure to include PropTypes.
 
 export default Card;
