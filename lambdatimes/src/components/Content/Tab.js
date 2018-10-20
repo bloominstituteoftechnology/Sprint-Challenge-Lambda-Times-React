f90c2fbf2e4d0ed85396b062321c44f658efb961
jbierly1 <james.bierly@gmail.com>
2018-10-20 14:56:02 -0400

    tabs displaying, but without words

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
+	let className = '';
+	if (props.tab === props.selectedTab) {
+		className = 'tab active-tab';
+	} else {
+		className = 'tab';
+	}
+	/* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  return (
-    <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
+	return (
+		<div
+			className={className}
+			onClick={() => props.selectTabHandler(props.tab)}
+			/* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
-      {props.tab.toUpperCase()}
-    </div>
-  );
+		/>
+	);
 };
 
 // Make sure you include PropTypes on your props.
