6d1fae56ea89e85b8a9a02153d86225859ca53ce
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-20 13:39:37 -0400

    Added index param to map statement for unique key prop.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
+import {cardData} from '../../data'
 
 const Cards = props => {
   return (
     <div className="cards-container">
-    {props.cards.map(card => {
-
-      
-
-       return  <Card card={card} />
-      
-      
-    })}
+    {props.cards.map((card, index) => <Card key={index} card={card} />
+    
+    )}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
@@ -19,5 +16,13 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cardObject: PropTypes.shape({
+    headline: PropTypes.string.isRequired,
+    tab: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
 
+  })
+}
 export default Cards;
