0374653a349e49c62d451cc41e2a1a75ea3b4fb0
cocoitali <cmburatto@gmail.com>
2018-10-20 10:27:44 -0700

    MVP (installed proptypes, type checked Card.js)

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Card = props => {
   return (
@@ -6,7 +7,7 @@ const Card = props => {
       <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img alt ='' src={props.card.img} />
+          <img alt="" src={props.card.img} />
         </div>
         <span>By {props.card.author}</span>
       </div>
@@ -14,6 +15,10 @@ const Card = props => {
   );
 };
 
-// Make sure to include PropTypes.
+Card.propTypes = {
+  headline: PropTypes.string,
+  author: PropTypes.string,
+  img: PropTypes.string
+};
 
 export default Card;
