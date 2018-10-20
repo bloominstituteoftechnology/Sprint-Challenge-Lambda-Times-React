00bff9397cbd9b0e1cc91f0dac15e748c699e706
cocoitali <cmburatto@gmail.com>
2018-10-20 10:54:17 -0700

    MVP (type checked Tabs.js, passed handlers)

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import Tab from './Tab';
+import React from "react";
+import Tab from "./Tab";
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-          {props.tabs.map (tab => (
-            <Tab tab={tab} />
-          ))}
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab => (
+          <Tab
+            tab={tab}
+            selectTabHandler={props.selectTabHandler}
+            selectedTab={props.selectedTab}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.PropTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+};
+
 export default Tabs;
