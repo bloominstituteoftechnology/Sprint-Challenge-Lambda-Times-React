aac029c1c4a83b3ddd924b5c3ff1a652e6c873f7
Jesse <theiseja@gmail.com>
2018-10-20 12:17:37 -0500

    Part 1 is complete, implenting Proptypes now

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -6,10 +6,11 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
+      className={props.selectedTab === props.tab ? 'tab active-tab' : 'tab'}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         props.selectTabHandler(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
