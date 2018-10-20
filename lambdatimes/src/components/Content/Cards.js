282152a7a3a90279b7ce4f74d1b5bb0b1d846f67
Jesse <theiseja@gmail.com>
2018-10-20 13:32:49 -0500

    Fixed indentation, MVP complete

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,11 +4,10 @@ import PropTypes from 'prop-types'
 
 const Cards = props => {
   return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-          {props.cards.map(card => 
-            <Card key={card.headline} card={card}/>)}
+    <div className='cards-container'>
+      {/* Using the cards prop, map over the list creating a
+          new Card component for each passing the card as the only prop */}
+      {props.cards.map(card => <Card key={card.headline} card={card} />)}
     </div>
   )
 }
