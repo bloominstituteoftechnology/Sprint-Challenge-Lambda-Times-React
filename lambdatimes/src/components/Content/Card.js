4ab00bf12d132a49fd6a2593402b6ee59ba8d08b
FBT328 <Katherine.Rust.Andrews@gmail.com>
2018-10-20 13:08:18 -0700

    Added answers, added content info, added card and cards info, working on tab and tabs info.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -3,17 +3,18 @@ import React from 'react';
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={props.card.img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+// TODO: Add PropTypes
 
 export default Card;
