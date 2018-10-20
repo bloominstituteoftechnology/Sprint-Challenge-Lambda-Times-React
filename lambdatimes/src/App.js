a11e49c0535409b33402254b7ea2d01612edf3dd
jbierly1 <james.bierly@gmail.com>
2018-10-20 13:03:22 -0400

    initial commit

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
-
+//initial commit
 const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<TopBar />
+			<Header />
+			<Content />
+		</div>
+	);
+};
 
 export default App;
