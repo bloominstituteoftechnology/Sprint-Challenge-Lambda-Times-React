be61679a7fa6114817830ede407267ebf152e618
unknown <jerrardsmith215@gmail.com>
2018-10-20 13:54:03 -0400

    Content.js corrected, Tabs & Tab.js completed (no PropTypes yet

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,13 +4,24 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+    let nameClass =  '';
+    if (props.selectedTab === props.tab) {
+      nameClass = 'tab active-tab';
+    }
+
+    else{
+      nameClass = 'tab';
+    }
+
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={nameClass}
+      onClick={() => 
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+        props.selectTabHandler(props.tab)
+      }
     >
       {props.tab.toUpperCase()}
     </div>
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,7 +8,7 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-            
+            {props.tabs.map(tabby => <Tab tab={tabby} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler}/>)}
       </div>
     </div>
   );
