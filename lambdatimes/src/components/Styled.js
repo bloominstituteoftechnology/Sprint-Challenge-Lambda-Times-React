9bdac90e8f644ae188b60037d98737defe2cd341
cocoitali <cmburatto@gmail.com>
2018-10-21 08:51:09 -0700

    STRETCH (created seperate file for styled-components)

---
+++ lambdatimes/src/components/Styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+//Cards.js
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+      width: 1200px;
+    }
+`;
