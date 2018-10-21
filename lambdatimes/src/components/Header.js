2739de5e8c9aa074e30a5d3c646d528f30eb0e70
2thumbstech <2thumbstech@gmail.com>
2018-10-20 18:21:40 -0600

    CSS

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import HT from '/CSS/HT.css'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-
-
 const Header = () => {
   return (
     <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+        <span className="date">SMARCH 32, 2018</span>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
+      <span className="temp">-45°</span>
     </div>
   )
 }
