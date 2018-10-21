6b037510be82d8e22057b9f87e358b92a6d4f263
Bharath <b.lavendra.95@gmail.com>
2018-10-20 19:09:29 -0500

    Edits, now functional!

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -9,7 +9,7 @@ const Card = props => {
       </div>
       <div className="author">
         <div className="img-container">
-          <img src={props.card.img /* image source goes here */} />
+          <img src={props.card.img} alt={props.card.img}/* image source goes here */ />
         </div>
         <span>By {props.card.author/* author goes here */}</span>
       </div>
