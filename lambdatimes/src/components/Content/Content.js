d1f3970e915423de23578e31cdab248157653aab
erscriber <erscriber@gmail.com>
2018-10-22 13:29:46 -0400

    completed code for cards.js and reworked filter code on content.js

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -42,14 +42,11 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    if (this.state.selected !== 'all') {
-      this.setState({
-        cards: cardData.filter(data =>{
-          return data.tab ===this.state.selected
-        })
-      });
+    if (this.state.selected === 'all') {
+       return this.state.cards;
+    } else {
+      return this.state.cards.filter(data => data.tab === this.state.selected)
     }
-    return this.state.cards;
   };
 
   render() {
