c58e48c2f1f72ffc819a6ab0275ee68218433b05
Orlando Castillo <omaddoc@gmail.com>
2018-10-22 20:55:14 -0400

    finished

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,40 @@
 import React from 'react';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <div style={header}>
+      <span style={date}>SMARCH 32, 2018</span>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
+      <span style={temp}>98°</span>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+const header = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexDirection: 'row',
+  backgroundColor: '#fff',
+  borderBottom: '1px solid lightgrey',
+  width: '100%',
+  height: '120px',
+  marginTop: '44px',
+  paddingBottom: '15px'
+};
+
+const date = {
+  marginLeft: '25px',
+  flex: '1'
+};
+
+const temp = {
+  textAlign: 'right',
+  marginRight: '25px',
+  flex: '1'
+};
+
+export default Header;
