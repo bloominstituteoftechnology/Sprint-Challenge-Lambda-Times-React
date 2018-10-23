b88c79e0c944cf97bbc358150dfadd61c194b6a5
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-10-22 22:08:13 -0400

    Adding changes for code review.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
 
-  //     if (props.tab === props.selectedTab()){
-  //       props.tabs.className = 'tab active-tab'
-  //     }
-  //     else { props.tabs.className = 'tab'}
-  // return (
+     
+       const tabChange = props.tab
+  return (
     <div
-      className={props}
-      onClick={props.selectTabHandler}
+      className={''}
+      onClick={() => {
+       
+        props.selectTab(tabChange)
+      }}
+
+        /* Replace this dummy click handler function with your selectTabHandler function from props 
+         you'll need to pass the `tab` in as an argument to this handler. */
+      
     >
-      {/* {props.tabs.toUpperCase()} */}
+      {props.tab.toUpperCase()}
     </div>
   );
 };
 
-// Make sure you include PropTypes on your props.
-
-this.props = {
-  props: PropTypes.string.isRequired
+Tab.propTypes = {
+  tabObject: PropTypes.shape({
+    tab: PropTypes.string.isRequired
+    
+  })
 }
+// Make sure you include PropTypes on your props.
 
 export default Tab;
---
+++ lambdatimes/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
