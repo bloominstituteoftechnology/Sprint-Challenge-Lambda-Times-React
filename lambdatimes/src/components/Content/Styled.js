9817b180d554cb1ce319338ec1110c4370820cb7
cocoitali <cmburatto@gmail.com>
2018-10-22 21:03:45 -0700

    FIX (refactored two more styled components that I had overlooked)

--- lambdatimes/src/components/Content/Styled.js
+++ lambdatimes/src/components/Content/Styled.js
@@ -10,6 +10,125 @@ export const AppWrapper = styled.div`
   font-family: "PT Sans", sans-serif;
 `;
 
+//TopBar.js
+export const TopBarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+export const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+export const TopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+
+  > span {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+`;
+
+export const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  > span {
+    cursor: pointer;
+    margin-right: 5%;
+    > :last-child {
+      margin-right: 0;
+    }
+    > :hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const TopBarContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+
+  > span {
+    cursor: pointer;
+  }
+`;
+
+//Header.js
+export const AppHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+export const HeaderH1 = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+export const Date = styled.span`
+  margin-left: 25px;
+  flex: 1;
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+export const Temp = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
 //Card.js
 export const CardStyled = styled.div`
   border-bottom: 1px solid lightgrey;
@@ -119,4 +238,58 @@ export const Title = styled.span`
   font-size: 12px;
   letter-spacing: 1px;
   font-weight: bold;
+`;
+
+//Carousel.js
+
+export const CarouselWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
+
+export const LeftButton = styled.div`
+  color: #333;
+  background-color: #fff;
+  border: 2px solid #333;
+  top: 50%;
+  left: 25px;
+  transform: translate(0, -50%);
+  > :hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+`;
+
+export const RightButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  top: 50%;
+  right: 25px;
+  transform: translate(0, -50%);
+  > :hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
 `;
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,49 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-const AppHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
-  background-color: #fff;
-  border-bottom: 1px solid lightgrey;
-  width: 100%;
-  height: 120px;
-  margin-top: 44px;
-  padding-bottom: 15px;
-  @media (min-width: 1280px) {
-    width: 1280px;
-  }
-`;
-
-const HeaderH1 = styled.h1`
-  font-size: 60px;
-  font-family: Didot, serif;
-  font-weight: bold;
-  flex: 8;
-  text-align: center;
-  color: #000;
-`;
-
-const Date = styled.span`
-  margin-left: 25px;
-  flex: 1;
-  align-self: flex-end;
-  font-size: 11px;
-  font-weight: bold;
-  letter-spacing: 1px;
-`;
-
-const Temp = styled.span`
-  text-align: right;
-  margin-right: 25px;
-  flex: 1;
-  align-self: flex-end;
-  font-size: 11px;
-  font-weight: bold;
-  letter-spacing: 1px;
-`;
+import { AppHeader, HeaderH1, Date, Temp } from "./Content/Styled";
 
 const Header = () => {
   return (
