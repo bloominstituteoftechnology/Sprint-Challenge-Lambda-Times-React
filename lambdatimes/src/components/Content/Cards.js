90736f352cd8270bfda77b528502f9158233b900
Trai Lynne <trailynne@gmail.com>
2018-11-16 11:39:56 -0700

    Cards: Passed Props to individual cards Iterated of props.cards

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,6 +6,8 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+
+          {props.cards.map(c => (<Card card={c} />))}
     </div>
   )
 }
