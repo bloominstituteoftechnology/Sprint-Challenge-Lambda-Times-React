8014dfbece13a411f057d166372efcc7779886e0
Aman <amsingh714@gmail.com>
2018-11-16 13:05:54 -0500

    added TopBar Style

---
+++ lambdatimes/src/CSS/Styles.js
@@ -0,0 +1,12 @@
+
+
+const TopBarStyle = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: none;
+    flex-direction: row;
+    position: fixed;
+    height: 44px;
+    background-color: #333;
+`;
