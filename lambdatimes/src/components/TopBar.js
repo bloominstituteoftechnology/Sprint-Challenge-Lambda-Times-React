9d2e6640402358f9aa39b0d4ba7bc01c72feff64
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-16 16:14:46 -0500

    "work in progress"

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -4,6 +4,94 @@ import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const TopBarSection = styled.div`
+  .top-bar {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  }
+  .top-bar .container {
+    width: 100%;
+    display: flex;
+    justify-content: none;
+    align-items: none;
+    flex-direction: row;
+    color: #fff;
+    letter-spacing: 1px;
+    padding: 0 10px;
+  }
+  @media (min-width: 1280px) {
+    .top-bar .container {
+      width: 1280px;
+    }
+  }
+  .top-bar .container .container-left {
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+    flex: 1;
+    font-size: 11px;
+  }
+  .top-bar .container .container-left span {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+  .top-bar .container .container-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex: 3;
+    font-size: 9px;
+  }
+  .top-bar .container .container-center span {
+    cursor: pointer;
+    margin-right: 5%;
+  }
+  .top-bar .container .container-center span:last-child {
+    margin-right: 0;
+  }
+  .top-bar .container .container-center span:hover {
+    text-decoration: underline;
+  }
+  .top-bar .container .container-right {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: row;
+    flex: 1;
+    font-size: 11px;
+    font-weight: bold;
+  }
+  .top-bar .container .container-right span {
+    cursor: pointer;
+  }
+`;
+
+const TopBarDiv = styled.div``;
+
+const TopContainerDiv = styled.div`
+  color: #fff;
+    letter-spacing: 1px;
+    `;
+
+const TopContainerLeft = styled.div``;
+
+const AllSpans = styled.span``;
+
+const TopContainerCenter = styled.div``;
+
+const TopContainerRight = styled.div``;
+
+
+
 
 const TopBar = () => {
   return (
