032336febc7ede985e4f3ade0815501bae0312b0
ToMMaaY <thuynh629@gmail.com>
2018-11-16 09:44:03 -0800

    Updated Card,js

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -5,7 +5,7 @@ const Cards = props => {
   return (
     <div className="cards-container">
       {props.cards.map(card => (
-        <Card />
+        <Card headline={card.headline} imgSrc={card.img} author={card.author} />
       ))}
     </div>
   );
