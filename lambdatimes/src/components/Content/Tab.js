193c3048a56748861ce99116ace0e8922312071a
Josh Z <jzieger10@gmail.com>
2018-11-16 12:20:36 -0500

    Set up state on Content, added required information to Tabs, Tab, and Cards components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,12 +4,19 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      console.log('ClassName', props.tab)
+      if (props.tab === props.selectedTab) {
+        // props.tab.className = 'tab active-tab'
+      } else {
+        // props.tab.className = 'tab'
+      }
   return (
     <div
       className={''}
-      onClick={() => {
+      onClick={(tab) => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        props.tab.selectedTabHandler(tab)
       }}
     >
       {props.tab.toUpperCase()}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+        
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          {props.tabs.map( tab => {
+            return (
+              <Tab 
+                selectedTabHandler={props.changeSelected}
+                selectedTab={props.selected}
+                tab={tab}
+                // key={props.cards.headline}
+              />)
+          })}
       </div>
     </div>
   );
 };
+
+Tabs.propTypes = {
+
+}
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
