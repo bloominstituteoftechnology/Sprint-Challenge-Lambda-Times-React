ee4a09df66f8a1c3a22120f1b679256f2fba902e
jheinz1999 <jonathan.heinz@aol.com>
2018-11-16 09:28:00 -0800

    added animaton to carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,41 +1,73 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
+import { carouselData } from '../../data';
+import {TweenMax} from 'gsap/TweenMax';
 // Complete this Carousel
-const images = ['./assets/carousel/computer.jpeg', './assets/carousel/mountains.jpeg', './assets/carousel/trees.jpeg', './assets/carousel/turntable.jpeg'];
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
       imgList: [],
-      currentIndex: 0
+      currentIndex: 0,
+      nextIndex: 0
     }
   }
   componentDidMount(){
 
-    this.setState({imgList: images});
+    this.setState({imgList: carouselData});
 
   }
 
   leftClick = () => {
 
     let newIndex = (this.state.currentIndex > 0) ? this.state.currentIndex - 1 : this.state.imgList.length - 1;
+    TweenMax.set('#nextImg', {xPercent: 100});
 
-    this.setState({currentIndex: newIndex});
+    this.setState({nextIndex: newIndex}, () => {
+
+      TweenMax.set('#nextImg', {display: 'block'});
+
+      TweenMax.to('#currentImg', 1, {xPercent: -100, onComplete: () => {
+
+        this.setState({currentIndex: newIndex});
+        TweenMax.set('#currentImg', {xPercent: 0});
+        TweenMax.set('#nextImg', {display: 'none'});
+
+      }});
+
+      TweenMax.to('#nextImg', 1, {xPercent: 0});
+
+    });
 
   }
 
   rightClick = () => {
 
     let newIndex = (this.state.currentIndex < this.state.imgList.length - 1) ? this.state.currentIndex + 1 : 0;
+    TweenMax.set('#nextImg', {xPercent: -100});
 
-    this.setState({currentIndex: newIndex});
+    this.setState({nextIndex: newIndex}, () => {
+
+      TweenMax.set('#nextImg', {display: 'block'});
+
+      TweenMax.to('#currentImg', 1, {xPercent: 100, onComplete: () => {
+
+        this.setState({currentIndex: newIndex});
+        TweenMax.set('#currentImg', {xPercent: 0});
+        TweenMax.set('#nextImg', {display: 'none'});
+
+      }});
+
+      TweenMax.to('#nextImg', 1, {xPercent: 0});
+
+    });
 
   }
 
   render(){
     return (
       <div className="carousel">
-        <img src={this.state.imgList[this.state.currentIndex]} style={{display: 'block'}} />
+        <img id='nextImg' src={this.state.imgList[this.state.nextIndex]} style={{display: 'block', position: 'absolute'}}/>
+        <img id='currentImg' src={this.state.imgList[this.state.currentIndex]} style={{display: 'block', position: 'absolute'}} />
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
