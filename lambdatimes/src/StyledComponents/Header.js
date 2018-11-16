0ad783c132b2f4215c336663ae965ff58adb5b94
Lidii <lidiiany92@gmail.com>
2018-11-16 13:45:35 -0500

    Refactored Header Component to use a react styled component

--- lambdatimes/src/StyledComponents/Header.js
+++ lambdatimes/src/StyledComponents/Header.js
@@ -13,4 +13,36 @@ export const HeaderContainer = styled.div`
     margin-top: 44px;
     padding-bottom: 15px;
 
+    @media (min-width: 1280px) {
+          width: 1280px;
+      }
+
+`;
+
+export const HeaderHeading = styled.h1`
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+`;
+
+export const HeaderDate = styled.span`
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    margin-left: 25px;
+    flex: 1;
+`;
+
+export const HeaderTemp = styled.span`
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    text-align: right;
+    margin-right: 25px;
+    flex: 1;
 `;
--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HeaderContainer } from '../StyledComponents/Header';
+import { HeaderContainer, HeaderHeading, HeaderDate, HeaderTemp } from '../StyledComponents/Header';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
+      <HeaderDate>SMARCH 32, 2018</HeaderDate>
+      <HeaderHeading>Lambda Times</HeaderHeading>
+      <HeaderTemp>98°</HeaderTemp>
     </HeaderContainer>
   )
 }
