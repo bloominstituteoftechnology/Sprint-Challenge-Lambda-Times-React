81336a87824f2f50f1ddda8258e394509751af5b
Dennis Orbison <orbison.dennis@gmail.com>
2018-11-16 09:27:12 -0800

    proptypes added, card and tab components displaying and functional

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+        {props.tabs.map( tab => {
+          return <Tab 
+            tab={tab} 
+            selectTabHandler={props.selectTabHandler}
+            selectedTab={props.selectedTab}/>
+        })}
+
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
@@ -13,4 +21,11 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string,
+}
+
 export default Tabs;
