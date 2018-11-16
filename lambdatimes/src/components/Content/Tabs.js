e6ba4be28a8cfb28020c54aa4f4f4e75583a3292
jamespagedev <jamespagedev@outlook.com>
2018-11-16 10:04:44 -0700

    TopBar styling complete

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
+  console.log(props);
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab => {return <Tab selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={tab}></Tab>})}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string,
+  tabs: PropTypes.array
+}
+
 export default Tabs;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,105 @@
 import React from 'react';
-
+import styled from "styled-components";
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+/***************************************************************************************************
+********************************************** Styles **********************************************
+***************************************************************************************************/
+const DivTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const DivContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const DivContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const SpanContainerLeft = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+const DivContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const SpanContainerCenter = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+  &:last-child {
+    margin-right: 0;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const DivContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const SpanContainerRight = styled.span`
+  cursor: pointer;
+`;
+
+/***************************************************************************************************
+********************************************* Component ********************************************
+***************************************************************************************************/
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <DivTopBar>
+      <DivContainer>
+        <DivContainerLeft>
+          <SpanContainerLeft>TOPICS</SpanContainerLeft><SpanContainerLeft>SEARCH</SpanContainerLeft>
+        </DivContainerLeft>
+        <DivContainerCenter>
+          <SpanContainerCenter>GENERAL</SpanContainerCenter><SpanContainerCenter>BROWNBAG</SpanContainerCenter><SpanContainerCenter>RANDOM</SpanContainerCenter><SpanContainerCenter>MUSIC</SpanContainerCenter><SpanContainerCenter>ANNOUNCEMENTS</SpanContainerCenter>
+        </DivContainerCenter>
+        <DivContainerRight>
+          <SpanContainerRight>LOG IN</SpanContainerRight>
+        </DivContainerRight>
+      </DivContainer>
+    </DivTopBar>
   )
 }
 
