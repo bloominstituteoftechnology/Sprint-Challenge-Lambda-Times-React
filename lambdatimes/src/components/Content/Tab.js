dcd4d02581a506c51c44fb0e5acae28554f6ffc3
BrandCam <BrandCam>
2018-11-16 11:25:03 -0600

    tab style changes on select

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,13 +4,24 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  return (
+  return props.tab === props.selectedTab ? (
     <div
-      className={""}
+      className={"tab active-tab"}
       onClick={() => {
         props.selectTabHandler(props.tab);
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+      }}
+    >
+      {props.tab.toUpperCase()}
+    </div>
+  ) : (
+    <div
+      className={"tab"}
+      onClick={() => {
+        props.selectTabHandler(props.tab);
+        /* Replace this dummy click handler function with your selectTabHandler function from props 
+           you'll need to pass the `tab` in as an argument to this handler. */
       }}
     >
       {props.tab.toUpperCase()}
