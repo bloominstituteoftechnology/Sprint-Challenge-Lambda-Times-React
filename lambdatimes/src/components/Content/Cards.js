6a3b67ccf5c8152315181f2215ea7999374a943a
jheinz1999 <jonathan.heinz@aol.com>
2018-11-16 08:29:10 -0800

    finshed Cards.js

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
-const Cards = props => {
+const Cards = ({cards}) => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {cards.map(card => <Card data={card} />)}
     </div>
   )
 }
