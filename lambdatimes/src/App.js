8353bff4a7e181f601180173a98d601559e80e62
kmarker1101 <kmarker1101@icloud.com>
2018-11-16 10:53:14 -0600

    Part 1 complete

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
-import TopBar from './components/TopBar';
-import Header from './components/Header';
-import Content from './components/Content/Content';
+import React, {Component} from "react";
+import TopBar from "./components/TopBar";
+import Header from "./components/Header";
+import Content from "./components/Content/Content";
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <TopBar/>
+                <Header/>
+                <Content/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import Tab from './Tab';
+import React from "react";
+import Tab from "./Tab";
+import PropTypes from "prop-types";
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          {props.tabs.map((item, i) => (
+              <Tab
+                  key={i}
+                  tab={item}
+                  selectedTab={props.selectedTab}
+                  selectTabHandler={props.selectTabHandler}
+              />
+          ))}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+    tabs: PropTypes.arrayOf(PropTypes.string)
+};
 export default Tabs;
