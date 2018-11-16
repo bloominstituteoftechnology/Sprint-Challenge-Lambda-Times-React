90c0904258f3b02512006ed208c7deeef599c359
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-11-16 09:48:23 -0800

    Add part I and part II funcitonality

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -6,8 +7,9 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
+      className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
       onClick={() => {
+        props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
@@ -18,5 +20,10 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab : PropTypes.string.isRequired,
+  selectTabHandler : PropTypes.func.isRequired,
+  selectedTab : PropTypes.string.isRequired,
+};
 
 export default Tab;
