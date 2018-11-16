6110d896189423130c005e60bfb785c1ce9fc3c1
jheinz1999 <jonathan.heinz@aol.com>
2018-11-16 08:30:50 -0800

    finished Card.js

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Card = props => {
+const Card = ({data}) => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{data.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={data.img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {data.author}</span>
       </div>
     </div>
   );
