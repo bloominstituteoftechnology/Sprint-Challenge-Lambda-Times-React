9efa2524a9a2a721926af5fb0a2a67af81531456
jheinz1999 <jonathan.heinz@aol.com>
2018-11-16 09:07:28 -0800

    completed Carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
-// Complete this Carousel 
+// Complete this Carousel
+const images = ['./assets/carousel/computer.jpeg', './assets/carousel/mountains.jpeg', './assets/carousel/trees.jpeg', './assets/carousel/turntable.jpeg'];
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      imgList: [],
+      currentIndex: 0
     }
   }
   componentDidMount(){
+
+    this.setState({imgList: images});
 
   }
 
   leftClick = () => {
 
+    let newIndex = (this.state.currentIndex > 0) ? this.state.currentIndex - 1 : this.state.imgList.length - 1;
+
+    this.setState({currentIndex: newIndex});
+
   }
 
   rightClick = () => {
 
+    let newIndex = (this.state.currentIndex < this.state.imgList.length - 1) ? this.state.currentIndex + 1 : 0;
+
+    this.setState({currentIndex: newIndex});
+
   }
 
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
   render(){
     return (
       <div className="carousel">
+        <img src={this.state.imgList[this.state.currentIndex]} style={{display: 'block'}} />
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -64,6 +65,7 @@ export default class Content extends Component {
           and `selectTabHandler` that includes the function to change the selected tab
         */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
