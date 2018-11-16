b166795b43bdb9e405d29a301f01790cb8d8b2b5
Yasirah Krueng <ykrueng@Lambda-School-Loaner-39s-MacBook-Pro.local>
2018-11-16 11:41:46 -0600

    refactor Tab component to use styled components and remove the corresponding css styles from index.css

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,17 +1,38 @@
-// libraries import
+// Importing libraries
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// Styled Component
+const TabComp = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const Tab = props => {
   // destructuring props for readability
   const {tab, selectedTab, selectTabHandler} = {...props};
   return (
-    <div
+    <TabComp
       className={`tab ${tab === selectedTab ? 'active-tab' : ''}`}
       onClick={() => selectTabHandler(tab) }
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabComp>
   );
 };
 
