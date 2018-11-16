44a46c215e0c4a09791c027dee89f40de73bd811
Yasirah Krueng <ykrueng@Lambda-School-Loaner-39s-MacBook-Pro.local>
2018-11-16 11:46:01 -0600

    refactor Cards component to use styled components and remove the corresponding css styles from index.css

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,22 +1,38 @@
-// libraries impor
+// Importing libraries
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
 
-// components import
+// Importing component
 import Card from './Card';
+
+// Styled Component
+const CardContainerComp = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   // destructuring props for readability
   const {cards} = {...props};
   return (
-    <div className="cards-container">
+    <CardContainerComp className="cards-container">
       {
         // map over the tabs
         cards.map((card, i) => (
           <Card key={i} card={card} />
         ))
       }
-    </div>
+    </CardContainerComp>
   )
 }
 
