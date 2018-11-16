f40069014557279fed7d1357c3bdfb208d573d81
David Freitag <freitagdavid@outlook.com>
2018-11-16 12:03:04 -0500

    lambdatimes/src/components/Content/Content: Basic functions complete filtercards still todo

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -7,25 +7,33 @@ import Cards from './Cards';
 import { tabData, cardData } from '../../data';
 
 export default class Content extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: 'all',
-      tabs: [],
-      cards: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected : 'all',
+            tabs     : [],
+            cards    : []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            selected : this.props.selected,
+            tabs     : tabData,
+            cards    : cardData
+        });
+        // Once the component has mounted, get the data and reflect that data on the state.
+    }
+
+    changeSelected = (tab) => {
+        // this function should take in the tab and update the state with the new tab.
+        this.setState({
+            selected : tab
+        });
     };
-  }
 
-  componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
-  }
-
-  changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
-  };
-
-  filterCards = () => {
-    /* Right now this function only returns the cards on state.
+    filterCards = () => {
+        /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
       
@@ -37,20 +45,24 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
-  };
+        return this.state.cards;
+    };
 
-  render() {
-    return (
-      <div className="content-container">
-        {/* 
+    render() {
+        return (
+            <div className="content-container">
+                {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
-      </div>
-    );
-  }
+                <Tabs
+                    tabs={this.state.tabs}
+                    selectedTab={this.state.selectedTab}
+                    selectedTabHandler={this.changeSelected}
+                />
+                <Cards cards={this.filterCards()} />
+            </div>
+        );
+    }
 }
