b3e54401c88aac9b01d424a8874445f5abe5688e
Chaya Otikor <c.otikor@gmail.com>
2018-11-16 14:13:11 -0700

    Used styled components on Tabs/Tab

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import styled from './../../../node_modules/styled-components';
 
-const Tab = (props) => {
+const Div = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+color: ${props => props.inactive ?'#fff' : '#333'};
+background-color: ${props => props.inactive ?'#333' : '#fff' };
+border: ${props => props.inactive ? null : '2px solid #333' };
+margin: 0 5px;
+padding: 2px 10px;
+font-size: 12px;
+letter-spacing: 2px;
+cursor: pointer;
+font-weight: bold;
+:hover {
+	text-decoration: underline;
+}
+`
+
+
+
+class Tab extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			inactive: true
+		}
+	}
 	/* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-	return (
-		<div
-			id={props.tab}
-			className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
+	render (){
+		return (
+		<Div 
+			inactive = {this.state.inactive}
+			id={this.props.tab}
 			onClick={(event) => {
-				props.selectTabHandler(event);
+				this.props.selectTabHandler(event);
+				this.setState({
+					inactive: false
+				})
+				
 			}}
 		>
-			{props.tab}
-		</div>
+			{this.props.tab}
+		</Div>
 	);
+		}
 };
 Tab.propTypes = {
 	tab: PropTypes.string.isRequired,
