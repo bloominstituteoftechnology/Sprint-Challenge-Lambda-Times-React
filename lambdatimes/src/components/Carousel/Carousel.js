084b182f13ce6f4198b814c7cf73108c5dea5d95
Chaya Otikor <c.otikor@gmail.com>
2018-11-16 13:27:55 -0700

    Refactored TopBar to remove repeated styling

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,68 +1,120 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import { carouselData } from '../../data';
+import styled from '../../../node_modules/styled-components';
+
+const CarouselSection = styled.section`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	height: 500px;
+	position: relative;
+	overflow: hidden;
+	margin-top: 16px;
+	@media (min-width: 1200px) {
+		width: 1200px;
+	}
+`;
+
+const CarouselButton = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: none;
+	flex-direction: row;
+	color: #fff;
+	background-color: #333;
+	font-size: 40px;
+	border-radius: 50%;
+	position: absolute;
+	width: 50px;
+	height: 50px;
+	cursor: pointer;
+	transform: translate(0, -50%);
+	top: 50%;
+	left: ${(props) => (props.left ? '25px' : null)};
+	right: ${(props) => (props.right ? '25px' : null)};
+	:hover {
+		color: #333;
+		background-color: #fff;
+		border: 2px solid #333;
+	}
+`;
+
+const Image = styled.img`
+	width: 100%;
+	display: none;
+`;
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      images: [],
-      imageIndex: 0
-    }
-  }
-  componentDidMount(){
-    this.setState({
-      images: carouselData
-    })
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			images: [],
+			imageIndex: 0
+		};
+	}
+	componentDidMount() {
+		this.setState({
+			images: carouselData
+		});
+	}
 
-  leftClick = () => {
-    if (this.state.imageIndex === 0){
-      this.setState({
-        imageIndex: 3
-      })
-    } else if(this.state.imageIndex === 3) {
-      this.setState({
-        imageIndex: 2
-      })
-    } else if(this.state.imageIndex === 2) {
-      this.setState({
-        imageIndex: 1
-      })
-    } else{
-      this.setState({
-        imageIndex: 0
-      })
-    }
-  }
+	leftClick = () => {
+		if (this.state.imageIndex === 0) {
+			this.setState({
+				imageIndex: 3
+			});
+		} else if (this.state.imageIndex === 3) {
+			this.setState({
+				imageIndex: 2
+			});
+		} else if (this.state.imageIndex === 2) {
+			this.setState({
+				imageIndex: 1
+			});
+		} else {
+			this.setState({
+				imageIndex: 0
+			});
+		}
+	};
 
-  rightClick = () => {
-    if (this.state.imageIndex === 0){
-      this.setState({
-        imageIndex: 1
-      })
-    } else if(this.state.imageIndex === 1) {
-      this.setState({
-        imageIndex: 2
-      })
-    } else if(this.state.imageIndex === 2) {
-      this.setState({
-        imageIndex: 3
-      })
-    } else{
-      this.setState({
-        imageIndex: 0
-      })
-    }
-  }
+	rightClick = () => {
+		if (this.state.imageIndex === 0) {
+			this.setState({
+				imageIndex: 1
+			});
+		} else if (this.state.imageIndex === 1) {
+			this.setState({
+				imageIndex: 2
+			});
+		} else if (this.state.imageIndex === 2) {
+			this.setState({
+				imageIndex: 3
+			});
+		} else {
+			this.setState({
+				imageIndex: 0
+			});
+		}
+	};
 
-  
-  render(){
-    return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <img src={this.state.images[this.state.imageIndex]} alt='current' style={{display: 'block'}}/>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<CarouselSection>
+				<CarouselButton left onClick={this.leftClick}>
+					{'<'}
+				</CarouselButton>
+				<Image
+					src={this.state.images[this.state.imageIndex]}
+					alt="current"
+					style={{ display: 'block' }}
+				/>
+				<CarouselButton right onClick={this.rightClick}>
+					{'>'}
+				</CarouselButton>
+			</CarouselSection>
+		);
+	}
 }
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,94 +1,79 @@
 import React from 'react';
 import styled from './../../node_modules/styled-components';
 
-
-const TopBarSection = styled.section `
-width: 100%;
-display: flex;
-justify-content: center;
-align-items: none;
-flex-direction: row;
-position: fixed;
-height: 44px;
-background-color: #333;
-`
-const ContainerDiv = styled.div `
-width: 100%;
-display: flex;
-justify-content: none;
-align-items: none;
-flex-direction: row;
-color: #fff;
-letter-spacing: 1px;
-padding: 0 10px;
-@media (min-width: 1280px) {
-  
-    width: 1280px;
+const Section = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center ${(props) => (props.main ? 'center' : 'none')};
+  align-items: none;
+  flex-direction: row;
+  position: ${(props) => (props.main ? 'fixed' : null)};
+  height: ${(props) => (props.main ? '44px' : null)};
+  background-color: ${(props) => (props.main ? '#333' : null)};
+  z-index: ${(props) => (props.main ? '10' : null)};
+  color: ${(props) => (props.secondary ? '#fff' : null)};
+  letter-spacing: ${(props) => (props.secondary ? '1px' : null)};;
+  padding: ${(props) => (props.secondary ? '0 10px' : null)};
+  @media (min-width: 1280px) {
+    width: ${(props) => (props.secondary ? '1280px' : null)}; ;
   }
 }
-`
-const LeftDiv = styled.div `
-display: flex;
-  justify-content: none;
-  align-items: center;
-  flex-direction: row;
-  flex: 1;
-  font-size: 11px;
-`
+`;
 
-const Span1 = styled.span `
-cursor: pointer;
-  margin-right: 25%;
-  font-weight: bold;
-`
+const Div = styled.div`
+	display: flex;
+	justify-content: ${(props) =>
+		props.left
+			? 'none'
+			: props.center
+			? 'center'
+			: props.right
+			? 'flex-end'
+			: null};
+	align-items: center;
+	flex-direction: row;
+	flex: ${(props) =>
+		props.left ? '1' : props.center ? '3' : props.right ? '1' : null};
+	font-size: ${(props) =>
+		props.left ? '11px' : props.center ? '9px' : props.right ? '11px' : null};
+	font-weight: ${(props) => (props.right ? 'bold' : null)};
+`;
 
-const CenterDiv = styled.div `
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: row;
-flex: 3;
-font-size: 9px;
-`
-const Span2 = styled.span `
-cursor: pointer;
-margin-right: 5%;
-:hover {
-  text-decoration: underline;
-}
-`
-const RightDiv = styled.div`
-display: flex;
-justify-content: flex-end;
-align-items: center;
-flex-direction: row;
-flex: 1;
-font-size: 11px;
-font-weight: bold;
-`
+const Span = styled.span`
+	cursor: pointer;
+	margin-right: ${(props) =>
+		props.left ? '25%' : props.center ? '5%' : props.last ? '0' : null};
+	font-weight: ${(props) => (props.left ? 'bold' : 'normal')};
+	:hover {
+		text-decoration: ${(props) =>
+			props.center ? 'underline' : props.last ? 'underline' : 'none'};
+	}
+`;
 
-const Span3 = styled.span `
-cursor: pointer;
-`
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
-  return (
-    <TopBarSection>
-      <ContainerDiv>
-      <LeftDiv>
-          <Span1>TOPICS</Span1><Span1>SEARCH</Span1>
-        </LeftDiv>
-        <CenterDiv >
-          <Span2>GENERAL</Span2><Span2>BROWNBAG</Span2><Span2>RANDOM</Span2><Span2>MUSIC</Span2><Span2>ANNOUNCEMENTS</Span2>
-        </CenterDiv>
-        <RightDiv>
-          <Span3>LOG IN</Span3>
-        </RightDiv>
-      </ContainerDiv>
-    </TopBarSection>
-  )
-}
+	return (
+		<Section main>
+			<Section secondary>
+				<Div left>
+					<Span left>TOPICS</Span>
+					<Span left>SEARCH</Span>
+				</Div>
+				<Div center>
+					<Span center>GENERAL</Span>
+					<Span center>BROWNBAG</Span>
+					<Span center>RANDOM</Span>
+					<Span center>MUSIC</Span>
+					<Span last>ANNOUNCEMENTS</Span>
+				</Div>
+				<Div right>
+					<Span>LOG IN</Span>
+				</Div>
+			</Section>
+		</Section>
+	);
+};
 
 export default TopBar;
