c09aac861e55c536af4b117efc2ee58f93b0d5da
BrandCam <BrandCam>
2018-11-16 12:58:18 -0600

    carousel styled with styled components

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,5 +1,70 @@
 import React, { Component } from "react";
 import { carouselData } from "../../data";
+import styled from "styled-components";
+
+const CarouselContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  img {
+    width: 100%;
+    display: none;
+  }
+`;
+const CarouselButtonLeft = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  :hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+  top: 50%;
+  left: 25px;
+  transform: translate(0, -50%);
+`;
+const CarouselButtonRight = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  :hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+  top: 50%;
+  right: 25px;
+  transform: translate(0, -50%);
+`;
+
 // Complete this Carousel
 export default class Carousel extends Component {
   constructor(props) {
@@ -50,15 +115,13 @@ export default class Carousel extends Component {
 
   render() {
     return (
-      <div className="carousel">
+      <CarouselContainer>
         {this.selectedImage()}
-        <div className="left-button" onClick={this.leftClick}>
-          {"<"}
-        </div>
-        <div className="right-button" onClick={this.rightClick}>
+        <CarouselButtonLeft onClick={this.leftClick}>{"<"}</CarouselButtonLeft>
+        <CarouselButtonRight onClick={this.rightClick}>
           {">"}
-        </div>
-      </div>
+        </CarouselButtonRight>
+      </CarouselContainer>
     );
   }
 }
