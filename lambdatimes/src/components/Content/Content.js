9ddffd68889e33a9945c043ea9a974cc9e4bc6fb
Yasirah Krueng <ykrueng@Lambda-School-Loaner-39s-MacBook-Pro.local>
2018-11-16 12:12:55 -0600

    add Carousel component to App

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 // Importing components
 import Tabs from './Tabs';
+import Carousel from '../Carousel/Carousel';
 import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
@@ -53,6 +54,7 @@ export default class Content extends Component {
           selectedTab={this.state.selected}
           selectTabHandler={this.changeSelected}
         />
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </ContentContainer>
     );
