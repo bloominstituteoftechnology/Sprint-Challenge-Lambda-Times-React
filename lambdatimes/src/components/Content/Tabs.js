f89c03033d26701920fbdfcb9550f8a09a40ed9f
Yasirah Krueng <ykrueng@Lambda-School-Loaner-39s-MacBook-Pro.local>
2018-11-16 10:23:00 -0600

    update Tabs component to map over tabs and to validate props types

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,39 @@
+// libraries import
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// components import
 import Tab from './Tab';
+
 const Tabs = props => {
+  // destructuring props for readability
+  const {tabs, selectedTab, selectTabHandler} = {...props}
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {
+          // map over the tabs
+          tabs.map(
+            (tab, i) => (
+              <Tab
+                key={i}
+                tab={tab}
+                selectedTab={selectedTab}
+                selectTabHandler={selectTabHandler}
+              />
+            ))
+        }
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired,
+
+}
+
 export default Tabs;
