e1ba34ec20c61b4c950f80ded1e3756493998350
jamespagedev <jamespagedev@outlook.com>
2018-11-16 10:14:59 -0700

    Header styling complete

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,67 @@
 import React from 'react';
-
+import styled from "styled-components";
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+/***************************************************************************************************
+********************************************** Styles **********************************************
+***************************************************************************************************/
+const DivHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const SpanHeaderDate = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const H1Header = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+
+const SpanHeaderTemp = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
+/***************************************************************************************************
+********************************************* Component ********************************************
+***************************************************************************************************/
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <DivHeader>
+      <SpanHeaderDate>MARCH 32, 2018</SpanHeaderDate>
+      <H1Header>Lambda Times</H1Header>
+      <SpanHeaderTemp>98°</SpanHeaderTemp>
+    </DivHeader>
   )
 }
 
