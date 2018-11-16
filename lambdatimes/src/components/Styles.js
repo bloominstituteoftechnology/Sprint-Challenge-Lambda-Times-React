330767c7a3c7b9c8fba88ddb1a6b950f06213f6f
Justin <jdlow2@gmail.com>
2018-11-16 12:39:42 -0500

    styled components for Header. MVP complete.

--- lambdatimes/src/components/Styles.js
+++ lambdatimes/src/components/Styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// TopBar component
 export const StyledTopBar = styled.div`
   width: 100%;
   display: flex;
@@ -71,4 +72,50 @@ export const ContainerRight = styled.div`
   span {
     cursor: pointer;
   }
+`;
+
+// Header component
+
+export const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+`;
+
+export const Temp = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
+export const Date = styled.span`
+  margin-left: 25px;
+  flex: 1;
 `;
