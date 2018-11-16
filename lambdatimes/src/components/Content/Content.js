48ad81211ec5017f3d7ed122306fd8b2b8ee8904
dyiar <austin.hl.blake@gmail.com>
2018-11-16 09:18:41 -0800

    Got all the content to show up on the page

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,7 +18,10 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-  }
+    this.setState({ tabs: [...tabData],
+      cards: [...cardData]})
+}
+
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
@@ -37,7 +40,7 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    // return this.state.cards;
   };
 
   render() {
@@ -49,7 +52,7 @@ export default class Content extends Component {
           and `selectTabHandler` that includes the function to change the selected tab
         */}
         <Tabs tabs={this.state.tabs} />
-        <Cards cards={this.filterCards()} />
+        <Cards cards={this.state.cards} />
       </div>
     );
   }
