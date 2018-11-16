5e17f071c66c4887414249cf5614030c4e827ceb
steelworx <eileencuevas62@gmail.com>
2018-11-16 12:17:24 -0500

    Props passed are now validated via PropTypes

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -19,5 +20,11 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  text: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string,
+}
 
 export default Tab;
