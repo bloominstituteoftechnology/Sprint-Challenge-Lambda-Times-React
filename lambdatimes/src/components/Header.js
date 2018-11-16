ae0174d118473c841d70afe08c37d5fa1d8174e7
Mr. Metelus <Mdmetelus@gmail.com>
2018-11-16 16:14:10 -0500

    "Work in progress"

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -18,30 +18,25 @@ const HeaderSection = styled.div`
   @media (min-width: 1280px) {
     width: 1280px;
   }
-  h1 {
-    font-size: 60px;
-    font-family: Didot, serif;
-    font-weight: bold;
-    flex: 8;
-    text-align: center;
-    color: #000;
-  }
-  span {
-    align-self: flex-end;
-    font-size: 11px;
-    font-weight: bold;
-    letter-spacing: 1px;
-  }
 `;
 
 const TheDateIs = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
   margin-left: 25px;
   flex: 1;
   letter-spacing: 1px;
 `;
 
 const Headline = styled.h1`
-  color: #000;
+  font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
   `;
 
 const Temperature = styled.span`
