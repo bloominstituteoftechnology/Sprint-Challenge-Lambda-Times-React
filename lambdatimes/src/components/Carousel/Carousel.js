3f7490e95145459670c7d1913e50a536ff7ca7e1
Justin <jdlow2@gmail.com>
2018-11-16 13:18:36 -0500

    stretch: added carousel with infinite scroll

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,45 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
+      images: [],
+      currentImage: 0,
 
     }
   }
   componentDidMount(){
+    this.setState({ images: carouselData });
 
   }
 
   leftClick = () => {
+    const length = this.state.images.length;
+    let nextImg = this.state.currentImage - 1;
+    if (nextImg < 0) {
+      nextImg = length  -1;
+    }
+    this.setState({ currentImage: nextImg});
 
   }
 
   rightClick = () => {
+    const length = this.state.images.length;
+    let nextImg = this.state.currentImage + 1;
+    if (nextImg >= length) {
+      nextImg = 0;
+    }
+    this.setState({ currentImage: nextImg});
 
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.images[this.state.currentImage]} alt="" style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
