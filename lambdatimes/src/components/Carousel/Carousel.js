3069c00b2cf0ad2f7b8be520431fada37e78e42b
BrandCam <BrandCam>
2018-11-16 12:38:00 -0600

    carousel added

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,64 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import React, { Component } from "react";
+import { carouselData } from "../../data";
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      caroselImgs: [],
+      selectedImageIndex: 0
+    };
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({
+      caroselImgs: carouselData
+    });
   }
 
   leftClick = () => {
-
-  }
+    if (this.state.selectedImageIndex === 0) {
+      this.setState({
+        selectedImageIndex: this.state.caroselImgs.length - 1
+      });
+    } else {
+      this.setState({
+        selectedImageIndex: this.state.selectedImageIndex - 1
+      });
+    }
+  };
 
   rightClick = () => {
-
-  }
+    if (this.state.caroselImgs.length - 1 === this.state.selectedImageIndex) {
+      this.setState({
+        selectedImageIndex: 0
+      });
+    } else {
+      this.setState({
+        selectedImageIndex: this.state.selectedImageIndex + 1
+      });
+    }
+  };
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+    return (
+      <img
+        src={this.state.caroselImgs[this.state.selectedImageIndex]}
+        style={{ display: "block" }}
+      />
+    );
+  };
+
+  render() {
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        {this.selectedImage()}
+        <div className="left-button" onClick={this.leftClick}>
+          {"<"}
+        </div>
+        <div className="right-button" onClick={this.rightClick}>
+          {">"}
+        </div>
       </div>
-    )
+    );
   }
 }
