fe400990251ac0adda198d2a70e7e815a6dac846
wcolts2000 <wcolts2000@gmail.com>
2018-11-16 11:43:35 -0800

    added carousel component during lunch

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,60 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import React, { Component } from "react";
+import { carouselData } from "../../data";
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      slider: [],
+      selectedImageIndex: 0
+    };
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({
+      slider: carouselData
+    });
   }
 
   leftClick = () => {
-
-  }
+    this.setState({
+      selectedImageIndex:
+        this.state.selectedImageIndex === 0
+          ? this.state.slider.length - 1
+          : this.state.selectedImageIndex - 1
+    });
+  };
 
   rightClick = () => {
+    this.setState({
+      selectedImageIndex:
+        this.state.selectedImageIndex === this.state.slider.length - 1
+          ? 0
+          : this.state.selectedImageIndex + 1
+    });
+  };
 
-  }
+  selectedImageIndex = () => {
+    return (
+      <img
+        src={this.state.slider[this.state.selectedImageIndex]}
+        style={{ display: "block" }}
+      />
+    );
+  };
 
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+  render() {
+    console.log("SLIDER STATE LENGTH", this.state.slider.length);
+
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <div className="left-button" onClick={this.leftClick}>
+          {"<"}
+        </div>
+        {this.selectedImageIndex()}
+        <div className="right-button" onClick={this.rightClick}>
+          {">"}
+        </div>
       </div>
-    )
+    );
   }
 }
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 import Tabs from "./Tabs";
 import Cards from "./Cards";
+import Carousel from "../Carousel/Carousel";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from "../../data";
+import { tabData, cardData, carouselData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
@@ -63,6 +64,7 @@ export default class Content extends Component {
           selectedTab={this.state.selected}
           selectedTabHandler={this.changeSelected}
         />
+        <Carousel carouselData={carouselData} />
         <Cards
           cards={this.filterCards()}
           selectedTabState={this.state.selected}
