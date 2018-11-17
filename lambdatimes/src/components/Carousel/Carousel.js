b633afe06b1d309f029a18b1dfaf92519363838d
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-11-16 18:00:27 -0600

    carousel working

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,33 +1,49 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
+
+
 export default class Carousel extends Component {
   constructor(props){
+    console.log(props)
     super(props);
     this.state = {
-
+      data: [],
+      Currentindex: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      data: carouselData,
+      Currentindex: 0
+    })
   }
 
   leftClick = () => {
-
+    if(this.state.Currentindex === 0) {
+      this.setState({Currentindex: this.state.data.length-1})
+    } else {
+      this.setState({Currentindex: this.Currentindex-1})
+    }
   }
 
   rightClick = () => {
-
+    if(this.state.Currentindex === this.state.data.length-1) {
+      this.setState({Currentindex: 0})
+    } else {
+      this.setState({Currentindex: this.state.Currentindex+1})
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.data[this.state.Currentindex]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 import Tabs from './Tabs';
+import Carousel from './../Carousel/Carousel';
 import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+import styled from 'styled-components';
+
+const Contentcontainer = styled.div `
+display: flex; 
+flex-direction: column; 
+align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -37,21 +45,10 @@ export default class Content extends Component {
     }
   }
 
-                                   /* Right now this function only returns the cards on state.
-                                                    We're going to make this function more dynamic
-                                                  by using it to filter out our cards for when a tab is selcted
-          
-                                                          Notice that we're passing this function to our <Cards /> component below.
-                                                          This function returns an array of cards, so we can just pass it down as such.
-                                                    
-                                                          Your algorithim for the logic here is as follows: 
-                                                            - if the selected tab is 'all' it should return all 
-                                                              of the items from cardData. 
-                                                            - else, it should only return those cards whose 'tab' matched this.state.selected.
-                                                        */
+                        
 render() {
     return (
-      <div className="content-container">
+      <Contentcontainer>
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
@@ -62,10 +59,12 @@ render() {
           selectedTab={this.state.selected}
           selectTabHandler={this.changeSelected}
          />
+         
+         <Carousel/>
         <Cards 
           cards={this.filterCards()} 
         />
-      </div>
+      </Contentcontainer>
     );
   }
 }
