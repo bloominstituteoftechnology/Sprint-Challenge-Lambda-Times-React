3f9ced506ad7da0dae105525e7af0994b5683191
1@$@1github <asaofdiamonds@gmail.com>
2018-11-30 18:22:54 -0700

    styled-cpmponents for Header.JS completed

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,17 +1,54 @@
 import React from 'react';
-//import sc from 'styled-components';
+import sc from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderDiv = sc.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+const HeaderDate = sc.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`;
+const HeaderH1 = sc.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+const HeaderTemp = sc.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderDiv>
+      <HeaderDate>SMARCH 32, 2018</HeaderDate>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <HeaderTemp>98°</HeaderTemp>
+    </HeaderDiv>
   )
 }
 
-export default Header
+export default Header;
