c91c42b11aa4339df2d7a03938f52ea0fe4c526d
David Situ <davidsitu626@gmail.com>
2018-11-30 17:09:59 -0800

    David Situ part 1 - changeSelected, filterCards Functioning part 2 - PropTypes, pending part 3 - Styled Components

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -16,39 +16,58 @@ export default class Content extends Component {
     };
   }
 
-  componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
+  componentDidMount(tab) {
+    this.setState({
+      cards: cardData,
+      tabs: tabData
+    })
   }
 
-  changeSelected = tab => {
+  changeSelected = (tab) => {
     // this function should take in the tab and update the state with the new tab.
+    console.log(tab);
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
+      by using it to filter out our cards for when a tab is selected
+
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
 
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
+      Your algorithim for the logic here is as follows:
+        - if the selected tab is 'all' it should return all
+          of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
-    return this.state.cards;
+
+        1. if this.state.selected == all return this.state.cards
+        2. else
+        2. filter for cards.tab that has the same name as the this.state.selected
+    */console.log('filtering')
+    if(this.state.selected == 'all'){
+      return this.state.cards
+    }else{
+      return(this.state.cards.filter(card =>
+        card.tab == this.state.selected
+      ))
+    };
   };
 
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
+        {/*
+          Add 2 props to the Tabs component,
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
+      if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
+      if(props.selectedTab == props.tab){
+      return <div className={'tab active-tab'}>{props.tab.toUpperCase()}</div>};
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={'tab'} onClick={() => {props.selectTabHandler(props.tab)}}
     >
       {props.tab.toUpperCase()}
     </div>
