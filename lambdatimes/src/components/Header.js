007f32db01baf6c4eb28902533a24ae90588bf00
Alex <alexander.t.carter@gmail.com>
2018-12-06 15:46:38 -0800

    styled components successfully created

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderDiv = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: row;
+	background-color: #fff;
+	border-bottom: 1px solid lightgrey;
+	width: 100%;
+	height: 120px;
+	margin-top: 44px;
+	padding-bottom: 15px;
+
+	@media (min-width: 1280px) {
+	  .header {
+	    width: 1280px;
+	  }
+	}
+`;
+
+const HeaderDivH1 = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const HeaderSpanDate = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const HeaderSpanTemp = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderDiv>
+      <HeaderSpanDate>SMARCH 32, 2018</HeaderSpanDate>
+      <HeaderDivH1>Lambda Times</HeaderDivH1>
+      <HeaderSpanTemp>98°</HeaderSpanTemp>
+    </HeaderDiv>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,8 +1,108 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const StyledTopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    .top-bar .container {
+      width: 1280px;
+    }
+  }
+`;
+
+const StyledTopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const StyledTopBarContainerLeftSpan = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+const StyledTopBarContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const StyledTopBarContainerCenterSpan = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const StyledTopBarContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+
+const StyledTopBarContainerRightSpan = styled.span`
+  cursor: pointer;
+`;
+
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+
+const TopBar = () => {
+  return (
+    <StyledTopBar>
+      <StyledTopBarContainer>
+        <StyledTopBarContainerLeft>
+          <StyledTopBarContainerLeftSpan>TOPICS</StyledTopBarContainerLeftSpan><StyledTopBarContainerLeftSpan>SEARCH</StyledTopBarContainerLeftSpan>
+        </StyledTopBarContainerLeft>
+        <StyledTopBarContainerCenter>
+          <StyledTopBarContainerCenterSpan>GENERAL</StyledTopBarContainerCenterSpan><StyledTopBarContainerCenterSpan>BROWNBAG</StyledTopBarContainerCenterSpan><StyledTopBarContainerCenterSpan>RANDOM</StyledTopBarContainerCenterSpan><StyledTopBarContainerCenterSpan>MUSIC</StyledTopBarContainerCenterSpan><StyledTopBarContainerCenterSpan>ANNOUNCEMENTS</StyledTopBarContainerCenterSpan>
+        </StyledTopBarContainerCenter>
+        <StyledTopBarContainerRight>
+          <StyledTopBarContainerRightSpan>LOG IN</StyledTopBarContainerRightSpan>
+        </StyledTopBarContainerRight>
+      </StyledTopBarContainer>
+    </StyledTopBar>
+  )
+}
+
+export default TopBar;
+
+//copy of code being refactored for backup
+
 /*
 .top-bar {
   width: 100%;
@@ -73,36 +173,5 @@ import styled from 'styled-components';
   cursor: pointer;
 }
 */
-/*
-const TopBarDiv = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: none;
-  flex-direction: row;
-  position: fixed;
-  height: 44px;
-  background-color: #333;
-`;
-*/
 
-
-const TopBar = () => {
-  return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default TopBar;
+ 
