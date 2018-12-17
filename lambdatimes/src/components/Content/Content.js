30a15abd0892275c70310e919aa8d57665876962
vanceleon <vanceleon44@gmail.com>
2018-12-17 00:02:20 -0500

    content component fixes

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
-      tabs: [], 
+      selected: "all",
+      tabs: [],
       cards: []
     };
   }
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-    this.setState({tabs: tabData});
-    this.setState({cards: cardData});
+    this.setState({ tabs: tabData, cards: cardData });
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    
-
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
@@ -35,40 +33,29 @@ export default class Content extends Component {
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
-
       Your algorithim for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
+          of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
 
-    //object reference
-  //  {console.log(tabData)}
-  //  {console.log(cardData[0].tab)}
-
-
-  //  if(this.tabs === this.cards.tab){
-  //     return this.state.selected;
-  //  }
- };
-
+   if (this.state.selected === "all") return this.state.cards;
+   return this.state.cards.filter(card => card.tab === this.state.selected);
+  }
   render() {
     return (
       <div className="content-container">
-        
-        
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs 
-          tabs={this.state.tabs} 
-          selectedTab={this.changeSelected}
-          selectTabHandler={this.filterCards}/>
-        <Cards 
-          cards={this.filterCards()}
-          />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
+        <Cards cards={this.filterCards()} />
       </div>
     );
   }
