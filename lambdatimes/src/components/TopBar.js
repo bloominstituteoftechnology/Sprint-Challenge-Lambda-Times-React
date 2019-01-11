a254555d3b3279bea98bf5dd4120cd95b44005a7
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2019-01-11 10:41:36 -0500

    wrapping up hour 3 sprint challenge

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-
+// const TopBarWrap = styled.div`
+// width: 100%;
+// display: flex;
+// justify-content: center;
+// align-items: none;
+// flex-direction: row;
+// position: fixed;
+// height: 44px;
+// background-color: #333;
+// `;
 
 const TopBar = () => {
   return (
