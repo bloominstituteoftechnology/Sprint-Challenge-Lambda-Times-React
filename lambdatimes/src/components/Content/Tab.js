528c01739a002a72ef12de2f337987497fbc3307
fbt328 <katherine.rust.andrews@gmail.com>
2019-01-12 10:33:13 -0800

    tab and tabs component should be functioning, inspecting elements shows changes on click, but there are no cards for visual changes yet. Will come back to them once cards are set up and displaying data.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -8,6 +8,7 @@ const Tab = props => {
     <div
       className={''}
       onClick={() => {
+        props.selectedTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,8 +5,15 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((aTab, i) => {
+          return (
+          <Tab 
+          selectedTabHandler={props.selectedTabHandler} 
+          selectedTab={props.selectedTab} 
+          tab={aTab}
+          key={i}
+          />);
+        })}
       </div>
     </div>
   );
@@ -14,3 +21,5 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+{/* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
