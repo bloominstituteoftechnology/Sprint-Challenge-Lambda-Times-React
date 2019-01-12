caf11e6d7b2ea53f17d57d1f5a75c5eaa6c5d9a6
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-01-12 12:04:23 -0600

    Finished Tabs.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import Tab from './Tab';
 import PropTypes from 'prop-types';
 
-const Tabs = props => {
-  return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-      </div>
-    </div>
-  );
+const Tab = (props) => {
+	return (
+		<div
+			id={props.tab}
+			className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
+			onClick={(event) => {
+				props.selectTabHandler(event);
+			}}
+		>
+			{props.tab}
+		</div>
+	);
 };
 
-// Make sure to use PropTypes to validate your types!
-export default Tabs;
+
+Tab.propTypes = {
+	tab: PropTypes.string.isRequired,
+	selectedTab: PropTypes.string,
+	selectTabHandler: PropTypes.func
+};
+
+export default Tab;
