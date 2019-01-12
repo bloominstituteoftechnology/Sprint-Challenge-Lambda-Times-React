c71723f95570dcba6f48887f7fc25823f7cde06f
david-chua <dp_chua@hotmail.com>
2019-01-12 14:45:29 -0500

    finished carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,7 +5,8 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-      carousel: []
+      carousel: [],
+      selectedCarouselIndex: 0
 
     }
   }
@@ -14,15 +15,25 @@ export default class Carousel extends Component {
   }
 
   leftClick = () => {
-
+    if (this.state.selectedCarouselIndex == 0){
+      this.setState({ selectedCarouselIndex: 3})
+    } else {
+      this.setState({ selectedCarouselIndex:  this.state.selectedCarouselIndex -1 })
+    }
   }
 
   rightClick = () => {
-
+    if (this.state.selectedCarouselIndex == 3){
+      this.setState({ selectedCarouselIndex: 0})
+    } else {
+      this.setState({
+        selectedCarouselIndex: this.state.selectedCarouselIndex + 1
+      })
+    }
   }
 
   selectedImage = () => {
-    return <img src={this.state.carousel[0]} style={{display: 'block'}} />
+    return <img src={this.state.carousel[this.state.selectedCarouselIndex]} style={{display: 'block'}} />
   }
 
   render(){
