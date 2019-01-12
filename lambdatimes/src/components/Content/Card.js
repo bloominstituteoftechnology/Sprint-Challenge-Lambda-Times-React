3d46b99355b045597ef123352bcf46df2ee6f202
mustafa inaya <moose.inaya@icloud.com>
2019-01-12 09:40:08 -0800

    CREATE, Created a Pull Request.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
-  );
+const Card = (props) => {
+	return (
+		<div className="card">
+			<div className="headline">{/* headline goes here */}</div>
+			<div className="author">
+				<div className="img-container">
+					<img src={props.img} />
+				</div>
+				<span>By {/* author goes here */}</span>
+			</div>
+		</div>
+	);
 };
 
 // Make sure to include PropTypes.
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+const Cards = (props) => {
+	return (
+		<div className="cards-container">
+			{/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
-    </div>
-  )
-}
+		</div>
+	);
+};
 
 // Make sure you include prop types for all of your incoming props
 
