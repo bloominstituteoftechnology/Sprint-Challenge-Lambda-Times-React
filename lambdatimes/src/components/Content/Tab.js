322ab8505b402043201157439b0a9301fc626aab
Leila <nb.leila10@gmail.com>
2019-01-12 12:23:36 -0600

    Change styled in TopBar to use styled-components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -7,7 +7,6 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-      const classes = props.selectedTab === props.tab ? 'tab active-tab' : 'tab';
   return (
     <div
       className= { props.selectedTab === props.tab ? 'tab active-tab' : 'tab' }
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import { TopBarWrapper, TopBarContainer, TopBarContainerLeft, TopBarContainerCenter, TopBarContainerRight } from '../styles/TopBarStyles';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TopBarWrapper>
+      <TopBarContainer>
+        <TopBarContainerLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </TopBarContainerLeft>
+        <TopBarContainerCenter>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </TopBarContainerCenter>
+        <TopBarContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </TopBarContainerRight>
+      </TopBarContainer>
+    </TopBarWrapper>
   )
 }
 
