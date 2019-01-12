1ad8a5b050a15a5be789446054663efb3da0bcee
Trip1701 <bj2336@email.vccs.edu>
2019-01-12 13:13:05 -0500

    Header converted to styled component

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <HeaderContainer>
+      <Date>SMARCH 32, 2018</Date>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <Temp>98°</Temp>
+    </HeaderContainer>
   )
 }
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  max-width: 1280px;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+
+  @media (min-width: 1280px) {
+    .header {
+      width: 1280px;
+    }
+  }
+`;
+
+const Date = styled.span`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const Temp = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
 
 export default Header
