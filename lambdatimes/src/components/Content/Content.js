690684fd03e0fea3474489152b250e858e98576b
echopecho <pbsmithtbn@gmail.com>
2019-01-12 12:02:10 -0600

    Add Tab functionality
    I completed the required code to allow the user to filter cards based of the tab clicked. It was just a matter of adding a conditional that included a filter based the state of the selected tab.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -29,6 +29,7 @@ export default class Content extends Component {
     this.setState({
       selected: tab
     })
+    console.log(this.state.selected)
   };
 
   filterCards = () => {
@@ -44,7 +45,12 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+   if(this.state.selected === 'all') {
+     return this.state.cards;
+    }else {
+     return this.state.cards.filter(card => card.tab === this.state.selected)
+   }
+    // return this.state.cards;
   };
 
   render() {
