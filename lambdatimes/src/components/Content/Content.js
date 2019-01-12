0a4c638f03218857c3c85dd57843dcabf6b96d0b
Kseniya Platonava <ksu2008@gmail.com>
2019-01-12 13:09:07 -0500

    Answers And Cards And Tabs Appear On The Screen

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,6 +18,8 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({ tabs: [...tabData],
+      cards: [...cardData]})
   }
 
   changeSelected = tab => {
