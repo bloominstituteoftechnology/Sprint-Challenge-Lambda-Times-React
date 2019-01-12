a031a7e50fb95b0465247ca0cda8c4b59c7752dc
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-01-12 12:32:28 -0600

    Refactored Header

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
-  );
+const Card = (props) => {
+	return (
+		<div className="card">
+			<div className="headline">{props.headline}</div>
+			<div className="author">
+				<div className="img-container">
+					<img src={props.img} alt="Author Thumbnail" />
+				</div>
+				<span>By {props.author}</span>
+			</div>
+		</div>
+	);
 };
 
-// Make sure to include PropTypes.
 
+Card.propTypes = {
+	headline: PropTypes.string.isRequired,
+	img: PropTypes.string.isRequired,
+	author: PropTypes.string.isRequired
+};
 export default Card;
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,75 +1,46 @@
 import React, { Component } from 'react';
-
 import Tabs from './Tabs';
 import Cards from './Cards';
-
-// Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
+      selected: '',
       tabs: [],
       cards: []
     };
   }
 
   componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
-     this.setState({
-      tabs: tabData,
-      cards: cardData,
-      selected: ""
-    });
-  }
-
-  changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
     this.setState({
       tabs: tabData,
       cards: cardData
     })
-  };
-
-  filterCards = () => {
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
-
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
-   changeSelected = event => {
-    this.setState({
-      selected: event.target.id
-    })
-    if (this.state.selected === 'all') {
-      this.setState({
-        cards: cardData
-      })
-    } else {
-      this.setState({
-        cards: cardData.filter(card => card.tab === this.state.selected)
-    })
   }
+
+  changeSelected = event => {
+      this.setState({
+        selected: event.target.id
+      })
+      if (this.state.selected === 'all') {
+        this.setState({
+          cards: cardData
+        })
+      } else {
+        this.setState({
+          cards: cardData.filter(card => card.tab === this.state.selected)
+      })
+    }
   };
-};
 
   render() {
+    console.log(this.state.selected)
+
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={(event) => this.changeSelected(event)}/>
         <Cards cards={this.state.cards} />
       </div>
