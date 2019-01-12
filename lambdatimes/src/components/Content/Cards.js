a40321b689922bf0b91fd605984e3324425239b0
Alando Appling <alandoappling@gmail.com>
2019-01-11 21:12:31 -0800

    Finishing up MVP

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map((card,i) => <Card key={i} card={card}/>)}_
     </div>
   )
 }
 
-// Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      heading: PropTypes.string,
+      tab: PropTypes.string,
+      img: PropTypes.string,
+      author: PropTypes.string
+    })
+  )
+};// Make sure you include prop types for all of your incoming props
 
 export default Cards;
