a9b13e452fcff62921434f5fa5564acccc38f06d
dickwinters <coding1965@gmail.com>
2019-01-12 12:28:21 -0600

    configured TopBar to styled Components

---
+++ lambdatimes/src/components/styledComponents.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const TopBarStyle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+export const TBContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+export const TBLContainer = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+export const TBLContainerContent = styled.div`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+export const TBLContainerContentCenter = styled.div`
+  span {
+    cursor: pointer;
+    margin-right: 5%;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export const TBRContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+
+  span {
+    cursor: pointer;
+  }
+`;
