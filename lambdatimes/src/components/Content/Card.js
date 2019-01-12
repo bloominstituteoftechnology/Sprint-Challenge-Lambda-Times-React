08756ff65414e8c9e040fd67628878aa16ac352a
Lloyd Napier <lloyd.a.napier@gmail.com>
2019-01-12 12:54:08 -0800

    Tab, Tabs, Card, Cards and Content complete with changeSelected and filterCards

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
+import propTypes from 'prop-types';
+
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{props.card.headline/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={props.card.img} alt={props.card.headline/* image source goes here */} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author/* author goes here */}</span>
       </div>
     </div>
   );
 };
 
+Card.propTypes = {
+  card: propTypes.shape({
+    headline: propTypes.string.isRequired,
+    tab: propTypes.string.isRequired,
+    img: propTypes.string.isRequired,
+    author: propTypes.string.isRequired,
+  })
+};
 // Make sure to include PropTypes.
 
 export default Card;
