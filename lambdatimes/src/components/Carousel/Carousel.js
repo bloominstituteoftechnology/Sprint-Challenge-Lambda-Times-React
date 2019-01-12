a12c06dfba22869c84bfd73ec7273c4b57e08c76
echopecho <pbsmithtbn@gmail.com>
2019-01-12 13:21:57 -0600

    Add Carousel
    I completed the code needed to add a functional carousel of images to the page. Included in the functionality is the ability to infinitly scroll in either direction. The carousel will just cycle throw the images in order forever.

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,41 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
+      selected: 0
     }
   }
   componentDidMount(){
-
+    this.setState({
+      images: carouselData
+    })
   }
 
   leftClick = () => {
-
+    if(this.state.selected <= 0) {
+      this.setState({ selected: 3 })
+    } else {
+      this.setState({ selected: this.state.selected - 1 })
+    }
   }
 
   rightClick = () => {
-
+    if(this.state.selected >= 3) {
+      this.setState({ selected: 0 })
+    } else {
+      this.setState({ selected: this.state.selected + 1})
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.images[this.state.selected]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
