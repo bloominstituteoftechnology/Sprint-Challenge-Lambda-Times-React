ddf031c4880431e53a114b958a39d6a17fd3aa29
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-01-12 13:07:31 -0500

    ADD SELECTED TAB PROP & SELECT HANDLER PROP TO TABS COMPONENT

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -63,7 +63,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs selected={this.state.selected} selectTabHandler={this.filterCards} tabs={this.state.tabs} />
         <Cards cards={this.filterCards()} />
       </div>
     );
