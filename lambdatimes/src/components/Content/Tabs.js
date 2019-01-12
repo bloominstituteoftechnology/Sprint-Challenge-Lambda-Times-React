7b6406bc5a5e1ae4a3c6fc3bbd5e0ba5e10fd891
Ashley Owens <ash4741@gmail.com>
2019-01-12 14:20:18 -0500

    Correct Tab.js Error
    Got the tabs to display correctly and fixed type error in Tab.js

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -8,7 +8,7 @@ const Tabs = props => {
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
           {props.tabs.map(tab => (
-            <Tab key = {tab.tab} tab = {tab.tab} selectTabHandler = {tab.selectTabHandler} selectedTab = {tab.selectedTab} />
+            <Tab  tab = {tab} selectTabHandler = {tab.selectTabHandler} selectedTab = {tab.selectedTab} />
           ))}
           
       </div>
