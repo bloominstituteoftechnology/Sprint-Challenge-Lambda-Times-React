211df81557a96b295df13aa9750063fc829f4d45
jrizza88 <jetorres88@gmail.com>
2019-01-12 16:13:15 -0500

    MVP complete

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -4,13 +4,52 @@ import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderCss = styled.div` 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+      width: 1280px;
+    }
+`;
+
+const H1 = styled.h1`
+font-size: 60px;
+font-family: Didot, serif;
+font-weight: bold;
+flex: 8;
+text-align: center;
+color: #000;
+`;
+
+const HeaderSpan = styled.span`
+align-self: flex-end;
+font-size: 11px;
+font-weight: bold;
+letter-spacing: 1px;
+flex: 1;
+
+${props => props.type === `date` ? `margin-left: 25px;`: null};
+${props => props.type === `temp`? `text-align: right;`: null};
+${props => props.type === `temp` ? `margin-right: 25px;`: null};
+`
+
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderCss>
+      <HeaderSpan type="date">SMARCH 32, 2018</HeaderSpan>
+      <H1>Lambda Times</H1>
+      <HeaderSpan type="temp">98°</HeaderSpan>
+    </HeaderCss>
   )
 }
 
