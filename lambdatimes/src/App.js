a88867c83a53b258d5b2800e4c7fe1a799554ea1
CatsAreMLG <joshuadgon@gmail.com>
2019-01-12 12:34:58 -0600

    Changed Card and Cards components to styled components

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import TopBar from "./components/TopBar";
 import Header from "./components/Header";
 import Content from "./components/Content/Content";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: "PT Sans", sans-serif;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <StyledApp>
       <TopBar />
       <Header />
       <Content />
-    </div>
+    </StyledApp>
   );
 };
 
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Card from "./Card";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledCardContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <StyledCardContainer>
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
       {props.cards.map((card, i) => (
         <Card key={`card-${i}`} card={card} />
       ))}
-    </div>
+    </StyledCardContainer>
   );
 };
 
