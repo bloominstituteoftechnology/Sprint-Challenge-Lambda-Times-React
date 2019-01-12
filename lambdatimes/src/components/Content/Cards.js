b647afccb20a9095b384ff40681c77eb578e2cfb
Paul DeYonghe <p.deyonghe@gmail.com>
2019-01-12 12:12:28 -0600

    worked on some other sections, unsure if I am doing this right

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,6 +6,10 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map(card, ind) =>
+        return Card card = {card}
+        key = {ind}
+      }
     </div>
   )
 }
