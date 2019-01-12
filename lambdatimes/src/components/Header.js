ce67ad4936fe767dff289a08f1409ae0b794433d
ltann <leeroyleeroy18@gmail.com>
2019-01-12 12:45:55 -0800

    used styled components for Header and TopBar

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -15,13 +15,30 @@ const HeaderContainer = styled.div`
   height: 120px;
   margin-top: 44px;
   padding-bottom: 15px;
+  
 `
+const SpanStyle = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`
+
+const H1Style = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`
+
 const Header = () => {
   return (
     <HeaderContainer >
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
+      <SpanStyle>SMARCH 32, 2018</SpanStyle>
+      <H1Style>Lambda Times</H1Style>
+      <SpanStyle>98°</SpanStyle>
     </HeaderContainer>
   )
 }
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,86 @@
 import React from 'react';
+import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarMain = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`
+
+const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`
+
+const TopBarLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`
+const TopBarLeftSpan = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`
+
+const TopBarCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`
+const TopBarCenterSpan = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+`
+
+const TopBarRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`
+
+const TopBarRightSpan = styled.div`
+  margin-right: 0;
+`
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopBarMain>
+      <TopBarContainer>
+        <TopBarLeft>
+          <TopBarLeftSpan>TOPICS</TopBarLeftSpan><TopBarLeftSpan>SEARCH</TopBarLeftSpan>
+        </TopBarLeft>
+        <TopBarCenter>
+          <TopBarCenterSpan>GENERAL</TopBarCenterSpan><TopBarCenterSpan>BROWNBAG</TopBarCenterSpan><TopBarCenterSpan>RANDOM</TopBarCenterSpan><TopBarCenterSpan>MUSIC</TopBarCenterSpan><TopBarCenterSpan>ANNOUNCEMENTS</TopBarCenterSpan>
+        </TopBarCenter>
+        <TopBarRight>
+          <TopBarRightSpan>LOG IN</TopBarRightSpan>
+        </TopBarRight>
+      </TopBarContainer>
+    </TopBarMain>
   )
 }
 
