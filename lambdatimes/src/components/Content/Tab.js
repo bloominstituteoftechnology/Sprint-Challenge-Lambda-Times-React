62283d1c524800454177e5064a50bc1ec628dc64
Leila <nb.leila10@gmail.com>
2019-01-12 11:40:05 -0600

    Add tabs functionality - no filtering yet

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import '../../CSS/index.css';
+
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      const classes = props.selectedTab === props.tab ? 'tab active-tab' : 'tab';
   return (
     <div
-      className={''}
+      className= { props.selectedTab === props.tab ? 'tab active-tab' : 'tab' }
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         props.selectTabHandler(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
