91987532ef9504ad9fdc0157ba370baacc299e9e
Andrew <andrewryanogle@gmail.com>
2019-01-12 11:47:55 -0800

    I cant get my filter post function to work and my tabs css to work right I am going to continue to work on it

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled, {css} from 'styled-components';
+
+const TabStyle = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  ${props => props.active
+    ? css`border: 1px solid red`
+    : null
+  }
+`;
 
 const Tab = props => {
+  console.log(props.value);
+  console.log(props.tab);
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-
-  return (
-    <div
-      className={'tab'}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+    return (
+    <TabStyle 
+    onClick={() => props.filterCards}
+    name = "selected"
+    value = {props.tab}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabStyle>
   );
 };
 
-// Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string,
+  value: PropTypes.string,
+};
 
 export default Tab;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,20 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Tab from './Tab';
+import styled from 'styled-components';
+
+const TabsStyles = styled.div`
+ border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  .topics {
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+
+  .title {
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+}
+
+`
+
 const Tabs = props => {
+
   return (
-    <div className="tabs">
+    <TabsStyles>
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-            {props.tabs.map(tab=><Tab
-              key = {tab}
-              tab = {tab}
-             />)}
+        <div>
+          {props.tabs.map((tab) => 
+              <Tab
+                key={tab}
+                tab={tab}
+                value={props.value}
+                filterCards={props.filterCards}
+                //selectedTabHandler={props.selectedTabHandler}
+              />
+            )
+          }
+        </div>
       </div>
-    </div>
+    </TabsStyles>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  value: PropTypes.string,
+  //selectedTabHandler: PropTypes.func
+};
+
 export default Tabs;
