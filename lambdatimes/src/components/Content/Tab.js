9144c9cb3ea71a395b3f30a07aaa2be042866fc3
Bao Pham <ryanvuepham@gmail.com>
2019-01-12 11:48:09 -0600

    finished Content.js

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+//import PropTypes from "prop-types";
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  
+
+  
   return (
     <div
       className={''}
