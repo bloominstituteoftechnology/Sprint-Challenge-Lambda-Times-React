240eb47013dd1e10483a3c90c5d7e181e1b4a8bf
dickwinters <coding1965@gmail.com>
2019-01-12 12:42:24 -0600

    styled header with styled components

--- lambdatimes/src/components/styledComponents.js
+++ lambdatimes/src/components/styledComponents.js
@@ -9,6 +9,10 @@ export const TopBarStyle = styled.div`
   position: fixed;
   height: 44px;
   background-color: #333;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
 `;
 
 export const TBContainer = styled.div`
@@ -64,4 +68,47 @@ export const TBRContainer = styled.div`
   span {
     cursor: pointer;
   }
+`;
+
+export const HeaderStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+export const HeaderH1 = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const Span = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+export const Date = styled(Span)`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+export const Temp = styled(Span)`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
 `;
