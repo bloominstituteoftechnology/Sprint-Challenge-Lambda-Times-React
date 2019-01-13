2fe2d0670c25d093eea26a5fbf08600288919e0a
brettmadrid <brettmadrid9@gmail.com>
2019-01-12 20:45:33 -0800

    Converted Content.js from css to StyledComponents

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-
+import styled from 'styled-components'
 import Tabs from "./Tabs";
 import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from "../../data";
+
+const ContentContainer = styled.div`
+  display: flex; 
+  flex-direction: column; 
+  align-items: center;
+`
 
 export default class Content extends Component {
   constructor(props) {
@@ -53,7 +59,7 @@ export default class Content extends Component {
 
   render() {
     return (
-      <div className="content-container">
+      <ContentContainer>
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
@@ -65,7 +71,7 @@ export default class Content extends Component {
           selectTabHandler={this.changeSelected}
         />
         <Cards cards={this.filterCards()} />
-      </div>
+      </ContentContainer>
     );
   }
 }
