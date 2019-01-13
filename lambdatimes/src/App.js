ad3c809b67b61774f58d7b4c55ac731aa9f29c17
brettmadrid <brettmadrid9@gmail.com>
2019-01-12 20:38:53 -0800

    Converted App.js to StyledComponents

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 
+const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+`
+
 const App = () => {
   return (
-    <div className="App">
+    <AppContainer>
       <TopBar />
       <Header />
       <Content />
-    </div>
+    </AppContainer>
   );
 }
 
