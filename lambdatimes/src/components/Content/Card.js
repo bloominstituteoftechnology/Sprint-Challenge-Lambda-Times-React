6e52d3ad9f6669412651105f277c140a91daf666
kingmetri <demetribethel@gmail.com>
2019-01-14 18:15:22 -0500

    added Card. and proptypes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -6,14 +6,26 @@ const Card = props => {
       <div className="headline">{/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={props.card.img /* image source goes here */} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author/* author goes here */}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+
+Card.Proptypes = {
+  card: PropTypes.shape({
+    card: PropTypes.string,
+    headline: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string
+  
+})
+
+
+}
 
 export default Card;
