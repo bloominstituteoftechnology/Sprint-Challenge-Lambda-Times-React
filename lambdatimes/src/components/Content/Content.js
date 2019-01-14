35b1d4c9e7cb158f8f242945c6c4220ec50dc901
kingmetri <demetribethel@gmail.com>
2019-01-14 18:30:43 -0500

    Remember everything starts with state. Mounted.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,6 +18,10 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData,
+    })
   }
 
   changeSelected = tab => {
