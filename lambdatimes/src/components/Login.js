fbaec191a084b5e5cb0af4a7260498788dff33c3
brettmadrid <brettmadrid9@gmail.com>
2019-01-13 19:36:21 -0800

    Styled Login page using StyledComponents

--- lambdatimes/src/components/Login.js
+++ lambdatimes/src/components/Login.js
@@ -1,4 +1,41 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const LoginContainer = styled.div`
+  width: 40%;
+  margin: 0 auto;
+`
+const LoginTitle = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  color: #000;
+  width: 62%;
+  margin: 0 auto;
+`
+const Form = styled.form`
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+const Label = styled.label`
+  width: 22%;
+  margin: 0 auto;
+`
+const Input = styled.input`
+  width: 70%;
+  height: 30px;
+  background-color: lightyellow;
+  margin: 0 auto;
+`
+const Button = styled.button`
+  width: 70%;
+  height: 30px;
+  margin: 0 auto;
+`
+
 class Login extends React.Component{
  constructor(props){
   super(props);
@@ -7,11 +44,12 @@ class Login extends React.Component{
    password: '',
   };
  };
- handleInputChange = e => {
+
+ inputChangeHandler = e => {
   this.setState({ [e.target.name]: e.target.value });
  };
 
- handleLoginSubmit = e => {
+ loginSubmitHandler = e => {
  const user = this.state.username;
  localStorage.setItem('user', user);
  window.location.reload();
@@ -19,33 +57,28 @@ class Login extends React.Component{
 
  render(){
   return(
-   <div className="login-container">
-    <h1 className="login-title"> Lambda Times </h1>
-    <form>
-     <div className="login-label">
-      <label> Username: </label><br />
-     </div>
-     <input
-      className="login-input"
+   <LoginContainer>
+    < LoginTitle> Lambda Times </ LoginTitle>
+    <Form>
+      <Label> Please login... </Label><br />
+     <Input
+      placeholder="Email or username"
       type="text"
       name="username"
       value={this.state.username}
-      onChange={this.handleInputChange} /><br />
-     <div className="login-label">
-     <label> Password: </label><br />
-     </div>
-     <input
-      className="login-input"
-      type="text"
+      onChange={this.inputChangeHandler} /><br />
+     <Input
+      placeholder="password"
+      type="password"
       name="password"
       value={this.state.password}
-      onChange={this.handleInputChange} /><br />
-     <button
+      onChange={this.inputChangeHandler} /><br />
+     <Button
       className="login-submit"
       type="subimt"
-      onClick={this.handleLoginSubmit}> Submit </button>
-    </form>
-   </div>
+      onClick={this.loginSubmitHandler}> Submit </Button>
+    </Form>
+   </LoginContainer>
   )
  }
 }
