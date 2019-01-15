d2a44727e57f9a9ca699aa5eb9f7a073a7984bdf
kingmetri <demetribethel@gmail.com>
2019-01-15 13:25:13 -0500

    Corrected Headline and Tab Styling.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{props.card.headline/* headline goes here */}</div>
       <div className="author">
         <div className="img-container">
           <img src={props.card.img /* image source goes here */} />
