ff977022dedda8d4fb82ef87e1c046cec63b62ba
KieranVieira <kieranvieira@live.com>
2019-01-18 12:09:41 -0500

    Styled TobBar.js and Header.js with styled-components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderSC = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+`;
+
+const DateSC = styled.span`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+const TempSC = styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
+    <HeaderSC>
+      <DateSC>SMARCH 32, 2018</DateSC>
       <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+      <TempSC>98°</TempSC>
+    </HeaderSC>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,11 +1,80 @@
 import React from 'react';
+import styled from 'styled-components'
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const TopBarSC = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: none;
+    flex-direction: row;
+    position: fixed;
+    height: 44px;
+    background-color: #333;
+    .container {
+      width: 100%;
+      display: flex;
+      justify-content: none;
+      align-items: none;
+      flex-direction: row;
+      color: #fff;
+      letter-spacing: 1px;
+      padding: 0 10px;
+      .container-left {
+        display: flex;
+        justify-content: none;
+        align-items: center;
+        flex-direction: row;
+        flex: 1;
+        font-size: 11px;
+        span {
+          cursor: pointer;
+          margin-right: 25%;
+          font-weight: bold;
+        }
+      }
+      .container-center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        flex: 3;
+        font-size: 9px;
+        span {
+          cursor: pointer;
+          margin-right: 5%;
+          &:last-child {
+            margin-right: 0;
+          }
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+      .container-right {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        flex-direction: row;
+        flex: 1;
+        font-size: 11px;
+        font-weight: bold;
+        span {
+          cursor: pointer;
+        }
+      }
+    }
+    @media (min-width: 1280px) {
+      .container {
+        width: 1280px;
+      }
+    }
+`;
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
+    <TopBarSC>
       <div className="container">
         <div className="container-left">
           <span>TOPICS</span><span>SEARCH</span>
@@ -17,7 +86,7 @@ const TopBar = () => {
           <span>LOG IN</span>
         </div>
       </div>
-    </div>
+    </TopBarSC>
   )
 }
 
