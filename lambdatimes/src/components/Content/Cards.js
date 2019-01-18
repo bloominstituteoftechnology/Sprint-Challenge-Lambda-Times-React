93d33cff5023ede3edd468c7043fcc7946abcc6e
KROHNICK <business.kanekperez@gmail.com>
2019-01-18 10:41:03 -0800

    Refactored most of css to styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import Card from "./Card";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardContainer>
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
       {props.cards.map(card => {
         return <Card card={card} />;
       })}
-    </div>
+    </CardContainer>
   );
 };
 
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../../CSS/index.css";
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
