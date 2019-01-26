0d4f70ca8354656f7e5ceb7c9fc2d49e7406f29b
mustafa inaya <moose.inaya@icloud.com>
2019-01-26 10:52:05 -0800

    IMPLEMENT, Implemented Life Cycle Methods, And Styled Componentes.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,25 @@
 import React from 'react';
-
-const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
-        </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
-  );
+import PropTypes from 'prop-types';
+const Card = (props) => {
+	return (
+		<div className="card">
+			<div className="headline">{props.card.headline}</div>
+			<div className="author">
+				<div className="img-container">
+					<img src={props.card.img} alt="#" />
+				</div>
+				<span>By {props.card.author}</span>
+			</div>
+		</div>
+	);
 };
 
 // Make sure to include PropTypes.
-
+Card.propTypes = {
+	card: PropTypes.shape({
+		headline: PropTypes.string,
+		img: PropTypes.string,
+		author: PropTypes.string
+	})
+};
 export default Card;
