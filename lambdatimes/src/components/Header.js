8da7d221ab7450bb2c8b2719c285ff033da138e6
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2019-01-26 10:45:18 -0800

    styled components for header done

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,4 +1,42 @@
 import React from 'react';
+import Styled from 'styled-components';
+
+const HeaderClass = Styled.div`
+ display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+`
+const Date = Styled.span`
+  margin-left: 25px;
+  flex: 1;
+`
+
+const Temp = Styled.span`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
