b9c4951462bd55511bbfda58e9ca3d7e0cc29f20
thomasutsey <tomutsey@yahoo.com>
2019-01-26 14:29:18 -0500

    Map over cards in the Cards Component
    return an instance of each card

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import { cardData } from '../../data';
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map((card) => {
+            <Card 
+            headline = {card.headline}
+            imgSrc = {card.img}
+            author = {card.author}
+            key = {card.headline}
+            />
+
+          })}
     </div>
   )
 }
