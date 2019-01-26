6403f8c38540047355ae44cc9de06d611048889e
James Wigdor <jpwigdor@gmail.com>
2019-01-26 13:19:52 -0500

    Reverted changes to  because cards would not display anymore

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -7,9 +7,9 @@ const Card = props => {
       <div className="headline">{props.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.data.img} />
+          <img src={'' /* image source goes here */} />
         </div>
-        <span>By {props.data.author}</span>
+        <span>By {/* author goes here */}</span>
       </div>
     </div>
   );
@@ -17,8 +17,8 @@ const Card = props => {
 
 Card.propTypes = {
   headline: PropTypes.string,
-  img: PropTypes.string,
-  author: PropTypes.string
+  img: PropTypes.object,
+  author: PropTypes.object
 };
 
 export default Card;
