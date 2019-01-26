0fc33729ea86484b9ef845c40c10d6b4f100f381
thomasutsey <tomutsey@yahoo.com>
2019-01-26 12:46:48 -0500

    Add functions to Content.js
    use ComponentDidMount to set state to the supplied data.js file
    
    Add change Selected function to set state to the tab that is selected which will be handled by tabs.js file.
    
    Add a filterCards function that will filter through card data to find the card needed to render.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,17 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
-  }
+    this.setState({
+      tabs:tabData,
+      cards:cardData
+    })
+  };
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected:this.tab
+    })
   };
 
   filterCards = () => {
@@ -37,7 +44,12 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    // return this.state.cards;
+    if(this.state.selected === 'all'){
+      return this.state.cards;
+    }else{
+      return this.state.cards.filter(card => card.tab === this.state.selected)
+    }
   };
 
   render() {
