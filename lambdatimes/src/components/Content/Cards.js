eb61635f7d45dd1838652784629ab08587c8574d
Derek Jones <derek@derekjones.io>
2019-01-26 14:52:08 -0500

    Add PropTypes validation

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
@@ -14,5 +15,9 @@ const Cards = props => {
 };
 
 // Make sure you include prop types for all of your incoming props
+
+Cards.propTypes = {
+  cards: PropTypes.array
+};
 
 export default Cards;
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -27,5 +28,11 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  tabs: PropTypes.array,
+  changeSelected: PropTypes.func,
+  selectedTab: PropTypes.string
+};
 
 export default Tab;
