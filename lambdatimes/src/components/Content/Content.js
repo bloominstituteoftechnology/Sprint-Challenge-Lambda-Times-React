a9edc4fff253c8c10718fdf47638d30358444a37
Derek Jones <derek@derekjones.io>
2019-01-26 14:06:38 -0500

    Add functionality to filter cards based on tab clicked
    I added the ability to filter out the cards depending on the tab clicked at the top. I've also set it to change the CSS of the tab that is active.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -23,6 +23,7 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
@@ -38,7 +39,13 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    let filteredCards = this.state.cards.filter(card => card.tab === this.state.selected);
+
+    if(this.state.selected === "all") {
+      return this.state.cards;
+    } else {
+      return filteredCards;
+    }
   };
 
   render() {
@@ -49,7 +56,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs} 
+          changeSelected={this.changeSelected} 
+          selectedTab={this.state.selected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,12 +4,21 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  let active = 'tab';
+  
+  if (props.selectedTab === props.tab) {
+    active = 'tab active-tab';
+  } else {
+    active;
+  }
+
   return (
     <div
-      className={'tab'}
+      className={active}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        return props.changeSelected(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
