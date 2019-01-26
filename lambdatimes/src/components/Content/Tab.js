4a00cb5b41aad947d029aada8fe848773ee1ce66
thomasutsey <tomutsey@yahoo.com>
2019-01-26 13:13:45 -0500

    Use Tab.js to determnine className

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,10 +4,17 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
-  return (
+      let tabClassName = '';
+      if (props.tab === props.selectedTab){
+        tabClassName = 'tab active-tab'
+      }else{
+        tabClassName = 'tab'
+      }
+  
+      return (
     <div
-      className={''}
-      onClick={() => {
+      className={tabClassName}
+      onClick={() => {props.selectedTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
