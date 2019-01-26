d57bbce8a5cf29d354ec4eec910d0d2d3972916f
thomasutsey <tomutsey@yahoo.com>
2019-01-26 13:39:35 -0500

    Use Tab.js to determing classname > > > > Use if statement to determine if the selected tab is = to the tabs > > ( having trouble understanding the flow of data here) > > > > Populate the onClick with the prop passed from content selectedTabHandler which sets state to the selected on state in the content component > > Add propTypes to each prop.

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -22,7 +23,15 @@ const Tab = props => {
       {props.tab.toUpperCase()}
     </div>
   );
+  
 };
+Tab.PropTypes = {
+  tab:PropTypes.string,
+  selectedTab:PropTypes.string,
+  selectedTabHandler:PropTypes.func
+}
+
+
 
 // Make sure you include PropTypes on your props.
 
