16ecc1197b247669e1488c9de90056cd8a204605
karlie0 <kamabrapa88@gmail.com>
2019-01-26 13:29:05 -0500

    Converted Header Component to styled component

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,67 @@
 import React from 'react';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  // width: 100%;
+  width: 1280px;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+
+const Date = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`
+
+const Temperature = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">MARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    // original code below
+    // <div className="header">
+    //   <span className="date">MARCH 32, 2018</span>
+    //   <h1>Lambda Times</h1>
+    //   <span className="temp">98°</span>
+    // </div>
+  
+
+ <HeaderWrapper>
+  <Date>MARCH 2, 2018</Date> 
+  <Title>Lambda Times</Title>
+  <Temperature>98°</Temperature> 
+ </HeaderWrapper>  
+
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
