36157c45fd75cb232986350d4b242b4b50a2b06d
Paul DeYonghe <p.deyonghe@gmail.com>
2019-01-28 22:36:51 -0600

    added some PropTypes in Tabs.js and Tab.js

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -19,5 +19,13 @@ const Tabs = props => {
   );
 };
 
+
+
 // Make sure to use PropTypes to validate your types!
+
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
+
+
 export default Tabs;
