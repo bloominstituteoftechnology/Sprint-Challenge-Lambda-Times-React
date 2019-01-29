2fcf81349f4c4b76c88bc9a663a7ab1381d83762
Paul DeYonghe <p.deyonghe@gmail.com>
2019-01-28 21:14:24 -0600

    made some progress; stuck now on getting state set on my filterCards() function

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -24,15 +24,16 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    this.setState.selected = this.tab
+    this.setState({selected: tab})
   };
 
   filterCards = () => {
+    
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
       
-      Notice that we're passing this function to our <Cards /> component below.
+      Notice that we're passing this function to our <Cards /> component below.card
       This function returns an array of cards, so we can just pass it down as such.
 
       Your algorithim for the logic here is as follows: 
@@ -40,6 +41,12 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+
+  if (this.state.slected==='all') {
+        this.setState({cards: cardData})
+      } else {
+        this.setState({cards: cardData.filter(cards => cards.tab === this.state.tabs.selected)})
+      }
     return this.state.cards;
   };
 
@@ -51,7 +58,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} selectedTab={this.state.tabs.selected} selectTabHandler={this.changeSelected()} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.tabs.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -8,8 +10,8 @@ const Tabs = props => {
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
         }
-        {props.tabs.map((t,i) => <Tab selectTabHandler="props.selectTabHandler()"
-                                                                    selectedTab="props.selectedTab"
+        {props.tabs.map((t,i) => <Tab selectTabHandler={props.selectTabHandler}
+                                                                    selectedTab={props.selectedTab}
                                                                     tab={t}
                                                                     key={i} />)}
       </div>
