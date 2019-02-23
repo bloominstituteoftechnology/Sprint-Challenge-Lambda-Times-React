0a74f4a4787919ca8118540089e51a30debd914c
tactnician <tactnician@gmail.com>
2019-02-22 20:04:17 -0500

    implementd card filtering

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -41,8 +41,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    
-    return this.state.cards;
+    let filtered = [];
+    if(this.state.selected === 'all'){
+      filtered = cardData;
+    }else {
+      filtered = cardData.filter(card => card.tab === this.state.selected ? filtered.push(card): null);
+      
+    }
+    return filtered;
   };
 
   render() {
