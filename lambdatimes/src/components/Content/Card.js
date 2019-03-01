fe9f5023ff222864455f7fb0cb45f140975375cf
jkaddu <jkaddu@yahoo.com>
2019-03-01 09:16:21 -0800

    cards done

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{props.headline}</div>
+      <div className="headline">{props.data.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.img} />
+          <img src={props.data.img} />
         </div>
-        <span>By {props.author}</span>
+        <span>By {props.data.author}</span>
       </div>
     </div>
   );
 };
 
-
 // Make sure to include PropTypes.
+Card.PropTypes = {
+  data: PropTypes.shape({
+    headline: PropTypes.string,
+    tab: PropTypes.string,
+    img: PropTypes.string,
+    author: PropTypes.string,
+  }),
+}
 
 export default Card;
--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {props.cards.map(card => <Card key={card.headline} data={card} /> )
+        /* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
-
+Cards.PropTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object),
+}
 export default Cards;
