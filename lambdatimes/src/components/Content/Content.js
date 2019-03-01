4992057966f3082034fb6f8c41d97ed8f941a773
jkaddu <jkaddu@yahoo.com>
2019-03-01 09:58:56 -0800

    tabs done

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,10 +18,18 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    console.log(cardData);
+    this.setState({
+      tabs: tabData,
+      cards: cardData,
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+     selected: tab,
+    })
   };
 
   filterCards = () => {
