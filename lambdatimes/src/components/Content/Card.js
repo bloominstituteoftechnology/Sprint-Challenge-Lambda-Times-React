44809d6e877cf66accff541f69c1ebf01d990b61
paintedlbird7 <rosaperezlarios@gmail.com>
2019-03-01 08:18:13 -0800

    Initial Commit

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import ProtoTypes from 'proto-types;'
+
 
 const Card = props => {
+  const {headline, author, image} = props.card;
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+        <img src={img} alt="image" />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {author}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+
+Card.propTypes = {
+  card: PropTypes.arrayOf(
+    PropTypes.shape({
+  headline: PropTypes.string.isRequired,
+  author:PropTypes.number.isRequired,
+  img: PropTypes.string.isRequired,
+  tab: PropTypes.string.isRequired,
+    })
+  )
+  }
 
 export default Card;
