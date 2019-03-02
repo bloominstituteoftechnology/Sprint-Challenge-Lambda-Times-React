859ff6126bbd59192d96c9e2afafbd4873e07f6b
Marc Thiongo <marcthuo@gmail.com>
2019-03-02 16:58:48 -0500

    tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,16 +18,24 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+
+      tabData: tabs,
+      cardData: cards
+     })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      tab: newTab
+    })
   };
 
   filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
+      by using it to filter out our cards for when a tab is selected
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
@@ -37,7 +45,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    const cards = this.state.data.filter( card => {
+      if (card.tab.includes(event.target.value)) {
+        return card
+      }
+    });
+    this.setState({
+      cardData: cards
+    })
   };
 
   render() {
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+          props.tabData.map(tab => (
+            <div key={tab} className='tabs'>
+              <h1>{tabs.headline}</h1>
+              <img src={tabs.img} alt={tabs.author} />
+              <h4>{tabs.author}</h4>
+            </div>
+         ))
+        }       
       </div>
     </div>
-  );
-};
+  )
 
-// Make sure to use PropTypes to validate your types!
+  selectTabHandler = event => {
+    this.setState({
+      tab: event.target.value
+    })
+  }
+
+  selectedTab = event => {
+    this.setState({
+      tab: event.target.value
+    })
+  }
+
+  // Make sure to use PropTypes to validate your types!
+  Tab.PropTypes = {
+    tabs: PropTypes.arrayOf(
+      PropTypes.shape({
+        headline: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired, 
+        type: PropTypes.arrayOf( 
+          PropTypes.oneOf([
+            'all',
+            'javascript',
+            'technology',
+            'node',
+            'React.js',
+            'bootstrap'
+          ])
+        )
+      })
+    )
+  };
+}
 export default Tabs;
