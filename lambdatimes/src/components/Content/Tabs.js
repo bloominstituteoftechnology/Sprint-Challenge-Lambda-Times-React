650287c11903957b5c37985578c7a1aee7ddb11e
Marc Thiongo <marcthuo@gmail.com>
2019-03-08 13:51:49 -0500

    cards/content/tab/tabs initial rendering

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +9,23 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.tabs.map(tab => (
+              <Tab key={tab}
+              selectTabHandler={props.selectTabHandler}
+              selectedTab={props.selectedTab}
+              tab={tab} />
+            ))}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  Tab: PropTypes.arrayOf(
+    PropTypes.string),
+  selectTabHandler: PropTypes.func,
+  selectedTab: PropTypes.string    
+};
+
 export default Tabs;
