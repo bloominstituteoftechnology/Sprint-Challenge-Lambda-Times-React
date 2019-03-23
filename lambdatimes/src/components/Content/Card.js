af374a668e6fd76fb6a5ae5c875833fbd6fdc9cb
John O'Keefe <jokeefe@weegolobal.com>
2019-03-23 13:18:22 -0400

    added styled Components for Header and TopBar

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -6,7 +6,7 @@ const Card = props => {
       <div className="headline">{props.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.cards.img} />
+          <img src={props.cards.img} alt='img'/>
         </div>
         <span>By {props.cards.author}</span>
       </div>
