2633a918c52417be541e3dc37d6a5817235b5374
JustinAndrade <j.andrade.john@gmail.com>
2019-03-24 00:26:13 -0400

    MVP Completed

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,13 +7,11 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        <div className='tabs-wrapper'>
           {props.tabs.map((t, index)  => (
             <Tab key={index} tab={t} selectTabHandler={props.changeSelected} selectedTab={props.selectedTab} />
           ))}
         {/* map over the tabs provided on your props, create a new Tab component for each one.
         give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
-        </div>
       </div>
     </div>
   );
