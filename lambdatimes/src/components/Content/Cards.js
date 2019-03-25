afa421aa8750690878d9fc2ec8c420ad472195ed
Matthias Bakken <matthias.bakken@gmail.com>
2019-03-25 16:36:59 -0700

    initial commit with cards and tabs rendering

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const Cards = props => {
@@ -6,10 +7,14 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+      {props.cards.map(card => <Card card={card} />)}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.PropTypes = {
+  cards: PropTypes.array.isRequired,
+}
 
 export default Cards;
