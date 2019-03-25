ef86cad61c94fb4656bb243e52d482dc61231a61
James Wigdor <jpwigdor@gmail.com>
2019-03-24 22:50:22 -0400

    Add Stylized-Components to everything. Stretch Goal 1 complete

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,17 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import Card from "./Card";
 
+const StyledCardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <StyledCardsContainer>
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
       {props.cards.map(card => {
         return <Card {...card} />;
       })}
-    </div>
+    </StyledCardsContainer>
   );
 };
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,13 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import Tab from "./Tab";
 
+const StyledTabs = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+  @media (min-width: 1280px) {
+    width: 12080px;
+  }
+`;
+
+const StyledTopics = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const StyledTitle = styled.span`
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`;
+
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <StyledTabs>
+      <StyledTopics>
+        <StyledTitle>TRENDING TOPICS:</StyledTitle>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
         {props.tabs.map(tab => (
@@ -18,8 +46,8 @@ const Tabs = props => {
             tab={tab}
           />
         ))}
-      </div>
-    </div>
+      </StyledTopics>
+    </StyledTabs>
   );
 };
 
