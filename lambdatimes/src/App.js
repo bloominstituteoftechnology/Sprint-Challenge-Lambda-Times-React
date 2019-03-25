e2a32d273d073871657308686b0be53a81c4066b
James Wigdor <jpwigdor@gmail.com>
2019-03-24 22:33:21 -0400

    Use Stylized Components on App.js & Content.js

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import TopBar from './components/TopBar';
-import Header from './components/Header';
-import Content from './components/Content/Content';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import TopBar from "./components/TopBar";
+import Header from "./components/Header";
+import Content from "./components/Content/Content";
+
+const StyledApp = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: "PT Sans", sans-serif;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <StyledApp>
       <TopBar />
       <Header />
       <Content />
-    </div>
+    </StyledApp>
   );
-}
+};
 
 export default App;
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 import Tabs from "./Tabs";
 import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from "../../data";
+
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -52,7 +59,7 @@ export default class Content extends Component {
 
   render() {
     return (
-      <div className="content-container">
+      <StyledContentContainer>
         {/* 
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
@@ -64,7 +71,7 @@ export default class Content extends Component {
           selectTabHandler={this.changeSelected}
         />
         <Cards cards={this.filterCards()} />
-      </div>
+      </StyledContentContainer>
     );
   }
 }
