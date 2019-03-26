8ff090fbc86c6a04105d597706fd9a492aa0f33b
JustinAndrade <j.andrade.john@gmail.com>
2019-03-25 21:09:47 -0400

    MVP Completed

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
-const span = styled.span;
 
 const TopBarWrapper = styled.div`
   width: 100%;
