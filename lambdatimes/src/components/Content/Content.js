8d96359778fd0ff1562ee7e9ad7992bd38e73650
danpatrickoneill <dan47573@gmail.com>
2019-03-29 11:41:46 -0400

    Completed content, tabs + tab; appears functional

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Tabs from './Tabs';
-import Cards from './Cards';
+import Tabs from "./Tabs";
+import Cards from "./Cards";
 
 // Importing our tab and card data. No need to change anything here.
-import { tabData, cardData } from '../../data';
+import { tabData, cardData } from "../../data";
 
 export default class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selected: 'all',
+      selected: "all",
       tabs: [],
       cards: []
     };
@@ -18,10 +18,21 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    });
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    // IS THIS NEXT BIT RIGHT???
+    this.setState({
+      selected:
+        tab.target.innerText.toLowerCase() === "react.js"
+          ? "React.js"
+          : tab.target.innerText.toLowerCase()
+    });
   };
 
   filterCards = () => {
@@ -37,7 +48,15 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === "all") {
+      return this.state.cards;
+    } else {
+      const filteredCards = this.state.cards.filter(
+        card => card.tab === this.state.selected
+      );
+
+      return filteredCards;
+    }
   };
 
   render() {
@@ -48,7 +67,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs
+          tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import Tab from './Tab';
+import React from "react";
+import PropTypes from "prop-types";
+
+import Tab from "./Tab";
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +10,22 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map((tab, index) => (
+          <Tab
+            key={index}
+            tab={tab}
+            selectedTab={props.selectedTab}
+            selectTabHandler={props.selectTabHandler}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tab: PropTypes.arrayOf(PropTypes.string)
+};
+
 export default Tabs;
