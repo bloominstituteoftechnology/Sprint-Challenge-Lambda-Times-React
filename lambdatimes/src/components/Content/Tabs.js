d276a53805fd4cb7f2eba45e59aa29164309a509
Alexnadria Supriana <alexa9410@gmail.com>
2019-03-29 12:37:52 -0400

    Update Tabs.js

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
 import PropTypes from 'prop-types';
-//import styled from 'styled-components';
+import styled from 'styled-components';
 
 
 const TabsGeneral = styled.div`
