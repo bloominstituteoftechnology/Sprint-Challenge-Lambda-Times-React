a09e6aaf7b6681693e8edcafba6bbc2ad4e13859
danpatrickoneill <dan47573@gmail.com>
2019-03-29 12:58:24 -0400

    Implemented carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,66 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data'
-// Complete this Carousel 
+import React, { Component } from "react";
+import { carouselData } from "../../data";
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      images: [],
+      index: 0
+    };
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({
+      images: carouselData
+    });
   }
 
   leftClick = () => {
-
-  }
+    console.log(this.state.images);
+    if (this.state.index === 0) {
+      this.setState({
+        index: 3
+      });
+    } else {
+      this.setState({
+        index: this.state.index - 1
+      });
+    }
+  };
 
   rightClick = () => {
+    console.log(this.state.images);
+    if (this.state.index === 3) {
+      this.setState({
+        index: 0
+      });
+    } else {
+      this.setState({
+        index: this.state.index + 1
+      });
+    }
+  };
 
-  }
+  SelectedImage = () => {
+    return (
+      <img
+        src={this.state.images[this.state.index]}
+        style={{ display: "block" }}
+      />
+    );
+  };
 
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+  render() {
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <div className="left-button" onClick={this.leftClick}>
+          {"<"}
+        </div>
+        <this.SelectedImage />
+        <div className="right-button" onClick={this.rightClick}>
+          {">"}
+        </div>
       </div>
-    )
+    );
   }
 }
