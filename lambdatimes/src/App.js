3aad51c8b3884d95c99b292f68613078a62aaa7c
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-29 13:44:56 -0400

    styled-components, think i've been fooled

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -3,14 +3,27 @@ import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
 import Carousel from './components/Carousel/Carousel';
+import ThemeProvider from './theme';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+`;
 const App = () => {
   return (
-    <div className="App">
-      <TopBar />
-      <Carousel />
-      <Header />
-      <Content />
-    </div>
+    <ThemeProvider>
+      <StyledApp>
+        <TopBar />
+        <Carousel />
+        <Header />
+        <Content />
+      </StyledApp>
+    </ThemeProvider>
   );
 };
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
@@ -8,17 +8,22 @@ const TopBar = () => {
     <div className="top-bar">
       <div className="container">
         <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
+          <span>TOPICS</span>
+          <span>SEARCH</span>
         </div>
         <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
+          <span>GENERAL</span>
+          <span>BROWNBAG</span>
+          <span>RANDOM</span>
+          <span>MUSIC</span>
+          <span>ANNOUNCEMENTS</span>
         </div>
         <div className="container-right">
           <span>LOG IN</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopBar;
