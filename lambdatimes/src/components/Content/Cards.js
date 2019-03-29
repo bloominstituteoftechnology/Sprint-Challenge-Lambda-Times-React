50c721ce279697bb86e68fb3127c877f066cc9c9
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-29 12:48:52 -0400

    project done, where's my gf

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import Card from './Card';
 
-const Cards = props => {
+const Cards = ({ cards }) => {
+  console.log('TCL: Cards -> cards', cards);
+
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {cards.map((card, i) => (
+        <Card key={i} {...card} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-// Make sure you include prop types for all of your incoming props
-
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string.isRequired,
+      img: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Tab from './Tab';
-const Tabs = props => {
+
+const Tabs = ({ tabs, selectTabHandler, selectedTab }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {tabs.map(tab => (
+          <Tab
+            key={tab}
+            tab={tab}
+            selectedTab={selectedTab}
+            selectTabHandler={selectTabHandler}
+          />
+        ))
+        /* map over the tabs provided on your props, create a new Tab component for each one.
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+        }
       </div>
     </div>
   );
