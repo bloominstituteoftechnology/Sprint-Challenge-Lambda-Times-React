b95f85bae2a1f99d418b5982cb0e63c985861a40
austie702 <austie702@gmail.com>
2019-03-29 22:55:56 -0500

    Now that the implicit return nightmare is over, MVP is up and running

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,15 +6,12 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
             {props.tabs.map(tab => (
-              <Tab tab={tab} key={tab} selectTabhandler={props.selectTabHandler} selectedTab={props.selectedTab} />
+              <Tab tab={tab} key={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} />
             ))}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
 export default Tabs;
