53177884427a678e5673bf01bc9793685db17444
Moses Samuel <mosamuel98@gmail.com>
2019-05-03 10:23:09 -0700

    add JSX for Cards component

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import Card from './Card';
+import React from 'react'
+import Card from './Card'
 
-const Cards = props => {
+
+export default function Cards (props) {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {
+        props.cards.map(Card)
+      }
     </div>
   )
 }
 
-// Make sure you include prop types for all of your incoming props
 
-export default Cards;
+// ---- Instructions ----
+
+/* 
+  Using the cards prop, map over the list creating a 
+  new Card component for each passing the card as the only prop.
+*/
+
+// Make sure you include prop types for all of your incoming props
