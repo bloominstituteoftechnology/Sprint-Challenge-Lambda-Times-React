51ab9c50568c5e67d7c72c066327a112d65ad047
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-05-03 09:52:22 -0700

    Styled components done, MVP

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,112 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
-const TopBar = () => {
-  return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+const StyledTopBar = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: none;
+	flex-direction: row;
+	position: fixed;
+	height: 44px;
+	background-color: #333;
+	.container {
+		width: 100%;
+		display: flex;
+		justify-content: none;
+		align-items: none;
+		flex-direction: row;
+		color: #fff;
+		letter-spacing: 1px;
+		padding: 0 10px;
+		.container-left {
+			display: flex;
+			justify-content: none;
+			align-items: center;
+			flex-direction: row;
+			flex: 1;
+			font-size: 11px;
+			span {
+				cursor: pointer;
+				margin-right: 25%;
+				font-weight: bold;
+			}
+		}
+		.container-center {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: row;
+			flex: 3;
+			font-size: 9px;
+			span {
+				cursor: pointer;
+				margin-right: 5%;
+				&:last-child {
+					margin-right: 0;
+				}
+				&:hover {
+					text-decoration: underline;
+				}
+			}
+		}
+		.container-right {
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			flex-direction: row;
+			flex: 1;
+			font-size: 11px;
+			font-weight: bold;
+			span {
+				cursor: pointer;
+			}
+		}
+		.container-login {
+			color: #ec0;
+			&:hover {
+				color: goldenrod;
+				transform: scale(1.1);
+			}
+		}
+	}
+	@media (min-width: 1280px) {
+		.container {
+			width: 1280px;
+		}
+	}
+`;
+
+const TopBar = (props) => {
+	return (
+		<StyledTopBar>
+			<div className="container">
+				<div className="container-left">
+					<span>TOPICS</span>
+					<span>SEARCH</span>
+				</div>
+				<div className="container-center">
+					<span>GENERAL</span>
+					<span>BROWNBAG</span>
+					<span>RANDOM</span>
+					<span>MUSIC</span>
+					<span>ANNOUNCEMENTS</span>
+				</div>
+				<div className="container-right">
+					{localStorage.getItem('loggedIn') ? (
+						<i className="fas fa-user" onClick={props.logOut} />
+					) : (
+						<div className="container-login">
+							<span onClick={(event) => props.login(event)}>LOG IN</span>
+						</div>
+					)}
+				</div>
+			</div>
+		</StyledTopBar>
+	);
+};
 
 export default TopBar;
