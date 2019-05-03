2ff7ba07ceca90b841a85c94682ccd3346ed8da8
mdelzotto <madelzotto@comcast.net>
2019-05-03 12:44:25 -0400

    using prop to show articles on newspaper

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -18,9 +18,15 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
   }
 
-  changeSelected = tab => {
+  changeSelected = (x,tab) => {
+    x.preventDefault()
+    this.setState({selected:tab})
     // this function should take in the tab and update the state with the new tab.
   };
 
@@ -28,27 +34,36 @@ export default class Content extends Component {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
-      
+
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
 
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
+      Your algorithim for the logic here is as follows:
+        - if the selected tab is 'all' it should return all
+          of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+
+    if (this.state.selected === 'all') {
+      return this.state.cards;
+    }
+    else {
+      return this.state.cards.filter((card) => card.tab.includes(this.state.selected));
+    }
   };
 
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
+        {/*
+          Add 2 props to the Tabs component,
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectedTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types'
+
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+            props.tabs.map((tab) => {
+              return <Tab
+                key={tab}
+                tab={tab}
+                selectedTab={props.selectedTab}
+                selectedTabHandler={props.selectedTabHandler}
+              />
+            })
+
+          }
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+
+Tab.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string),
+  selectedTab: PropTypes.string.isRequired,
+  selectedTabHandler: PropTypes.func.isRequired
+}
+
 export default Tabs;
