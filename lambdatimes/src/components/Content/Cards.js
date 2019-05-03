27a4f5d636d22f942d51d17cc9a6fcba72717234
Chase Wex <clapingmanly@hotmail.com>
2019-05-03 11:35:17 -0400

    Fixed cards so they render the correct data when passed

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
+  console.log(props.cards);
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+          new Card component for each passing the card as the only prop*/
+          props.cards.map((x,i) => <Card key={`${i}  :card`} data={x}/>)
+      }
     </div>
   )
 }
-
+Cards.propTypes =
+{
+  data: Object
+}
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
