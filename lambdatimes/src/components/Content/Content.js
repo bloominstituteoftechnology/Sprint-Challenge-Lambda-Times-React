c123d444a03d2b7fb71218c4d9ffc5bcf6fdc304
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-03 10:02:37 -0700

    added 2 props to tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -49,7 +49,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs selectedTab={this.state.selected} selectTabHandler={this.state.changeSelected} tabs={this.state.tabs} />
         <Cards cards={this.filterCards()} />
       </div>
     );
