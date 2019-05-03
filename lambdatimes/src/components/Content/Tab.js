70a623174516db4f8c2c86978b3508da32f277c3
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-03 10:37:24 -0700

    added ternary function to className, added click handler function

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,12 +4,15 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+      
   return (
     <div
-      className={''}
+      className={props.selectedTab ? 'tab active-tab' : 'tab'}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+        return props.selectTabHandler(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
