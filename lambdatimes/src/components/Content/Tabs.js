5e0c14dbdfac4fa303af21917f3b14971f1f3b15
Chase Wex <clapingmanly@hotmail.com>
2019-05-03 11:52:00 -0400

    Added functionality to tabs and fix some warnings

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,7 +6,9 @@ const Tabs = props => {
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+          props.tabs.map((x,i)=> <Tab key={`${i}:tab`} tab={x} selectTabHandler={props.selectTabHandler}/>)
+        }
       </div>
     </div>
   );
