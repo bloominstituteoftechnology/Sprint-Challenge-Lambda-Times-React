8527180b6846781cca80453657a41f39b756d1cc
Manya0 <manya01@protonmail.com>
2019-05-03 14:31:35 -0400

    styles

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 const Card = props => {
   return (
@@ -15,5 +16,14 @@ const Card = props => {
 };
 
 // Make sure to include PropTypes.
+
+// Card.propTypes = {
+//   card: propTypes.shape({
+//     headline: propTypes.string.isRequired,
+//     tab: propTypes.string.isRequired,
+//     img: propTypes.string.isRequired,
+//     author: propTypes.string.isRequired
+// })
+// }
 
 export default Card;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,107 @@
 import React from 'react';
-
+import styled from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
+const TopBarWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const ContainerWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  @media (min-width: 1280px) {
+    .top-bar .container {
+      width: 1280px;
+    };
+`;
+
+const ContLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+
+const ContLeftSpan = styled.div`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+const ContCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+
+const ContCenterSpan = styled.div`
+  cursor: pointer;
+  margin-right: 5%;
+   :last-child {
+    margin-right: 0;
+  };
+  :hover {
+    text-decoration: underline;
+  };
+`;
+
+const ContRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+;`
+
+const ContRightSpan = styled.div`
+cursor: pointer;
+`;
+
+
+
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <TopBarWrapper>
+      <ContainerWrapper>
+        <ContLeft>
+          <ContLeftSpan>TOPICS</ContLeftSpan>
+          <ContLeftSpan>SEARCH</ContLeftSpan>
+        </ContLeft>
+        <ContCenter>
+          <ContCenterSpan>GENERAL</ContCenterSpan>
+          <ContCenterSpan>BROWNBAG</ContCenterSpan>
+          <ContCenterSpan>RANDOM</ContCenterSpan>
+          <ContCenterSpan>MUSIC</ContCenterSpan>
+          <ContCenterSpan>ANNOUNCEMENTS</ContCenterSpan>
+        </ContCenter>
+        <ContRight>
+          <ContRightSpan>LOG IN</ContRightSpan>
+        </ContRight>
+      </ContainerWrapper>
+    </TopBarWrapper>
   )
 }
 
