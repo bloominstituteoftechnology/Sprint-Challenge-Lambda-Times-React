38009f912dd552abef3ce2aa71e5dbbf004f6433
Moses Samuel <mosamuel98@gmail.com>
2019-05-03 09:55:56 -0700

    map over props and render tabs in Tabs component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import Tab from './Tab';
-const Tabs = props => {
+import React from 'react'
+import Tab from './Tab'
+
+
+export default function Tabs(props) {
+  const renderTab = (props) => (mappedVal, i) => (
+    <Tab tab={mappedVal} postition={i} {...props} />
+  )
+
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          {
+            props.tabs.map(renderTab({...props}))
+          }
       </div>
     </div>
-  );
-};
+  )
+}
+
+
+// ---- Instructions ----
+
+/* 
+  map over the tabs provided on your props, create a new Tab component for each one.
+  Give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props.
+*/
 
 // Make sure to use PropTypes to validate your types!
-export default Tabs;
