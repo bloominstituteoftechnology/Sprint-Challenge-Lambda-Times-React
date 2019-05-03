439ab8fec996f4feb96e78f63771d173ec14b12a
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-05-03 09:30:39 -0700

    cards.js done

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
-const Cards = props => {
+const Cards = (props) => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      {props.cards.map((card, id) => <Card cardData={card} key={id} />)}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+Cards.propTypes = {
+  card: PropTypes.array.isRequired
+}
 
 export default Cards;
