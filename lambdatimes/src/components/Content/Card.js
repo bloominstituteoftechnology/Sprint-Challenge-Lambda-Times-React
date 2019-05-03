b8858b48d0f97b59ae8a83cafd5e378bdb143618
Moses Samuel <mosamuel98@gmail.com>
2019-05-03 10:12:51 -0700

    add proper JSX for Card component

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Card = props => {
+export default function Card({ img:imgSrc, author, headline }) {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      <div className="headline">{headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={imgSrc} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By{author}</span>
       </div>
     </div>
   );
@@ -16,4 +16,3 @@ const Card = props => {
 
 // Make sure to include PropTypes.
 
-export default Card;
