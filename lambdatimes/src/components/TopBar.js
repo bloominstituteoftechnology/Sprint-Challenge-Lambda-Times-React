96902dd6e5558c3fecaafea029c2ef27f38efaad
Chase Wex <clapingmanly@hotmail.com>
2019-05-03 12:55:25 -0400

    Fixed error where carousle was clipping on the nav bar sicky

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 const Topbar = styled.div`
+  z-index: 100;
   width: 100%;
   display: flex;
   justify-content: center;
