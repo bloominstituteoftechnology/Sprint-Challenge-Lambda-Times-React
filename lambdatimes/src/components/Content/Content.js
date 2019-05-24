8cc515dcbb56733ca4a34cfae816d7f74698467f
laurenalindquist <laurenalindquist@gmail.com>
2019-05-24 08:40:06 -0700

    tabs rendered

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -52,7 +52,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} />
         {/* <Cards cards={this.filterCards()} /> */}
         <Cards cards={this.state.cards} />
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,6 +5,10 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+
+        {props.tabs.map(tab => 
+        <Tab tab={tab} selectedTab={props.selectedTab}/> )}
+
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
@@ -14,3 +18,6 @@ const Tabs = props => {
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
+
+// props.tabs = this.state.tabs
+// props.selectedtab = this.state.selected... which is selected: 'all'
