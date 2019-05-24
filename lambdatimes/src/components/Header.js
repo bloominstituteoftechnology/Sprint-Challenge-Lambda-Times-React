4357ab80af6628fcd991bfdc1e2b5ca131b180c4
Lambda_School_Loaner_96 <lambda_school_loaner_96@LambdaSer96sMBP.attlocal.net>
2019-05-24 14:48:09 -0400

    styled-components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
@@ -12,5 +13,42 @@ const Header = () => {
     </div>
   )
 }
+
+const HeaderDiv = styled.div`
+display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+
+const DateSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+const Title = styled.h1`
+font-size: 60px;
+font-family: Didot, serif;
+font-weight: bold;
+flex: 8;
+text-align: center;
+color: #000;
+`;
+
+const TempSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`
+
 
 export default Header
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
@@ -20,5 +21,64 @@ const TopBar = () => {
     </div>
   )
 }
+
+const TopDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const TBContainer = styled.div`
+width: 100%;
+display: flex;
+justify-content: none;
+align-items: none;
+flex-direction: row;
+color: #fff;
+letter-spacing: 1px;
+padding: 0 10px;
+}
+@media (min-width: 1280px) {
+.top-bar .container {
+  width: 1280px;
+}
+}
+`
+
+const ContainLeft = styled.div`
+display: flex;
+justify-content: none;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+`
+
+const ContainCent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`
+
+const ContainRight = styled.div`
+display: flex;
+justify-content: flex-end;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+font-weight: bold;
+`
+
+
+
 
 export default TopBar;
