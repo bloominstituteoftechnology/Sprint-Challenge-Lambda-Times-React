006bce67cc720f13daf7fabe68a83e779fd3bf85
Kyle Barnett <kbarnett23@gmail.com>
2019-06-01 12:08:57 -0600

    finishing MVP working on styled components now

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -3,6 +3,8 @@ import React from 'react';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+
+
 const Header = () => {
   return (
     <div className="header">
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,96 @@
 import React from 'react';
-
+import styled from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarStyles = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`;
+
+const ContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+
+  span {
+    cursor: pointer;
+    margin-right: 25%;
+    font-weight: bold;
+  }
+`;
+
+const ContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+
+  span {
+    cursor: pointer;
+    margin-right: 5%;
+  }
+
+  span:last-child {
+    margin-right: 0;
+  }
+
+  span:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+
+  span {
+    cursor: pointer;
+  }
+`;
+
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
+    <TopBarStyles>
+      <Container>
+        <ContainerLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
+        </ContainerLeft>
+        <ContainerCenter>
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+        </ContainerCenter>
+        <ContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+        </ContainerRight>
+      </Container>
+    </TopBarStyles>
   )
 }
 
