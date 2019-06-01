8f4b1504ed04cd1727a1992d28a2d4157fec8c0f
JimmyMcBride <mcbride967@gmail.com>
2019-06-01 12:16:38 -0500

    topbar styled

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -34,13 +34,13 @@ export default class Content extends Component {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
       by using it to filter out our cards for when a tab is selcted
-      
+
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
 
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
+      Your algorithim for the logic here is as follows:
+        - if the selected tab is 'all' it should return all
+          of the items from cardData.
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
     // return this.state.cards;
@@ -57,13 +57,13 @@ export default class Content extends Component {
   render() {
     return (
       <div className="content-container">
-        {/* 
-          Add 2 props to the Tabs component, 
+        {/*
+          Add 2 props to the Tabs component,
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs 
-          tabs={this.state.tabs} 
+        <Tabs
+          tabs={this.state.tabs}
           selectedTab={this.state.selected}
 					selectTabHandler={this.changeSelected}
         />
