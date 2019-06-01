a7fee275d830f595d0a21dac8acd62ea810c7445
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-05-31 21:22:35 -0500

    mvp met

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
@@ -13,5 +13,5 @@ const App = () => {
   );
 }
 
-export default app;
+export default App;
 
