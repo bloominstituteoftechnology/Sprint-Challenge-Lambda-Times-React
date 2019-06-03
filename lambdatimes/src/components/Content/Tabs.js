f588c600e939ecc96cfc097caa6dd5551b082b59
schang1146 <sammychang2185@gmail.com>
2019-06-02 20:02:01 -0700

    tabs show; changing tabs works

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,6 +8,9 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab => (
+          <Tab key={tab} tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} />
+        ))}
       </div>
     </div>
   );
