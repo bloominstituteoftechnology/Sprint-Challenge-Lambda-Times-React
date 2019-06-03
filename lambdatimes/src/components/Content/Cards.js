9576cf1610e1ac4e38c6b84adf5c723257f7e43a
schang1146 <sammychang2185@gmail.com>
2019-06-02 20:12:21 -0700

    changing tabs filters cards

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,8 +6,8 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
-      {props.cards.map(card => (
-        <Card card={card} />
+      {props.cards.map((card,idx) => (
+        <Card key={idx} card={card} />
       ))}
     </div>
   )
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -44,11 +44,14 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === 'all') {
+      return this.state.cards;
+    } else {
+      return this.state.cards.filter(card => card.tab === this.state.selected);
+    }
   };
 
   render() {
-    console.log(this)
     return (
       <div className="content-container">
         {/* 
