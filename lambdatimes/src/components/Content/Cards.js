35b645fc84d466287a2b26e3ded895a7d15d3679
jacobdurst <jdd2007@gmail.com>
2019-06-03 15:04:26 -0400

    tabs and cards displaying correctly

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
+  console.log("props in Cards, ", props)
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+      { 
+        props.cards.map( card => { return < Card card = {card} /> })
+        
+        
+        /* Using the cards prop, map over the list creating a 
+          new Card component for each passing the card as the only prop*/
+      
+      }
+
     </div>
   )
 }
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,13 +4,22 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+  console.log("props in Tab, ", props)
+  let tab = "";
+  if(props.tab === props.selectedTab) {
+    tab = "tab active-tab"
+  }
+  else {
+    tab = "tab"
+  }
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={"tab"}
+      onClick={ () => {
+        const selectedTab = props.tab;
+        props.selectTabHandler(selectedTab)}}
         /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+         you'll need to pass the `tab` in as an argument to this handler. */ 
     >
       {props.tab.toUpperCase()}
     </div>
