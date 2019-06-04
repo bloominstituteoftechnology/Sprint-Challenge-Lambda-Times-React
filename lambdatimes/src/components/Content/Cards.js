a09bcf18853c5e76f5ea176dc052c58572399662
shian simms <ssimmswork@gmail.com>
2019-06-03 20:30:44 -0400

    mvp

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import Proptypes from 'prop-types';
+
 
 const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map(card => (
+            <Card key={card.i} 
+            card={card} 
+            />
+          ))}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+
 
 export default Cards;
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const tabStyleDiv = styled.div `
+  display: flex;
+  margin:auto;
+   `
+  
 const Tabs = props => {
-  return (
+    return (
+    <tabStyleDiv>
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+       
+            {props.tabs.map ( tab => {
+              return (
+                <Tab  
+                  id = {tab.index}
+                  tab = {tab}
+              selectTabHandler={props.selectTabHandler}
+              selectedTab={props.selectedTab}
+                  />
+              )
+            }
+            )}
+              
       </div>
     </div>
+     </tabStyleDiv>
   );
 };
-
 // Make sure to use PropTypes to validate your types!
+PropTypes.Tabs = {
+  tabs: PropTypes.array,
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func,
+  };
 export default Tabs;
