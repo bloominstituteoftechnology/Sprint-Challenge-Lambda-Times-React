ece5420854120ef15bb90ed19eeb41e21188f461
Elise Spain <elisespain@gmail.com>
2019-06-07 08:53:04 -0700

    tab prototypes

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
+  className={ props.tab === props.selectedTab ? 'tab active-tab':'tab'}
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
       className={''}
-      onClick={() => {
+      onClick={(tab) => {
+        return(
+          selectTabHandler ={props.selectTabHandler}
+        )
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
@@ -18,5 +23,11 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes ={
+  tab: PropTypes.arrayOf(Prop.Types.string),
+  selectedTab:PropTypes.string,
+  selectTabHandler:PropTypes.string
+
+}
 
 export default Tab;
