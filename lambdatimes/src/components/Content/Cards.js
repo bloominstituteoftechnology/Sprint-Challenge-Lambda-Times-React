6b6c70c6c172778097e1296e1436dd3f4265bc2a
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-07 11:16:55 -0500

    Update Cards.js

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,31 @@
+
 import React, { Component } from 'react';
 import Card from './Card';
-
+import PropTypes from 'prop-types'
+//map through the cards whist passing props
 const Cards = props => {
+ 
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      { props.cards.map( card => (
+         < Card card ={card} />
+         
+      ))
+        
+        /* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+         
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
-
+Cards.propTypes = {
+  cardData: PropTypes.shape({
+    headline:PropTypes.string,
+    tab:PropTypes.string,
+    img:PropTypes.string,
+    author:PropTypes.string,
+  })
+}
 export default Cards;
