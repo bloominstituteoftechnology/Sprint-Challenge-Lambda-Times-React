396105c7dad3b40a2442b4b1cadb26de75dd36be
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-06-07 11:43:02 -0700

    header and top bar styled components finished

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,51 @@
 import React from 'react';
-
+import styled from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const HeaderStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
 
+const StyledHeading = styled.h1`
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+const StyledSpan = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`
+const StyledDate = styled(StyledSpan)`
+  margin-left: 25px;
+  flex: 1;
+`
+const StyledTemp = styled(StyledSpan)`
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderStyle>
+      <StyledDate>SMARCH 32, 2018</StyledDate>
+      <StyledHeading>Lambda Times</StyledHeading>
+      <StyledTemp>98°</StyledTemp>
+    </HeaderStyle>
   )
 }
 
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,89 @@
 import React from 'react';
-
+import styled from 'styled-components'
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+`
+const StyledContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+`
+const StyledContainerLeft = styled(StyledContainer)
+`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`
+const StyledContainerCenter = styled(StyledContainer)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`
+const StyledContainerRight = styled(StyledContainer)`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`
+const StyledSpanLeft = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`
+const StyledSpanCenter = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+const StyledSpanRight = styled.span`
+  cursor: pointer;
+`
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <StyledTopBar>
+      <StyledContainer>
+        <StyledContainerLeft>
+          <StyledSpanLeft>TOPICS</StyledSpanLeft>
+          <StyledSpanLeft>SEARCH</StyledSpanLeft>
+        </StyledContainerLeft>
+        <StyledContainerCenter>
+          <StyledSpanCenter>GENERAL</StyledSpanCenter>
+          <StyledSpanCenter>BROWNBAG</StyledSpanCenter>
+          <StyledSpanCenter>RANDOM</StyledSpanCenter>
+          <StyledSpanCenter>MUSIC</StyledSpanCenter>
+          <StyledSpanCenter>ANNOUNCEMENTS</StyledSpanCenter>
+        </StyledContainerCenter>
+        <StyledContainerRight>
+          <StyledSpanRight>LOG IN</StyledSpanRight>
+        </StyledContainerRight>
+      </StyledContainer>
+    </StyledTopBar>
   )
 }
 
