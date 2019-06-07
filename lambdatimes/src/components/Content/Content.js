b54cd05739f5e4a4ddee8b2d90e34fcb70f87bb1
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-07 11:17:33 -0500

    Update Content.js

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -14,14 +14,24 @@ export default class Content extends Component {
       tabs: [],
       cards: []
     };
+    
   }
-
-  componentDidMount() {
-    // Once the component has mounted, get the data and reflect that data on the state.
+// console.log(this.state.tabs)
+  componentDidMount = () => {
+     
+    this.setState({
+      tabs:tabData,
+      cards:cardData,
+    })
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({
+      selected: tab,
+      
+
+    })
   };
 
   filterCards = () => {
@@ -31,13 +41,22 @@ export default class Content extends Component {
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
-
       Your algorithim for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+
+  // 
+  const filtered = this.state.cards.filter((card) => card.tab === this.state.selected ); 
+  
+  if(this.state.selected === 'all'){
     return this.state.cards;
+  }else{
+    return filtered;
+    
+  }
+    
   };
 
   render() {
@@ -48,7 +67,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs  selectTabHandler= {this.filterCards} tabs={this.state.tabs} selectedtab={this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
